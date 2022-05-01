5609ede1895baac5c70b97953947707fc5d39e0e
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-05-01 09:35:00 -0500

    progress on schema fingerprinting

--- common/helpers/src/avro.rs
+++ common/helpers/src/avro.rs
@@ -1,1 +1,36 @@
-use apache_avro::{schema::Schema, Codec, Reader, Writer};
+use apache_avro::schema::Schema;
+use sha2::Sha256;
+
+#[derive(thiserror::Error, Debug)]
+pub enum AvroError {
+	#[error("I/O error")]
+	Io(#[from] std::io::Error),
+	#[error("Avro error")]
+	Avro(#[from] apache_avro::Error),
+}
+
+pub fn fingerprint_raw_schema(raw_schema: &str) -> Result<(Schema, Vec<u8>), AvroError> {
+	// parse_str will fail if schema is not valid
+	let schema = Schema::parse_str(raw_schema)?;
+	// get the schema finger print
+	let schema_fingerprint = schema.fingerprint::<Sha256>();
+	Ok((schema, schema_fingerprint.bytes))
+}
+
+pub fn fingerprint_raw_schema_list(
+	raw_schema: &[&str],
+) -> Result<(Vec<Schema>, Vec<Vec<u8>>), AvroError> {
+	let mut schema_fingerprints = Vec::new();
+	let mut schemas = Vec::new();
+	// iterate over schema list and generate schema fingerprints
+	for raw_schema in raw_schema {
+		// parse_str will fail if schema is not valid
+		let schema = Schema::parse_str(raw_schema)?;
+		// get the schema finger print
+		let schema_fingerprint = schema.fingerprint::<Sha256>();
+		// add to return
+		schema_fingerprints.push(schema_fingerprint.bytes);
+		schemas.push(schema);
+	}
+	Ok((schemas, schema_fingerprints))
+}
--- common/helpers/src/lib.rs
+++ common/helpers/src/lib.rs
@@ -1,3 +1,1 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
 pub mod avro;
