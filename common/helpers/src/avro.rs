ddef355850b4e15ba2bd8bcf016177e77ff0a470
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-05-02 07:21:15 -0500

    placeholder writer vec schema

--- common/helpers/src/avro.rs
+++ common/helpers/src/avro.rs
@@ -1,4 +1,8 @@
-use apache_avro::schema::Schema;
+use apache_avro::{schema::Schema, Codec, Reader, Writer, types::Record};
+use std::collections::HashMap;
+use std::io::{Read, Write};
+use std::str;
+use crate::types::*;
 
 #[derive(thiserror::Error, Debug)]
 pub enum AvroError {
@@ -9,20 +13,43 @@ pub enum AvroError {
 }
 
 pub fn set_writer_schema<'a, W: Write>(writer: W, schema: &'a Schema) -> Writer<'a, W> {
-    Writer::with_codec(schema, writer, Codec::Snappy)
+	Writer::with_codec(schema, writer, Codec::Snappy)
 }
 
-pub fn get_reader_schema<'a, R: Read>(reader: R, schema: &'a Schema) -> Result<Reader<'a, R>, AvroError> {
-    Ok(Reader::with_schema(&schema, reader)?)
+pub fn get_writer_schema<'a>(schema: &'a Schema) -> Writer<'a, Vec<u8>> {
+	Writer::with_codec(schema, Vec::new(), Codec::Snappy)
 }
 
-pub fn populate_record<W: Write>(writer: & mut Writer<W>, record: &HashMap<String, Value>) -> Result<(), AvroError> {
-    let mut record_list = Record::new(writer.schema()).unwrap();
-    for (field_name, field_value) in record.iter() {
-        record_list.put(field_name, field_value.clone());
-    }
-    writer.append(record_list)?;
-    Ok(())
+pub fn populate_record<W: Write>(
+	writer: &mut Writer<W>,
+	record: &HashMap<String, SchemaValue>,
+) -> Result<(), AvroError> {
+	let mut record_list = Record::new(writer.schema()).unwrap();
+	for (field_name, field_value) in record.iter() {
+		record_list.put(field_name, field_value.clone());
+	}
+	writer.append(record_list)?;
+	Ok(())
+}
+
+pub fn get_reader_schema<'a, R: Read>(
+	reader: R,
+	schema: &'a Schema,
+) -> Result<Reader<'a, R>, AvroError> {
+	Ok(Reader::with_schema(&schema, reader)?)
+}
+
+pub fn translate_schema(schema: Vec<u8>) -> Result<Schema, AvroError> {
+	let schema_str = str::from_utf8(&schema)?;
+	Ok(Schema::parse_str(schema_str)?)
+}
+
+pub fn translate_schemas(schema: Vec<Vec<u8>>) -> Result<Vec<Schema>, AvroError> {
+	let mut schemas = Vec::new();
+	for s in schema {
+		schemas.push(translate_schema(s)?);
+	}
+	Ok(schemas)
 }
 
 pub fn fingerprint_raw_schema(raw_schema: &str) -> Result<(Schema, Vec<u8>), AvroError> {
