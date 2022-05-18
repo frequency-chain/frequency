baa67f3ee05005085c208275ad67afdc0eafa974
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-05-17 22:56:59 -0500

    placeholder for payment tx

---
+++ common/tx_payment/src/tx_pay.rs
@@ -0,0 +1,18 @@
+use codec::{Decode, Encode};
+use frame_support::{
+	decl_module, decl_storage,
+	traits::Get,
+	weights::{DispatchClass, DispatchInfo, PostDispatchInfo},
+};
+use scale_info::TypeInfo;
+use pallet_transaction_payment::OnChargeTransaction;
+use sp_runtime::{
+	traits::{
+		DispatchInfoOf, Dispatchable, PostDispatchInfoOf, SaturatedConversion, Saturating,
+		SignedExtension,
+	},
+	transaction_validity::{
+		TransactionPriority, TransactionValidity, TransactionValidityError, ValidTransaction,
+	},
+	DispatchResult, FixedPointOperand,
+};
