c0f25401fcd3e514ff4a87194e5a7ca8fed6f1ad
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-05-19 15:43:00 -0500

    placeholder

--- pallets/tx_fee/src/lib.rs
+++ pallets/tx_fee/src/lib.rs
@@ -2,7 +2,7 @@
 
 use frame_support::weights::{DispatchInfo, GetDispatchInfo};
 use pallet_transaction_payment::{FeeDetails, OnChargeTransaction};
-use sp_runtime::traits::Dispatchable;
+use sp_runtime::{traits::Dispatchable, FixedPointOperand};
 
 #[cfg(test)]
 mod tests;
@@ -30,16 +30,31 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
+	#[pallet::error]
+	pub enum Error<T> {}
+
 	impl<T: Config> Pallet<T> {
-		pub fn compute_extrinsic_cost<
-			Extrinsic: sp_runtime::traits::Extrinsic + GetDispatchInfo,
-		>(
+		pub fn compute_extrinsic_cost<Extrinsic: sp_runtime::traits::Extrinsic + GetDispatchInfo>(
 			unchecked_extrinsic: Extrinsic,
 			len: u32,
 		) -> FeeDetails<BalanceOf<T>>
 		where
 			T::Call: Dispatchable<Info = DispatchInfo>,
+			BalanceOf<T>: Send + Sync + From<u64> + FixedPointOperand,
 		{
+			let tip = 0_u32.into();
+			let dispatch_info =
+				<Extrinsic as GetDispatchInfo>::get_dispatch_info(&unchecked_extrinsic);
+			if unchecked_extrinsic.is_signed().unwrap_or(false) {
+				<pallet_transaction_payment::Pallet<T>>::compute_fee_details(
+					len,
+					&dispatch_info,
+					tip,
+				)
+			} else {
+				// Unsigned extrinsics have no inclusion fee.
+				FeeDetails { inclusion_fee: None, tip }
+			}
 		}
 	}
 }
