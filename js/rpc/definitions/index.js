54bfdf795b9410b34ba96a50d15bf04eda3bac65
Wil Wade <wil.wade@unfinished.com>
2022-06-08 11:12:24 -0400

    Definitions for all the current custom rpcs

--- js/rpc/definitions/index.js
+++ js/rpc/definitions/index.js
@@ -1,3 +1,7 @@
 // Add a default export here for each definition
 // export { default as pallet } from "./pallet.js";
 
+export { default as msa } from "./msa.js";
+export { default as messages } from "./messages.js";
+export { default as schemas } from "./schemas.js";
+export { default as mrc } from "./mrc.js";
