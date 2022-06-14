48cc4ae8e4582661ab3ca0a7c9c12300fb77c186
mehtaishita <ishitamehta33@outlook.com>
2022-06-14 09:58:18 -0500

    replacing with serde_json - serde json core is failing to compile

--- common/helpers/src/serde.rs
+++ common/helpers/src/serde.rs
@@ -1,7 +1,8 @@
-use crate::types::*;
-use serde::serde_json_core;
+use serde_json;
+// use serde_json_core;
+// use serde::de::{Err, Error};
 
-/// Represents error types returned by the `avro` module.
+// Represents error types returned by the `serde` module.
 #[derive(thiserror::Error, Debug)]
 pub enum SerdeError {
 	#[error("Invalid JSON schema: {0}")]
@@ -10,9 +11,9 @@ pub enum SerdeError {
 	InvalidRecords(),
 }
 
-pub fn validate_JSON_schema(json_schema: &Vec<u8>) -> Result<(), SerdeError> {
-    let schema_raw = serde_json::from_slice(json_schema)?;
-    if schema_raw.is_err() {
+pub fn validate_json_schema(json_schema: Vec<u8>) -> Result<(), SerdeError> {
+    let schema_raw: Result<Vec<u8>, serde_json::Error> = serde_json::from_slice(&json_schema);
+	if schema_raw.is_err() {
 		return Err(SerdeError::InvalidSchema("Invalid schema".to_string()))
 	}
 	Ok(())
