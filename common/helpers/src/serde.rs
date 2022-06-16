d86de083847b4fb3cdf269f8ede427984100af31
mehtaishita <ishitamehta33@outlook.com>
2022-06-16 10:35:38 -0500

    fixing imports

--- common/helpers/src/serde.rs
+++ common/helpers/src/serde.rs
@@ -1,18 +1,18 @@
-use serde_json;
 // use serde_json_core;
-// use serde::de::{Err, Error};
+use serde_json::{de, ser, Error, from_slice};
 
 // Represents error types returned by the `serde` module.
-#[derive(thiserror::Error, Debug)]
+ 
+#[derive(Error, Debug)]
 pub enum SerdeError {
 	#[error("Invalid JSON schema: {0}")]
-	InvalidSchema(String),
+	InvalidSchema(str),
 	#[error("Invalid JSON records")]
 	InvalidRecords(),
 }
 
 pub fn validate_json_schema(json_schema: Vec<u8>) -> Result<(), SerdeError> {
-    let schema_raw: Result<Vec<u8>, serde_json::Error> = serde_json::from_slice(&json_schema);
+    let schema_raw: Result<Vec<u8>, serde_json::ser::Error> = serde_json::from_slice(&json_schema).unwrap();
 	if schema_raw.is_err() {
 		return Err(SerdeError::InvalidSchema("Invalid schema".to_string()))
 	}
