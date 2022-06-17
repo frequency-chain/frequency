3c4c21997a9e106bfa3d32a4685ad16ccd92970c
mehtaishita <ishitamehta33@outlook.com>
2022-06-17 16:52:15 -0500

    add more tests and change function parameter types

--- common/helpers/src/serde.rs
+++ common/helpers/src/serde.rs
@@ -2,5 +2,15 @@
 use serde_json::{Result};
 
 pub fn validate_json_schema(json_schema: Vec<u8>) -> Result<()> {
-    serde_json::from_slice(&json_schema)
+    let slice_json_schema = json_schema.as_slice();
+    println!("Sliced json schema in Serde: {:?}", slice_json_schema);
+    serde_json::from_slice(json_schema.as_slice())
+}
+
+#[test]
+fn validate_serde_helper() {
+    let test_str_raw = r#"{"name":"John Doe"}"#;
+    let result = validate_json_schema(Vec::from(test_str_raw.as_bytes()));
+    println!("Here's the result {:?}", result);
+    assert_eq!(result.unwrap(),  ());
 }
