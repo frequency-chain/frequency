b3d06a21d56748055b15e1c474373c087b0b508c
mehtaishita <ishitamehta33@outlook.com>
2022-06-29 11:35:18 -0500

    serde helpers

--- common/helpers/src/serde.rs
+++ common/helpers/src/serde.rs
@@ -1,10 +1,12 @@
 // use serde_json_core;
-use serde_json::{Result};
+use serde_json::{Result, Value, Error};
 
-pub fn validate_json_schema(json_schema: Vec<u8>) -> Result<()> {
-    let slice_json_schema = json_schema.as_slice();
-    println!("Sliced json schema in Serde: {:?}", slice_json_schema);
-    serde_json::from_slice(json_schema.as_slice())
+pub fn validate_json_schema(json_schema: Vec<u8>) -> Result<()> { // short curcuit
+    let result: Value = serde_json::from_slice(&json_schema)?;
+    match result {
+        Value::Null => Error()
+    }
+    Ok(())
 }
 
 #[test]
@@ -12,5 +14,5 @@ fn validate_serde_helper() {
     let test_str_raw = r#"{"name":"John Doe"}"#;
     let result = validate_json_schema(Vec::from(test_str_raw.as_bytes()));
     println!("Here's the result {:?}", result);
-    assert_eq!(result.unwrap(),  ());
+    assert!(result.is_ok());
 }
