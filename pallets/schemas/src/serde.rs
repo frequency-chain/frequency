7c91e94179525e9e2af8df58beeaf70f526ce41f
mehtaishita <ishitamehta33@outlook.com>
2022-07-05 11:00:10 -0500

    fixing error enum

--- pallets/schemas/src/serde.rs
+++ pallets/schemas/src/serde.rs
@@ -1,17 +1,9 @@
-// use serde_json_core::{Result, Value, from_slice, Error};
 use serde_json::{from_slice, Value};
 
-/// Represents error types returned by the `Serde` module.
-#[derive(thiserror::Error, Debug)]
+#[derive(Debug)]
 pub enum SerdeError {
-	#[error("Serde error")]
-	Serde(#[from] serde_json::Error),
-	#[error("Invalid Null schema: {0}")]
 	InvalidNullSchema(String),
-	#[error("Invalid Json schema: {0}")]
 	InvalidSchema(String),
-	#[error("Invalid Json records")]
-	InvalidRecords(),
 }
 
 pub fn validate_json_schema(json_schema: Vec<u8>) -> Result<(), SerdeError> {
@@ -36,6 +28,7 @@ fn serde_helper_invalid_schema() {
 	let result = validate_json_schema(Vec::from(bad_schema.as_bytes()));
 	assert!(result.is_err());
 }
+
 #[test]
 fn serde_helper_null_schema() {
 	let bad_schema = r#"{}"#;
