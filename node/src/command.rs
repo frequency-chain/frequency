c59c4b7ae94fa0972abd6c915c0cfe59c4fcc789
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-07-14 20:01:15 +0530

    lint

--- node/src/command.rs
+++ node/src/command.rs
@@ -27,7 +27,9 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 		"frequency_dev" => Box::new(chain_spec::frequency_local::development_config()),
 		"frequency_local" => Box::new(chain_spec::frequency_local::local_testnet_config()),
 		"" | "local" => Box::new(chain_spec::frequency_local::local_testnet_config()),
-		path => Box::new(chain_spec::frequency_local::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
+		path => Box::new(chain_spec::frequency_local::ChainSpec::from_json_file(
+			std::path::PathBuf::from(path),
+		)?),
 	})
 }
 
