c2814d709847c399c2b4d204fa2fe32ddb11c83d
dopatraman <prakash@carbonfive.com>
2022-07-27 12:03:19 -0700

    Cmd line arg for weights script

--- scripts/generate_weights.sh
+++ scripts/generate_weights.sh
@@ -3,8 +3,27 @@
 set -e
 set -x
 
+while getopts p:w: flag
+do
+    case "${flag}" in
+        p) pallet=${OPTARG};;
+        w) weights_file=./pallets/${OPTARG};;
+    esac
+done
+
+if [ -z "$pallet" ]
+  then 
+    echo "Error: No Pallet Provided" >&2;
+    exit 1;
+fi
+if [ -z "$weights_file" ]
+  then
+    weights_file="./pallets/$pallet/src/weights.rs";
+    echo "No weight file provided, using default: $weights_file"
+fi
+
 cargo build --features runtime-benchmarks --release
 
 ./target/release/frequency build-spec > ./res/genesis/frequency-weights.json
 
-./target/release/frequency benchmark pallet --chain ./res/genesis/frequency-weights.json --execution=wasm --wasm-execution=compiled --pallet pallet_messages --extrinsic '*' --steps 20 --repeat 5 --output ./pallets/messages/src/weights.rs --template=./.maintain/frame-weight-template.hbs
+./target/release/frequency benchmark pallet --chain ./res/genesis/frequency-weights.json --execution=wasm --wasm-execution=compiled --pallet pallet_$pallet --extrinsic '*' --steps 20 --repeat 5 --output $weights_file --template=./.maintain/frame-weight-template.hbs
