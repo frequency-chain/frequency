dad45cd6685c14040fb5469f08c5b3533e56f697
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-08-01 17:17:11 +0530

    lint

--- node/service/src/chain_spec/frequency_rococo.rs
+++ node/service/src/chain_spec/frequency_rococo.rs
@@ -30,7 +30,9 @@ pub fn frequency_rococo_testnet() -> ChainSpec {
 			frequency_rococo_genesis(
 				// initial collators.
 				vec![],
-				Some(hex!["a4c16e3ce8b11a0e43102bc4a718e9f031df2d0f61b1b03031b2f45ed664f853"].into()),
+				Some(
+					hex!["a4c16e3ce8b11a0e43102bc4a718e9f031df2d0f61b1b03031b2f45ed664f853"].into(),
+				),
 				vec![],
 				para_id,
 			)
