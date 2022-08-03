709e6cdd202f1a07a6a7bf0c7db2184d5bce1212
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-08-03 12:20:50 +0530

    cleanup

--- node/service/src/service.rs
+++ node/service/src/service.rs
@@ -462,12 +462,7 @@ where
 		> + sp_offchain::OffchainWorkerApi<Block>
 		+ sp_consensus_aura::AuraApi<Block, AuraId>
 		+ sp_block_builder::BlockBuilder<Block>
-		+ cumulus_primitives_core::CollectCollationInfo<Block>
-		+ pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>
-		+ substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>
-		+ pallet_messages_runtime_api::MessagesApi<Block, AccountId, BlockNumber>
-		+ pallet_schemas_runtime_api::SchemasRuntimeApi<Block>
-		+ pallet_msa_runtime_api::MsaApi<Block, AccountId, BlockNumber>,
+		+ cumulus_primitives_core::CollectCollationInfo<Block>,
 	Executor: sc_executor::NativeExecutionDispatch + 'static,
 	sc_client_api::StateBackendFor<TFullBackend<Block>, Block>: sp_api::StateBackend<BlakeTwo256>,
 {
