46630b9e55a2286a95a0bb15395ae12725cc2618
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-08-09 00:32:04 +0530

    revert

--- runtime/frequency-rococo/src/lib.rs
+++ runtime/frequency-rococo/src/lib.rs
@@ -492,10 +492,10 @@ impl pallet_aura::Config for Runtime {
 
 parameter_types! {
 	pub const PotId: PalletId = PalletId(*b"PotStake");
-	pub const MaxCandidates: u32 = 1000;
-	pub const MinCandidates: u32 = 5;
+	pub const MaxCandidates: u32 = 0;
+	pub const MinCandidates: u32 = 0;
 	pub const SessionLength: BlockNumber = 6 * HOURS;
-	pub const MaxInvulnerables: u32 = 100;
+	pub const MaxInvulnerables: u32 = 2;
 	pub const ExecutiveBody: BodyId = BodyId::Executive;
 }
 
