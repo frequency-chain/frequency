d2c75a2be1710a4899f837e3404375957388cc1c
Prakash Venkatraman <pvenkatraman@westmonroe.com>
2022-09-19 14:45:36 -0700

    Automation for generating rococo local spec file

--- scripts/generate_relay_specs.sh
+++ scripts/generate_relay_specs.sh
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 #!/usr/bin/env bash
 
 set -e
 
 docker run parity/polkadot:v0.9.27 build-spec --disable-default-bootnode --chain rococo-local --raw > ./resources/rococo-local.json
 
+||||||| parent of 3abc4b7 (Automation for generating rococo local spec file)
+=======
+#!/usr/bin/env bash
+
+set -e
+
+echo $(pwd)
+
+docker run parity/polkadot:v0.9.27 build-spec --disable-default-bootnode --chain rococo-local --raw > ./resources/rococo-local.json
+>>>>>>> 3abc4b7 (Automation for generating rococo local spec file)
