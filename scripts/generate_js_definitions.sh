1f97d578c188bef09216eef86c1995312a0203af
Prakash Venkatraman <pvenkatraman@westmonroe.com>
2022-09-22 14:12:04 -0700

    Check frequency port before starting js generation

--- scripts/generate_js_definitions.sh
+++ scripts/generate_js_definitions.sh
@@ -1,28 +1,37 @@
 #!/bin/zsh
 
-# ! Please make sure to have both docker and docker-compose installed
-
 set -e
 
 STEP_COLOR='\033[4;36m'    # Cyan Underline
+MESSAGE='\033[0;33m'       # Yellow
 BOLD_MESSAGE='\033[1;33m'  # Bold Yellow
 SUCCESS='\033[0;92m'       # Green
 NC='\033[0m'               # No Color
 
-echo "\n${STEP_COLOR}Starting the Frequency container...${NC}"
-
-make start > /dev/null 2>&1 &
+echo "${STEP_COLOR}Checking to see if Frequency is running...${NC}"
+echo ""
+echo "${MESSAGE}Is Frequency running?${NC}"
 
 PID=$(lsof -i tcp:9933 | grep frequency | grep -v grep | xargs | awk '{print $2}')
 
-# Poll for PID
+if [ -z "$PID" ]
+then
+    echo "${MESSAGE}"
+    echo "No."
+    echo "${NC}"
+    echo "${STEP_COLOR}Starting Frequency in Instant Sealing Mode...${NC}"
+    make start > /dev/null 2>&1 &
 
-while [ -z "$PID" ]
-do
-    PID=$(lsof -i tcp:9933 | grep frequency | grep -v grep | xargs | awk '{print $2}')
-done
-
-echo "${SUCCESS}"
+    # Poll for PID
+    while [ -z "$PID" ]
+    do
+        PID=$(lsof -i tcp:9933 | grep frequency | grep -v grep | xargs | awk '{print $2}')
+    done
+else
+    echo "${SUCCESS}"
+    echo "Yes. ( You better go catch it ;-) )"
+fi
+echo ""
 echo "---------------------------------------------"
 echo "Use this PID to kill the Frequency process:"
 echo "${BOLD_MESSAGE}PID: ${PID}${SUCCESS}"
@@ -30,6 +39,6 @@ echo "---------------------------------------------"
 echo "${NC}"
 
 # cd into js dir
-cd js/api-augment
+cd "js/api-augment"
 
 npm run fetch:local && npm run build
