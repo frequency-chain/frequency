3ac7ecfefc3f5baddbc387af7fa2721f52f6ed51
Wil Wade <wil.wade@unfinished.com>
2022-10-06 18:37:52 +0000

    Build out get_messages_by_schema rpc logic

--- pallets/messages/src/rpc/src/lib.rs
+++ pallets/messages/src/rpc/src/lib.rs
@@ -1,18 +1,23 @@
 use codec::Codec;
+#[cfg(feature = "std")]
 use common_helpers::rpc::*;
 use common_primitives::{messages::*, schema::*};
+use frame_support::{ensure, fail};
 use jsonrpsee::{
-	core::{async_trait, RpcResult},
+	core::{async_trait, error::Error as RpcError, RpcResult},
 	proc_macros::rpc,
 };
 use pallet_messages_runtime_api::MessagesApi as MessagesRuntimeApi;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
-use sp_runtime::{generic::BlockId, traits::Block as BlockT};
+use sp_runtime::{
+	generic::BlockId,
+	traits::{AtLeast32BitUnsigned, Block as BlockT},
+};
 use std::sync::Arc;
 
 #[rpc(client, server)]
-pub trait MessagesApi<BlockHash, BlockNumber> {
+pub trait MessagesApi<BlockNumber> {
 	#[method(name = "messages_getBySchema")]
 	fn get_messages_by_schema(
 		&self,
@@ -34,25 +39,88 @@ impl<C, M> MessagesHandler<C, M> {
 	}
 }
 
+#[derive(Debug)]
+pub enum MessageRpcError {
+	/// Pagination request is bad
+	InvalidPaginationRequest,
+	/// Likely passed u32 block count
+	TypeConversionOverflow,
+	/// Schema Id doesn't exist or errored when retrieving from state
+	InvalidSchemaId,
+	/// Something errored when trying to get the messages from state
+	MessageRetrievalFailure,
+}
+
+impl From<MessageRpcError> for RpcError {
+	fn from(e: MessageRpcError) -> Self {
+		RpcError::Custom(format!("{:?}", e))
+	}
+}
+
 #[async_trait]
-impl<C, Block, BlockNumber> MessagesApiServer<<Block as BlockT>::Hash, BlockNumber>
-	for MessagesHandler<C, Block>
+impl<C, Block, BlockNumber> MessagesApiServer<BlockNumber> for MessagesHandler<C, Block>
 where
 	Block: BlockT,
-	C: Send + Sync + 'static,
-	C: ProvideRuntimeApi<Block>,
-	C: HeaderBackend<Block>,
+	C: ProvideRuntimeApi<Block> + HeaderBackend<Block> + 'static,
 	C::Api: MessagesRuntimeApi<Block, BlockNumber>,
-	BlockNumber: Codec,
+	BlockNumber: Codec + Copy + AtLeast32BitUnsigned,
 {
 	fn get_messages_by_schema(
 		&self,
 		schema_id: SchemaId,
 		pagination: BlockPaginationRequest<BlockNumber>,
 	) -> RpcResult<BlockPaginationResponse<BlockNumber, MessageResponse<BlockNumber>>> {
+		// Request Validation
+		ensure!(pagination.validate(), MessageRpcError::InvalidPaginationRequest);
+
+		// Connect to on-chain data
 		let api = self.client.runtime_api();
 		let at = BlockId::hash(self.client.info().best_hash);
-		let runtime_api_result = api.get_messages_by_schema(&at, schema_id, pagination);
-		map_rpc_result(runtime_api_result)
+
+		// Schema Fetch and Check
+		let schema: SchemaResponse = match api.get_schema_by_id(&at, schema_id) {
+			Ok(Some(s)) => s,
+			_ => fail!(MessageRpcError::InvalidSchemaId),
+		};
+
+		let mut response = BlockPaginationResponse::new();
+		let from: u32 = pagination
+			.from_block
+			.try_into()
+			.map_err(|_| MessageRpcError::TypeConversionOverflow)?;
+		let to: u32 = pagination
+			.to_block
+			.try_into()
+			.map_err(|_| MessageRpcError::TypeConversionOverflow)?;
+		let mut from_index = pagination.from_index;
+
+		'loops: for bid in from..to {
+			let block_number: BlockNumber = bid.into();
+			let list: Vec<MessageResponse<BlockNumber>> =
+				match api.get_messages_by_schema_and_block(&at, schema.schema_id, schema.payload_location, block_number) {
+					Ok(x) => x,
+					_ => return fail!(MessageRpcError::MessageRetrievalFailure),
+				};
+
+			let list_size: u32 =
+				list.len().try_into().map_err(|_| MessageRpcError::TypeConversionOverflow)?;
+			for i in from_index..list_size {
+				response.content.push(list[i as usize].clone());
+
+				if response.check_end_condition_and_set_next_pagination(
+					block_number,
+					i,
+					list_size,
+					&pagination,
+				) {
+					break 'loops
+				}
+			}
+
+			// next block starts from 0
+			from_index = 0;
+		}
+
+		map_rpc_result(Ok(Ok(response)))
 	}
 }
