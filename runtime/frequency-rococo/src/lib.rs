3904f41721cef2a00b656a0cdda7c53105a0ed2f
John Mela <john.mela@unfinished.com>
2022-10-06 10:56:59 -0400

    Add migration to frame_executive::Executive to indicate it should be executed in runtime

--- runtime/frequency-rococo/src/lib.rs
+++ runtime/frequency-rococo/src/lib.rs
@@ -133,7 +133,12 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
+	Migrations,
 >;
+
+// All migrations executed on runtime upgrade as a nested tuple of types implementing
+// `OnRuntimeUpgrade`.
+type Migrations = (pallet_msa::migrations::v1::MigrateToV1<Runtime>,);
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
