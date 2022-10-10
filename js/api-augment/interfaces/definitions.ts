e1e62b1e44eeb05e0beb4f41b686a3c1fa8a9174
Wil Wade <wil.wade@unfinished.com>
2022-10-10 00:04:23 +0000

    Better setup for JS pre-generation and cleaning of generated files

--- js/api-augment/interfaces/definitions.ts
+++ js/api-augment/interfaces/definitions.ts
@@ -1,6 +1,1 @@
-// Add a default export here for each definition
-// export { default as pallet } from "./definitions/pallet";
-
-export { default as msa } from "../definitions/msa";
-export { default as messages } from "../definitions/messages";
-export { default as schemas } from "../definitions/schemas";
+export * from "../definitions";
