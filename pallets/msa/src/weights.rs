9e0fe5a8a4a5353f437ca3eb40f378d5f2334ac5
Prakash Venkatraman <prakash@carbonfive.com>
2022-10-21 08:36:46 -0700

    Rename: delete_msa_key -> delete_msa_public_key (#543)
    # Goal
    The goal of this PR is to rename the `delete_msa_key` extrinsic.
    
    # Discussion
    This PR partially completes #508.
    
    Co-authored-by: Prakash Venkatraman <pvenkatraman@westmonroe.com>

--- pallets/msa/src/weights.rs
+++ pallets/msa/src/weights.rs
@@ -58,7 +58,7 @@ pub trait WeightInfo {
 	fn create_sponsored_account_with_delegation() -> Weight;
 	fn revoke_delegation_by_provider(s: u32) -> Weight;
 	fn add_key_to_msa() -> Weight;
-	fn delete_msa_key() -> Weight;
+	fn delete_msa_public_key() -> Weight;
 	fn retire_msa() -> Weight;
 	fn grant_delegation() -> Weight;
 	fn revoke_msa_delegation_by_delegator() -> Weight;
@@ -110,7 +110,7 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 	}
 	// Storage: Msa PublicKeyToMsaId (r:2 w:1)
 	// Storage: Msa PublicKeyCountForMsaId (r:1 w:1)
-	fn delete_msa_key() -> Weight {
+	fn delete_msa_public_key() -> Weight {
 		Weight::from_ref_time(18_000_000 as u64)
 			.saturating_add(T::DbWeight::get().reads(3 as u64))
 			.saturating_add(T::DbWeight::get().writes(2 as u64))
@@ -196,7 +196,7 @@ impl WeightInfo for () {
 	}
 	// Storage: Msa PublicKeyToMsaId (r:2 w:1)
 	// Storage: Msa PublicKeyCountForMsaId (r:1 w:1)
-	fn delete_msa_key() -> Weight {
+	fn delete_msa_public_key() -> Weight {
 		Weight::from_ref_time(18_000_000 as u64)
 			.saturating_add(RocksDbWeight::get().reads(3 as u64))
 			.saturating_add(RocksDbWeight::get().writes(2 as u64))
