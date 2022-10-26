29fadf9f666725ab2520c690ba5feaff5249a30b
Wil Wade <wil.wade@unfinished.com>
2022-10-26 13:23:53 -0400

    Improve Code Coverage (#570)
    # Goal
    The goal of this PR is to improve code coverage
    
    Closes #564 
    
    # Discussion
    - Removed `tests.rs` and `benchmarking.rs` files from `codecov` as we
    don't care about the coverage of those files
    - Cleaned up the tests in `serde.rs`
    - Add tests around serialization utils
    - Add basic tests around rpc result mapping
    - Added coverage reporting back into PR merged workflow

--- common/helpers/src/rpc.rs
+++ common/helpers/src/rpc.rs
@@ -35,3 +35,42 @@ pub fn map_rpc_result<T>(
 		)))),
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	// It is enough to test that we are getting into the correct match statement.
+
+	#[test]
+	fn maps_ok_ok_to_ok() {
+		let result = map_rpc_result(Ok(Ok(0)));
+		assert!(result.is_ok());
+		assert_eq!(0, result.unwrap());
+	}
+
+	#[test]
+	fn maps_ok_dispatch_err() {
+		let dispach_err = sp_runtime::ModuleError { index: 0, error: [0, 0, 0, 0], message: None };
+		let result = map_rpc_result::<u64>(Ok(Err(sp_runtime::DispatchError::Module(dispach_err))));
+		assert!(result.is_err());
+		let str = format!("{:?}", result.err().unwrap());
+		assert!(str.contains("Dispatch Module Error"), "Did not find in: {:?}", str);
+	}
+
+	#[test]
+	fn maps_ok_dispatch_err_to_call_err() {
+		let result = map_rpc_result::<u64>(Ok(Err(sp_runtime::DispatchError::Other("test"))));
+		assert!(result.is_err());
+		let str = format!("{:?}", result.err().unwrap());
+		assert!(str.contains("Dispatch Error"), "Did not find in: {:?}", str);
+	}
+
+	#[test]
+	fn maps_err_to_api_err() {
+		let result = map_rpc_result::<u64>(Err(ApiError::StateBackendIsNotTrie));
+		assert!(result.is_err());
+		let str = format!("{:?}", result.err().unwrap());
+		assert!(str.contains("Api Error"), "Did not find in: {:?}", str);
+	}
+}
