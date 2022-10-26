4230a4197c7f1640cd48e629ac297200c78d1fa6
Wil Wade <wil.wade@unfinished.com>
2022-10-26 19:57:30 +0000

    Runtime API Documentation

--- pallets/README.md
+++ pallets/README.md
@@ -2,13 +2,18 @@
 
 ## Add Documentation Lints
 
-In `src/lib.rs` add these lines:
+In these files:
+- `src/lib.rs`
+- `src/runtime-api/src/lib.rs`
+
+Add these lines:
 
 ```rust
 // Strong Documentation Lints
 #![deny(
     rustdoc::broken_intra_doc_links,
     rustdoc::missing_crate_level_docs,
-    rustdoc::invalid_codeblock_attributes
+    rustdoc::invalid_codeblock_attributes,
+    missing_docs
 )]
 ```
--- pallets/messages/src/runtime-api/src/lib.rs
+++ pallets/messages/src/runtime-api/src/lib.rs
@@ -1,20 +1,38 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::unnecessary_mut_passed)]
+// Strong Documentation Lints
+#![deny(
+	rustdoc::broken_intra_doc_links,
+	rustdoc::missing_crate_level_docs,
+	rustdoc::invalid_codeblock_attributes,
+	missing_docs
+)]
+
+//! Runtime API definition for [Messages](pallet-messages)
+//!
+//! This api must be implemented by the node runtime.
+//! Runtime APIs Provide:
+//! - An interface between the runtime and Custom RPCs.
+//! - Runtime interfaces for end users beyond just State Queries
 
 use codec::Codec;
 use common_primitives::{messages::*, schema::*};
 use frame_support::inherent::Vec;
 
 // Here we declare the runtime API. It is implemented it the `impl` block in
-// runtime file (the `runtime/src/lib.rs`)
+// runtime files (the `runtime` folder)
 sp_api::decl_runtime_apis! {
+
+	/// Runtime APIs for [Messages](pallet-messages)
 	pub trait MessagesRuntimeApi<BlockNumber> where
 		BlockNumber: Codec,
 	{
+		/// Retrieve the messages for a particular schema and block number
 		fn get_messages_by_schema_and_block(schema_id: SchemaId, schema_payload_location: PayloadLocation, block_number: BlockNumber) ->
 			Vec<MessageResponse<BlockNumber>>;
 
+		/// Retrieve a schema by id
 		fn get_schema_by_id(schema_id: SchemaId) -> Option<SchemaResponse>;
 	}
 }
