a8aff2904bc3173606f63aa258ed36686a7bdc19
Wil Wade <wil.wade@unfinished.com>
2022-10-26 18:18:08 -0400

    Rename/runtime api standardization (#580)
    # Goal
    The goal of this PR is to make our code less confusing around our APIs
    by aligning our naming for runtime api traits.
    
    Part of #255 
    
    # Discussion
    - No external or breaking changes
    - `pallet_msa_runtime_api::MsaApi` ->
    `pallet_msa_runtime_api::MsaRuntimeApi`
    - `pallet_messages_runtime_api::MessagesApi` ->
    `pallet_messages_runtime_api::MessagesRuntimeApi`
    
    # Checklist
    - [x] No Breaking renaming changes

--- pallets/messages/src/runtime-api/src/lib.rs
+++ pallets/messages/src/runtime-api/src/lib.rs
@@ -9,7 +9,7 @@ use frame_support::inherent::Vec;
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime file (the `runtime/src/lib.rs`)
 sp_api::decl_runtime_apis! {
-	pub trait MessagesApi<BlockNumber> where
+	pub trait MessagesRuntimeApi<BlockNumber> where
 		BlockNumber: Codec,
 	{
 		fn get_messages_by_schema_and_block(schema_id: SchemaId, schema_payload_location: PayloadLocation, block_number: BlockNumber) ->
--- pallets/msa/src/runtime-api/src/lib.rs
+++ pallets/msa/src/runtime-api/src/lib.rs
@@ -10,7 +10,7 @@ use sp_std::vec::Vec;
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime file (the `runtime/src/lib.rs`)
 sp_api::decl_runtime_apis! {
-	pub trait MsaApi<AccountId> where
+	pub trait MsaRuntimeApi<AccountId> where
 		AccountId: Codec,
 	{
 		// *Temporarily Removed* until https://github.com/LibertyDSNP/frequency/issues/418 is completed
