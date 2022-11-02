09229d483ef6c5f9d6db7b4d63c5d1ba9358be69
Wil Wade <wil.wade@unfinished.com>
2022-11-02 14:47:48 -0400

    Small Updates (#612)
    # Goal
    The goal of this PR is several small updates.
    
    Part of #255 and #339

--- scripts/generate_js_definitions.sh
+++ scripts/generate_js_definitions.sh
@@ -8,32 +8,32 @@ BOLD_MESSAGE='\033[1;33m'  # Bold Yellow
 SUCCESS='\033[0;92m'       # Green
 NC='\033[0m'               # No Color
 
-echo "${STEP_COLOR}Checking to see if Frequency is running...${NC}"
+echo -e "${STEP_COLOR}Checking to see if Frequency is running...${NC}"
 echo ""
-echo "${MESSAGE}Is Frequency running?${NC}"
+echo -e "${MESSAGE}Is Frequency running?${NC}"
 
 PID=$(lsof -i tcp:9933 | grep frequency | grep -v grep | xargs | awk '{print $2}')
 
 if [ -z "$PID" ]
 then
-    echo "${MESSAGE}"
+    echo -e "${MESSAGE}"
     echo "No."
-    echo "${NC}"
-    echo "${STEP_COLOR}Generating using CLI...${NC}"
+    echo -e "${NC}"
+    echo -e "${STEP_COLOR}Generating using CLI...${NC}"
     rm -f ./js/api-augment/metadata.json
     cargo run --features frequency export-metadata > ./js/api-augment/metadata.json
     # cd into js dir
     cd "js/api-augment"
     npm run build
 else
-    echo "${SUCCESS}"
+    echo -e "${SUCCESS}"
     echo "Yes. ( You better go catch it ;-) )"
     echo ""
     echo "---------------------------------------------"
     echo "Use this PID to kill the Frequency process:"
-    echo "${BOLD_MESSAGE}PID: ${PID}${SUCCESS}"
+    echo -e "${BOLD_MESSAGE}PID: ${PID}${SUCCESS}"
     echo "---------------------------------------------"
-    echo "${NC}"
+    echo -e "${NC}"
 
     # cd into js dir
     cd "js/api-augment"
