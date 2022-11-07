a28abaa3dca0b4919ae0229672932296cdce5568
Wil Wade <wil.wade@unfinished.com>
2022-11-06 20:21:09 -0500

    Clean up tests

--- js/api-augment/test/index.test.ts
+++ js/api-augment/test/index.test.ts
@@ -1,34 +1,34 @@
 import assert from "assert";
-import { options } from "../index";
+import { options, rpc } from "../index";
 import { ApiPromise } from "@polkadot/api";
 import { MockProvider } from "@polkadot/rpc-provider/mock";
 import { Metadata, TypeRegistry } from "@polkadot/types";
-import { result as rpcMetadata } from "../metadata.json";
+import metadataRaw from "../metadata.json";
 
 describe("index", function () {
-  const registry = new TypeRegistry();
-  registry.register(options.types);
-  const metadata = new Metadata(registry, rpcMetadata as "0x{string}");
-  registry.setMetadata(metadata);
-
   let mock: MockProvider;
+  let api: ApiPromise;
 
-  beforeEach(function (): void {
-    mock = new MockProvider(new TypeRegistry());
+  beforeEach(async function () {
+    const registry = new TypeRegistry();
+    registry.register(options.types);
+    mock = new MockProvider(registry);
+
+    api = await ApiPromise.create({
+      ...options,
+      provider: mock,
+      metadata: metadataRaw as any,
+    });
   });
 
   afterEach(async function () {
+    await api.disconnect();
     await mock.disconnect();
   });
 
   it("should have rpc calls", async function () {
-    const api = await ApiPromise.create({
-      ...options,
-      provider: mock,
-    });
     assert.notEqual(api.rpc.messages, undefined);
     assert.notEqual(api.rpc.msa, undefined);
     assert.notEqual(api.rpc.schemas, undefined);
-    await api.disconnect();
   });
 });
