3d3e18bfa3b7c3000d47f7d8ea5c2a8d4fdadef3
demisx <demisx@users.noreply.github.com>
2022-11-09 17:07:46 +0000

    Update CHANGELOG

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## v0.9.29-rc1 - 2022-10-22
-
--   Initial Release
--- tools/ci/release-notes/build-release-notes.sh
+++ tools/ci/release-notes/build-release-notes.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-set -ex
+# set -ex
 # echo "arg 1: $1"
 # echo "arg 2: $2"
+
+# sanitized_change_log=${$1//\`/}
+# echo "sanitized change_log:$sanitized_change_log"
+changelog=$1
 
 # args=("$@")
 # echo "args: ${args[@]}"
@@ -12,4 +16,6 @@ set -ex
 # frequency_ref2 = $1
 # frequency_ref1 = $2 || 'UNKNOWN'
 
-tera --env --env-key env --template release-notes.md.tera context.json
+# tera --env --env-key env --template release-notes.md.tera context.json
+# tera --env --env-key env --env-first --template release-notes.md.tera
+CHANGELOG="$changelog" tera --env --env-key env --env-only --template release-notes.md.tera
