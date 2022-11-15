16d5217f62a7bd9827124799212b34749d4f0a15
Prakash Venkatraman <pvenkatraman@westmonroe.com>
2022-11-15 14:23:28 -0800

    Parameterized WS provider url

--- integration-tests/schemas/scaffolding/apiConnection.ts
+++ integration-tests/schemas/scaffolding/apiConnection.ts
@@ -3,8 +3,12 @@ import { ApiRx, WsProvider, Keyring } from "@polkadot/api";
 import { KeyringPair } from "@polkadot/keyring/types";
 import { firstValueFrom } from "rxjs";
 
-export async function connect(): Promise<{api: ApiRx, keys: KeyringPair}> {
-    const provider = new WsProvider("ws://127.0.0.1:9944");
+export async function connect(providerUrl): Promise<{api: ApiRx, keys: KeyringPair}> {
+    let url = providerUrl;
+    if (!providerUrl) {
+        url = "ws://127.0.0.1:9944"
+    }
+    const provider = new WsProvider(url);
     const apiObservable = ApiRx.create({ provider, ...options });
     const keyring = new Keyring({ type: "sr25519" });
     const keys = keyring.addFromUri("//Alice");
