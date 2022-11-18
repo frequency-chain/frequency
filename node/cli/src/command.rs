f753143d91a3c6f139eaf92ccdc3d2b625094f76
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-11-18 15:34:17 -0600

    nice approach

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -476,12 +476,13 @@ pub fn run() -> Result<()> {
 			with_runtime_or_err!(chain_spec, {
 				{
 					runner.async_run(|config| {
+						let version = Cli::native_runtime_version(&config.chain_spec);
 						// grab the task manager.
 						let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
 						let task_manager =
 							sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
 								.map_err(|e| format!("Error: {:?}", e))?;
-						Ok((cmd.run(), task_manager))
+						Ok((cmd.run(version), task_manager))
 					})
 				}
 			})
--- node/cli/src/runtime_version_cmd.rs
+++ node/cli/src/runtime_version_cmd.rs
@@ -17,27 +17,13 @@ pub struct ExportRuntimeVersionCmd {
 
 impl ExportRuntimeVersionCmd {
 	/// Run the get-runtime-version command.
-	pub async fn run(&self) -> Result<(), Error> {
-		let runtime_version: RuntimeVersion = self.read_runtime_version().unwrap();
+	pub async fn run(&self, runtime_version: &RuntimeVersion) -> Result<(), Error> {
 		let result = json!(runtime_version);
 		let file: Box<dyn io::Write> = match &self.output {
 			Some(filename) => Box::new(fs::File::create(filename)?),
 			None => Box::new(io::stdout()),
 		};
 		to_writer(file, &result).map_err(|_| Error::from("Failed Encoding"))
-	}
-
-	#[allow(unreachable_code)]
-	pub fn read_runtime_version(&self) -> Result<RuntimeVersion, Error> {
-		#[cfg(feature = "frequency")]
-		{
-			return Ok(frequency_service::service::frequency_runtime::VERSION)
-		}
-		#[cfg(any(feature = "frequency-rococo-testnet", feature = "frequency-rococo-local"))]
-		{
-			return Ok(frequency_service::service::frequency_rococo_runtime::VERSION)
-		}
-		return Err(Error::from("No runtime version available"))
 	}
 }
 
