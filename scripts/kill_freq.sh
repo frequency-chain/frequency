d7bb369e53629b7a3977ceba9049307648f6e81e
Prakash Venkatraman <pvenkatraman@westmonroe.com>
2022-11-29 11:57:20 -0500

    Added skull emoji

--- scripts/kill_freq.sh
+++ scripts/kill_freq.sh
@@ -7,5 +7,5 @@ PID="$(ps aux | grep target/release/frequency | grep -v grep | xargs | awk '{pri
 if ! [ -z $PID ]
 then
     kill -9 $PID
-    echo "Frequency is dead."
+    echo "Frequency is dead 💀"
 fi
