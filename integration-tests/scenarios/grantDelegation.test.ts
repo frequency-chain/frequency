440ad4781f78fe1b3e8f6d64898d86921c734ba7
Prakash Venkatraman <prakash@carbonfive.com>
2022-11-30 10:44:21 -0800

    MSA Integration Tests Part 1 (#718)
    # Goal
    The goal of this PR is to test an MSA workflow that involves:
    1. Creating an MSA
    2. Adding a public key
    3. Deleting a public key
    4. Creating a Schema
    5. Retiring the MSA
    6. Creating a provider
    7. Granting a delegation
    8. Revoking a delegation
    
    Closes #695
    
    ## Notes
    This PR introduces a `scenarios/` dir that contains cross-pallet tests.
    If you want to change this structure, leave a note.
    
    # Checklist
    - [x] Tests added
    
    Authored-by: Prakash Venkatraman <pvenkatraman@westmonroe.com>

---
+++ integration-tests/scenarios/grantDelegation.test.ts
@@ -0,0 +1,140 @@
+import "@frequency-chain/api-augment";
+import { ApiRx } from "@polkadot/api";
+import { KeyringPair } from "@polkadot/keyring/types";
+import assert from "assert";
+import { connect, createKeys } from "../scaffolding/apiConnection";
+import { createMsa, createProvider, createSchema, grantDelegation, revokeDelegationByDelegator } from "../scaffolding/extrinsicHelpers";
+import { signPayloadSr25519 } from "../scaffolding/helpers";
+
+describe("Delegation Scenario Tests", () => {
+    let api: ApiRx;
+    let keys: KeyringPair;
+
+    before(async () => {
+        let connectApi = await connect(process.env.WS_PROVIDER_URL);
+        keys = createKeys("//Alice")
+        api = connectApi
+    })
+
+    after(() => {
+        api.disconnect()
+    })
+
+    it("should grant permissions to a provider for a specific set of schemas", async () => {
+        const providerKeys = createKeys("//Charlie");
+        await createMsa(api, keys);
+        await createMsa(api, providerKeys);
+
+        let createSchemaEvents = await createSchema(api, keys, {
+            type: "record",
+            name: "Post",
+            fields: [
+                {
+                    name: "title",
+                    type: {
+                        name: "Title",
+                        type: "string"
+                    }
+                },
+                {
+                    name: "content",
+                    type: {
+                        name: "Content",
+                        type: "string"
+                    }
+                },
+                {
+                    name: "fromId",
+                    type: {
+                        name: "DSNPId",
+                        type: "fixed",
+                        size: 8,
+                    },
+                },
+                {
+                    name: "objectId",
+                    type: "DSNPId",
+                },
+            ]
+        }, "AvroBinary", "OnChain");
+
+        assert.notEqual(createSchemaEvents["schemas.SchemaCreated"], undefined);
+
+        let createProviderEvents = await createProvider(api, providerKeys, "MyPoster");
+        assert.notEqual(createProviderEvents["msa.ProviderCreated"], undefined);
+
+        const schemaId = createSchemaEvents["schemas.SchemaCreated"][1];
+        const providerId = createProviderEvents["msa.ProviderCreated"][0];
+
+        const payload = {
+            authorizedMsaId: providerId,
+            schemaIds: [schemaId],
+            // If we ever have tests that exceed Block 24, this test will start failing
+            expiration: 24
+        }
+        const addProviderData = api.registry.createType("PalletMsaAddProvider", payload); 
+
+        const grantDelegationEvents = await grantDelegation(api, keys, providerKeys, signPayloadSr25519(keys, addProviderData), payload)
+        assert.notEqual(grantDelegationEvents["msa.DelegationGranted"], undefined);
+    }).timeout(15000);
+
+    it("should revoke a delegation by delegator", async () => {
+        const providerKeys = createKeys("//Dave");
+        await createMsa(api, keys);
+        await createMsa(api, providerKeys);
+
+        let createSchemaEvents = await createSchema(api, keys, {
+            type: "record",
+            name: "Post",
+            fields: [
+                {
+                    name: "title",
+                    type: {
+                        name: "Title",
+                        type: "string"
+                    }
+                },
+                {
+                    name: "content",
+                    type: {
+                        name: "Content",
+                        type: "string"
+                    }
+                },
+                {
+                    name: "fromId",
+                    type: {
+                        name: "DSNPId",
+                        type: "fixed",
+                        size: 8,
+                    },
+                },
+                {
+                    name: "objectId",
+                    type: "DSNPId",
+                },
+            ]
+        }, "AvroBinary", "OnChain");
+
+        assert.notEqual(createSchemaEvents["schemas.SchemaCreated"], undefined);
+
+        let createProviderEvents = await createProvider(api, providerKeys, "MyPoster");
+        assert.notEqual(createProviderEvents["msa.ProviderCreated"], undefined);
+
+        const schemaId = createSchemaEvents["schemas.SchemaCreated"][1];
+        const providerId = createProviderEvents["msa.ProviderCreated"][0];
+
+        const payload = {
+            authorizedMsaId: providerId,
+            schemaIds: [schemaId],
+            expiration: 36
+        }
+        const addProviderData = api.registry.createType("PalletMsaAddProvider", payload); 
+
+        const grantDelegationEvents = await grantDelegation(api, keys, providerKeys, signPayloadSr25519(keys, addProviderData), payload)
+        assert.notEqual(grantDelegationEvents["msa.DelegationGranted"], undefined);
+
+        const revokeDelegationEvents = await revokeDelegationByDelegator(api, keys, providerId)
+        assert.notEqual(revokeDelegationEvents["msa.DelegationRevoked"], undefined);
+    }).timeout(15000);
+})
--- integration-tests/schemas/createSchema.test.ts
+++ integration-tests/schemas/createSchema.test.ts
@@ -1,22 +1,21 @@
 import "@frequency-chain/api-augment";
 import { ApiRx } from "@polkadot/api";
-import { connect } from "./scaffolding/apiConnection"
+import { connect, createKeys } from "../scaffolding/apiConnection"
 
 import assert from "assert";
 
 import { AVRO_GRAPH_CHANGE } from "./fixtures/avroGraphChangeSchemaType";
-import { filter, firstValueFrom } from "rxjs";
-import { groupEventsByKey } from "./scaffolding/helpers";
 import { KeyringPair } from "@polkadot/keyring/types";
+import { createSchema } from "../scaffolding/extrinsicHelpers";
 
 describe("#createSchema", () => {
     let api: ApiRx;
     let keys: KeyringPair;
 
     before(async () => {
-        let {api: connectApi, keys: connectKeys} = await connect(process.env.WS_PROVIDER_URL);
+        let connectApi = await connect(process.env.WS_PROVIDER_URL);
+        keys = createKeys("//Alice")
         api = connectApi
-        keys = connectKeys
     })
 
     after(() => {
@@ -24,9 +23,7 @@ describe("#createSchema", () => {
     })
 
     it("should successfully create an Avro GraphChange schema", async () => {
-        const chainEvents = await firstValueFrom(api.tx.schemas.createSchema(JSON.stringify(AVRO_GRAPH_CHANGE), "AvroBinary", "OnChain").signAndSend(keys).pipe(
-                filter(({status}) => status.isInBlock || status.isFinalized),
-                groupEventsByKey()))
+        const chainEvents = await createSchema(api, keys, AVRO_GRAPH_CHANGE, "AvroBinary", "OnChain")
 
         assert.equal(chainEvents["system.ExtrinsicFailed"], undefined);
         assert.notEqual(chainEvents["system.ExtrinsicSuccess"], undefined);
