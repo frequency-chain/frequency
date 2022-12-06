3918024e00a1f7ae1c3418395a4c22f0d8ce3604
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-12-06 13:59:41 -0600

    add new weight v2 requirements

--- runtime/common/src/constants.rs
+++ runtime/common/src/constants.rs
@@ -96,8 +96,9 @@ pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
 pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
 
 /// We allow for 0.5 of a second of compute with a 12 second average block time.
-pub const MAXIMUM_BLOCK_WEIGHT: Weight = WEIGHT_PER_SECOND.saturating_div(2);
-
+pub const MAXIMUM_BLOCK_WEIGHT: Weight = WEIGHT_PER_SECOND
+	.saturating_div(2)
+	.set_proof_size(cumulus_primitives_core::relay_chain::v2::MAX_POV_SIZE as u64);
 pub type ZERO = ConstU32<0>;
 pub type FIFTY = ConstU32<50>;
 pub type HUNDRED = ConstU32<100>;
