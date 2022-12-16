2b67e8dcb165a28ffc001910e3a963639d6ec4ee
Dmitri <4452412+demisx@users.noreply.github.com>
2022-12-15 19:08:58 -0800

    add metadata compare artifact to a release

---
+++ tools/scripts/pallet-info.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  cat <<-EOF
+Usage: $0 <websocket.node>,
+  where:
+    <websocket.node> is a node running on websocket port [wss|ws://websocket_provider:port]
+      For example ws://127.0.0.1:9944
+EOF
+
+  exit 1
+fi
+
+
+echo "checking for 🏭 subwasm and installing if needed..."
+which subwasm || cargo install --locked --git https://github.com/chevdor/subwasm --tag v0.18.0
+
+ws_provider=$1
+
+pallets=$(subwasm metadata "${ws_provider}")
+
+echo "----Pallets----"
+echo "${pallets}"
+echo ""
+
+regex_for_pallet='^ - .*'
+while IFS= read -r pallet; do
+  if [[ $pallet =~ $regex_for_pallet ]];
+  then
+    pallet_name=$(echo $pallet | cut -d " " -f 3)
+    echo "----Pallet ${pallet_name}----"
+    pallet_data=$(subwasm metadata -m "${pallet_name}" "${ws_provider}")
+    echo -e "${pallet_data}"
+    echo ""
+  fi
+done <<< "$pallets"
