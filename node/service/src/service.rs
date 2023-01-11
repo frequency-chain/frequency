19dbee34d2dc2a94059dfb0e82f8e49ac5f969e0
Puneet Saraswat <61435908+saraswatpuneet@users.noreply.github.com>
2023-01-11 11:48:33 -0600

    [CHORE] Upgrade version to v0.9.32 (#751)
    # Goal
    The goal of this PR is <!-- insert goal here -->
    
    Closes #705 
    Closes #402 
    Closes #519 
    Closes #866
    Closes #865
    
    Notes:
    
    [PR for Substrate upgrade to
    0.9.32](https://github.com/substrate-developer-hub/substrate-parachain-template/pull/138)
    [Release Notes
    .31](https://github.com/paritytech/polkadot/releases/tag/v0.9.31)
    [Release Notes
    .32](https://github.com/paritytech/polkadot/releases/tag/v0.9.32)
    
    # Changes from 0.9.30 via 0.9.31 to 0.9.32
    
    - Bump dependency version to 0.9.32
    - Weight V2 : introduction of max proof size for weights, for now I am
    using what cumulus uses and suggested on substrate stack exchange
    ...[more](https://substrate.stackexchange.com/questions/5557/construct-runtime-integrity-test-failing).
    - Runtime builds now run a runtime integrity checks 
    - Major Democracy Pallet upgrades and changes
    
    # Checklist
    - [x] Design doc(s) updated
    - [x] Weights updated
    - [x] Run benchmarks
    
    Co-authored-by: Dmitri <4452412+demisx@users.noreply.github.com>
    Co-authored-by: Jenkins <jenkins@frequency.xyz>
    Co-authored-by: Wil Wade <wil.wade@unfinished.com>

--- node/service/src/service.rs
+++ node/service/src/service.rs
@@ -20,7 +20,7 @@ use cumulus_primitives_core::ParaId;
 use cumulus_primitives_parachain_inherent::MockValidationDataInherentDataProvider;
 use cumulus_relay_chain_inprocess_interface::build_inprocess_relay_chain;
 use cumulus_relay_chain_interface::{RelayChainError, RelayChainInterface, RelayChainResult};
-use cumulus_relay_chain_rpc_interface::{create_client_and_start_worker, RelayChainRpcInterface};
+use cumulus_relay_chain_minimal_node::build_minimal_relay_chain_node;
 // Substrate Imports
 pub use futures::stream::StreamExt;
 use polkadot_service::CollatorPair;
@@ -191,10 +191,8 @@ async fn build_relay_chain_interface(
 	hwbench: Option<sc_sysinfo::HwBench>,
 ) -> RelayChainResult<(Arc<(dyn RelayChainInterface + 'static)>, Option<CollatorPair>)> {
 	match collator_options.relay_chain_rpc_url {
-		Some(relay_chain_url) => {
-			let client = create_client_and_start_worker(relay_chain_url, task_manager).await?;
-			Ok((Arc::new(RelayChainRpcInterface::new(client)) as Arc<_>, None))
-		},
+		Some(relay_chain_url) =>
+			build_minimal_relay_chain_node(polkadot_config, task_manager, relay_chain_url).await,
 		None => build_inprocess_relay_chain(
 			polkadot_config,
 			parachain_config,
@@ -410,7 +408,6 @@ where
 			relay_chain_interface,
 			relay_chain_slot_duration,
 			import_queue,
-			collator_options,
 		};
 
 		start_full_node(params)?;
