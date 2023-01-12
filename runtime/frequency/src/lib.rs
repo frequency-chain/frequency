fa260999ff7a807c2a0c112baeb9ff3738b57852
enddy <enddy.code@gmail.com>
2023-01-12 09:23:35 -0800

    feat(capacity): add stake benchmarks  (#902)
    Add benchmarks for the "stake" extrinsic.
    This allows the weights to be used to measure
    the cost of calling the "stake" extrinsic.
    
    Closes #891

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -311,9 +311,11 @@ impl pallet_capacity::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = pallet_capacity::weights::SubstrateWeight<Runtime>;
 	type Currency = Balances;
-	type MinimumStakingAmount = ConstU128<1>;
+	type MinimumStakingAmount = ConstU128<EXISTENTIAL_DEPOSIT>;
 	type TargetValidator = Msa;
 	type MaxUnlockingChunks = ConstU32<4>;
+	#[cfg(feature = "runtime-benchmarks")]
+	type BenchmarkHelper = Msa;
 }
 
 impl pallet_schemas::Config for Runtime {
@@ -818,6 +820,7 @@ mod benches {
 		[pallet_msa, Msa]
 		[pallet_schemas, Schemas]
 		[pallet_messages, Messages]
+		[pallet_capacity, Capacity]
 	);
 }
 
