2b8e1986f4a380437075cbc61160b97498b08bfa
enddy <enddy.code@gmail.com>
2023-01-12 09:23:35 -0800

    feat(capacity): add stake benchmarks  (#902)
    Add benchmarks for the "stake" extrinsic.
    This allows the weights to be used to measure
    the cost of calling the "stake" extrinsic.
    
    Closes #891

--- scripts/run_all_benchmarks.sh
+++ scripts/run_all_benchmarks.sh
@@ -8,7 +8,7 @@ BENCHMARK="$RUNTIME benchmark pallet "
 # TODO: pallet_collator_selection and pallet_collective benchmarks fail due to errors in the
 #  actual benchmark code. See Issue #608
 EXTERNAL_PALLETS=(orml_vesting pallet_balances pallet_timestamp pallet_session pallet_scheduler pallet_democracy pallet_treasury pallet_preimage pallet_utility)
-CUSTOM_PALLETS=(messages msa schemas)
+CUSTOM_PALLETS=(messages msa schemas capacity)
 
 function exit_err() { echo "❌ 💔" ; exit 1; }
 
