5026ed4f3e675a78eff3471a8eeb527fba9af824
Dmitri <4452412+demisx@users.noreply.github.com>
2023-01-17 08:42:22 -0800

    add runtime info to release notes

--- tools/ci/release-notes/build-release-notes.sh
+++ tools/ci/release-notes/build-release-notes.sh
@@ -3,5 +3,12 @@
 
 polkadot_version=$1
 changelog=$2
+runtime_rococo_info=$3
+runtime_mainnet_info=$4
 
-CHANGELOG="$changelog" POLKADOT_VERSION="$polkadot_version" tera -a --env --env-key env --env-only --template release-notes.md.tera
+
+CHANGELOG="$changelog" \
+    POLKADOT_VERSION="$polkadot_version" \
+    RUNTIME_ROCOCO_INFO="$runtime_rococo_info" \
+    RUNTIME_MAINNET_INFO="$runtime_mainnet_info" \
+    tera -a --env --env-key env --env-only --template release-notes.md.tera
