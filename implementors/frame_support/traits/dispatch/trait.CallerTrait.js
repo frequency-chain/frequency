e3945c5c8a87acd8edb5021222eacc5e53121d4d
wilwade <wil@wilwade.com>
2023-01-17 17:12:55 +0000

    Deploying to gh-pages from @ LibertyDSNP/frequency@77b668bf6bdc59cce7716648c851429aac4d57e4 🚀

---
+++ implementors/frame_support/traits/dispatch/trait.CallerTrait.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"frequency_runtime":[["impl CallerTrait&lt;&lt;<a class=\"struct\" href=\"frequency_runtime/struct.Runtime.html\" title=\"struct frequency_runtime::Runtime\">Runtime</a> as Config&gt;::AccountId&gt; for <a class=\"enum\" href=\"frequency_runtime/enum.OriginCaller.html\" title=\"enum frequency_runtime::OriginCaller\">OriginCaller</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
