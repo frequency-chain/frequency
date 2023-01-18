af72c53b5aeef9c3bf5cc374e97b1a98811ab0d7
Shannon Wells <shannonwells@users.noreply.github.com>
2023-01-18 15:45:42 -0800

    Feat/withdraw unstaked 819 (#904)
    # Goal
    The goal of this PR is to implement the `withdraw_unstaked` extrinsic
    for the capacity pallet.
    
    Closes #819
    
    # Discussion
    
    # Checklist
    - [x] Dispatched origin is Signed by Staker.
    - [x] Removes all chunks that are less than or equal to the current
    Epoch and removes the lock by amount from the account balance.
    - [x] Updates StakingAccountLedger totals with remaining amounts.
    - [x] If an account has no remaining stake, clean up storage by removing
    StakingLedger
    - [x] Emits event `StakeWithdrawn` to notify that a withdrawal was made
    from the account and amount withdrawn.
    - [x] Returns `Error::NotAStakingAccount` if origin does not have
    anything staked
    - [x] Returns `Error::NoUnstakedTokensAvailable` if origin has a staking
    account but there is nothing available to withdraw
    - [x] Tests added
    - [x] Benchmarks added
    - [x] Weights updated
    - N/A Chain spec updated
    - N/A Design doc(s) updated
    - N/A Custom RPC OR Runtime API added/changed? Updated js/api-augment.
    
    Co-authored-by: Jenkins <jenkins@frequency.xyz>

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -183,7 +183,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: spec_name!("frequency"),
 	impl_name: create_runtime_str!("frequency"),
 	authoring_version: 1,
-	spec_version: 6,
+	spec_version: 7,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
