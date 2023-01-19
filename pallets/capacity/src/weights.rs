28bf0ca179645f88f2e209ddf2e859a934a6bf9e
John Mela <john.mela@unfinished.com>
2023-01-19 16:36:06 -0500

    feat(capacity): add ability to unstake  (#916)
    # Goal
    The goal of this PR is to allow the unstaking of token for the Capacity
    feature
    
    Closes #818
    
    Co-authored-by: enddy <3433442+enddynayn@users.noreply.github.com>
    Co-authored-by: Shannon Wells <shannonwells@users.noreply.github.com>

--- pallets/capacity/src/weights.rs
+++ pallets/capacity/src/weights.rs
@@ -55,6 +55,7 @@ use sp_std::marker::PhantomData;
 pub trait WeightInfo {
 	fn stake() -> Weight;
 	fn withdraw_unstaked() -> Weight;
+	fn unstake() -> Weight;
 }
 
 /// Weights for pallet_capacity using the Substrate node and recommended hardware.
@@ -77,6 +78,11 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 			.saturating_add(T::DbWeight::get().reads(2 as u64))
 			.saturating_add(T::DbWeight::get().writes(2 as u64))
 	}
+	fn unstake() -> Weight {
+		Weight::from_ref_time(20_829_000 as u64)
+			.saturating_add(RocksDbWeight::get().reads(3 as u64))
+			.saturating_add(RocksDbWeight::get().writes(3 as u64))
+	}
 }
 
 // For backwards compatibility and tests
@@ -97,5 +103,10 @@ impl WeightInfo for () {
 		Weight::from_ref_time(43_067_000 as u64)
 			.saturating_add(RocksDbWeight::get().reads(2 as u64))
 			.saturating_add(RocksDbWeight::get().writes(2 as u64))
+	}
+	fn unstake() -> Weight {
+		Weight::from_ref_time(20_829_000 as u64)
+			.saturating_add(RocksDbWeight::get().reads(3 as u64))
+			.saturating_add(RocksDbWeight::get().writes(3 as u64))
 	}
 }
