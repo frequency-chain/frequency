d8cff5f012f62e0787d620534441b1f0d7c5d195
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-01-19 14:50:30 -0600

    logs and update logic

--- pallets/msa/src/lib.rs
+++ pallets/msa/src/lib.rs
@@ -422,12 +422,13 @@ pub mod pallet {
 		fn offchain_worker(block_number: T::BlockNumber) {
 			// read the event count for the block
 			let block_event_count = <MSAEventCount<T>>::get();
-
+			let current_bucket_num: u32 = block_number.try_into().unwrap_or_default();
+			log_err!("block_event_count: {:?}", block_event_count);
 			// if there are events, read them and process them
 			if block_event_count > 0 {
 				let filtered_events: Vec<Event<T>> =
-					Self::read_events(block_number, block_event_count);
-				log_err!("filtered_events: {:?}", filtered_events);
+					Self::read_events(current_bucket_num, block_event_count);
+				log_err!("filtered_events size: {:?}", filtered_events.len());
 			}
 			let filtered_events: Vec<Event<T>> =
 				<frame_system::Pallet<T>>::read_events_no_consensus()
@@ -492,8 +493,9 @@ pub mod pallet {
 			let (new_msa_id, new_public_key) =
 				Self::create_account(public_key, |_| -> DispatchResult { Ok(()) })?;
 
-			Self::deposit_event(Event::MsaCreated { msa_id: new_msa_id, key: new_public_key });
-
+			let event = Event::MsaCreated { msa_id: new_msa_id, key: new_public_key };
+			Self::index_event(event.clone());
+			Self::deposit_event(event);
 			Ok(())
 		}
 
@@ -556,13 +558,12 @@ pub mod pallet {
 				})?;
 			let event =
 				Event::MsaCreated { msa_id: new_delegator_msa_id, key: new_delegator_public_key };
-			Self::deposit_event(event.clone());
+			Self::index_event(event.clone());
+			Self::deposit_event(event);
 			Self::deposit_event(Event::DelegationGranted {
 				delegator_id: DelegatorId(new_delegator_msa_id),
 				provider_id: ProviderId(provider_msa_id),
 			});
-			Self::index_event(event);
-
 			Ok(())
 		}
 
@@ -740,8 +741,8 @@ pub mod pallet {
 						msa_id,
 						key: add_key_payload.new_public_key.clone(),
 					};
-					Self::deposit_event(event.clone());
-					Self::index_event(event);
+					Self::index_event(event.clone());
+					Self::deposit_event(event);
 					Ok(())
 				},
 			)?;
@@ -777,8 +778,8 @@ pub mod pallet {
 					// Deposit the event
 					let event =
 						Event::PublicKeyDeleted { msa_id: who_msa_id, key: public_key_to_delete };
-					Self::deposit_event(event.clone());
-					Self::index_event(event);
+					Self::index_event(event.clone());
+					Self::deposit_event(event);
 				},
 				None => {
 					log_err!("SignedExtension did not catch invalid MSA for account {:?}, ", who);
@@ -910,8 +911,8 @@ pub mod pallet {
 					num_deletions = Self::delete_delegation_relationship(DelegatorId(msa_id));
 					Self::delete_key_for_msa(msa_id, &who)?;
 					let event = Event::PublicKeyDeleted { msa_id, key: who };
-					Self::deposit_event(event.clone());
-					Self::index_event(event);
+					Self::index_event(event.clone());
+					Self::deposit_event(event);
 					Self::deposit_event(Event::MsaRetired { msa_id });
 				},
 				None => {
@@ -1423,8 +1424,10 @@ impl<T: Config> Pallet<T> {
 	}
 
 	fn index_event(event: Event<T>) {
-		let block_number = <frame_system::Pallet<T>>::block_number();
+		let block_number: u32 =
+			<frame_system::Pallet<T>>::block_number().try_into().unwrap_or_default();
 		let current_event_count: u16 = <MSAEventCount<T>>::get().saturating_add(1);
+		log_err!("Current event count: {:?}", current_event_count);
 		<MSAEventCount<T>>::put(current_event_count);
 		let key = [
 			BLOCK_EVENT_KEY,
@@ -1432,15 +1435,19 @@ impl<T: Config> Pallet<T> {
 			current_event_count.encode().as_slice(),
 		]
 		.concat();
+		log_err!("Key: {:?}", key.encode().as_slice());
+		log_err!("Indexing event: {:?}", event);
 		// set the event in offchain storage
 		set_offchain_index(key.encode().as_slice(), event);
 	}
 
-	fn read_events(block_number: T::BlockNumber, event_count: u16) -> Vec<Event<T>> {
+	fn read_events(block_number: u32, event_count: u16) -> Vec<Event<T>> {
 		let mut events = vec![];
-		for i in 1..event_count {
+		for i in 1..event_count + 1 {
+			log_err!("Reading event: {:?}", i);
 			let key =
 				[BLOCK_EVENT_KEY, block_number.encode().as_slice(), i.encode().as_slice()].concat();
+			log_err!("Key: {:?}", key.encode().as_slice());
 			let optional_decoded_event = get_offchain_index::<Event<T>>(key.encode().as_slice());
 			if let Some(decoded_event) = optional_decoded_event {
 				events.push(decoded_event);
