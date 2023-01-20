06de123911568a3db23ea4b9a4ac9310e9f0dfa8
Joe Caputo <joseph.caputo@unfinished.com>
2023-01-20 16:20:51 -0500

    feat(store-binary-cid): Store binary CID/retrieve in Base32 (#948)
    # Goal
    - Always store IPFS CID as binary on-chain to reduce payload storage
    footprint
    - Always return retrieved CID using Base32 encoding
    
    Closes #787 
    
    # Checklist
    - [ ] Chain spec updated
    - [ ] Custom RPC OR Runtime API added/changed? Updated js/api-augment.
    - [ ] Design doc(s) updated
    - [x] Tests added
    - [ ] Benchmarks added
    - [x] Weights updated
    
    Co-authored-by: Jenkins <jenkins@frequency.xyz>

--- integration-tests/messages/addIPFSMessage.test.ts
+++ integration-tests/messages/addIPFSMessage.test.ts
@@ -4,25 +4,31 @@ import { PARQUET_BROADCAST } from "../schemas/fixtures/parquetBroadcastSchemaTyp
 import assert from "assert";
 import { createAndFundKeypair, devAccounts } from "../scaffolding/helpers";
 import { ExtrinsicHelper } from "../scaffolding/extrinsicHelpers";
-import { u16 } from "@polkadot/types";
-import { loadIpfs } from "./loadIPFS";
+import { u16, u32 } from "@polkadot/types";
+import { loadIpfs, getBases } from "./loadIPFS";
+import { firstValueFrom } from "rxjs";
+import { MessageResponse } from "@frequency-chain/api-augment/interfaces";
 
-describe("Add Offchain Message", function () {
+describe.only("Add Offchain Message", function () {
     this.timeout(5000); // Override default timeout of 500ms to allow for IPFS node startup
 
     let keys: KeyringPair;
     let schemaId: u16;
     let dummySchemaId: u16;
+    let messageBlockNumber: u32;
 
-    let ipfs_cid: string;
+    let ipfs_cid_64: string;
+    let ipfs_cid_32: string;
     let ipfs_node: any;
     const ipfs_payload_data = "This is a test of Frequency.";
     const ipfs_payload_len = ipfs_payload_data.length + 1;
 
     before(async function () {
         ipfs_node = await loadIpfs();
-        const file = await ipfs_node.add({ path: 'integration_test.txt', content: ipfs_payload_data }, { cidVersion: 1 });
-        ipfs_cid = file.cid.toString();
+        const { base64, base32 } = await getBases();
+        const file = await ipfs_node.add({ path: 'integration_test.txt', content: ipfs_payload_data }, { cidVersion: 1, onlyHash: true });
+        ipfs_cid_64 = file.cid.toString(base64);
+        ipfs_cid_32 = file.cid.toString(base32);
 
         keys = await createAndFundKeypair();
 
@@ -46,14 +52,14 @@ describe("Add Offchain Message", function () {
     })
 
     it('should fail if insufficient funds', async function () {
-        await assert.rejects(ExtrinsicHelper.addIPFSMessage(keys, schemaId, ipfs_cid, ipfs_payload_len).signAndSend(), {
+        await assert.rejects(ExtrinsicHelper.addIPFSMessage(keys, schemaId, ipfs_cid_64, ipfs_payload_len).signAndSend(), {
             message: /Inability to pay some fees/,
         });
     }).timeout(500);
 
     it('should fail if MSA is not valid (InvalidMessageSourceAccount)', async function () {
         const accountWithNoMsa = devAccounts[0].keys;
-        await assert.rejects(ExtrinsicHelper.addIPFSMessage(accountWithNoMsa, schemaId, ipfs_cid, ipfs_payload_len).signAndSend(), {
+        await assert.rejects(ExtrinsicHelper.addIPFSMessage(accountWithNoMsa, schemaId, ipfs_cid_64, ipfs_payload_len).signAndSend(), {
             name: 'InvalidMessageSourceAccount',
             section: 'messages',
         });
@@ -62,7 +68,7 @@ describe("Add Offchain Message", function () {
     it('should fail if schema does not exist (InvalidSchemaId)', async function () {
         // Pick an arbitrarily high schemaId, such that it won't exist on the test chain.
         // If we ever create more than 999 schemas in a test suite/single Frequency instance, this test will fail.
-        const f = ExtrinsicHelper.addIPFSMessage(keys, 999, ipfs_cid, ipfs_payload_len);
+        const f = ExtrinsicHelper.addIPFSMessage(keys, 999, ipfs_cid_64, ipfs_payload_len);
         await assert.rejects(f.fundAndSend(), {
             name: 'InvalidSchemaId',
             section: 'messages',
@@ -70,7 +76,7 @@ describe("Add Offchain Message", function () {
     }).timeout(500);
 
     it("should fail if schema payload location is not IPFS (InvalidPayloadLocation)", async function () {
-        const op = ExtrinsicHelper.addIPFSMessage(keys, dummySchemaId, ipfs_cid, ipfs_payload_len);
+        const op = ExtrinsicHelper.addIPFSMessage(keys, dummySchemaId, ipfs_cid_64, ipfs_payload_len);
         await assert.rejects(op.fundAndSend(), { name: "InvalidPayloadLocation" });
     }).timeout(500);
 
@@ -87,14 +93,22 @@ describe("Add Offchain Message", function () {
     }).timeout(500);
 
     it("should successfully add an IPFS message", async function () {
-        const f = ExtrinsicHelper.addIPFSMessage(keys, schemaId, ipfs_cid, ipfs_payload_len);
+        const f = ExtrinsicHelper.addIPFSMessage(keys, schemaId, ipfs_cid_64, ipfs_payload_len);
         const [event] = await f.fundAndSend();
 
         assert.notEqual(event, undefined, "should have returned a MessagesStored event");
         if (event && f.api.events.messages.MessagesStored.is(event)) {
+            messageBlockNumber = event.data.blockNumber;
             assert.deepEqual(event.data.schemaId, schemaId, 'schema ids should be equal');
             assert.notEqual(event.data.blockNumber, undefined, 'should have a block number');
             assert.equal(event.data.count.toNumber(), 1, "message count should be 1");
         }
     });
+
+    it("should successfully retrieve added message and returned CID should have Base32 encoding", async function () {
+        const f = await firstValueFrom(ExtrinsicHelper.api.rpc.messages.getBySchemaId(schemaId, { from_block: 0, from_index: 0, to_block: 999, page_size: 999 }));
+        const response: MessageResponse = f.content[0];
+        const cid = Buffer.from(response.cid.unwrap()).toString();
+        assert.equal(cid, ipfs_cid_32, 'returned CID should match base32-encoded CID');
+    })
 });
