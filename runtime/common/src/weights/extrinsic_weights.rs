fdbafad14ab9466844248694ce8673ca85efed63
Robert La Ferla <robert@onemsg.co>
2023-01-25 12:54:56 -0500

    More fixes for weight constants.

--- runtime/common/src/weights/extrinsic_weights.rs
+++ runtime/common/src/weights/extrinsic_weights.rs
@@ -18,37 +18,41 @@
 pub mod constants {
 	use frame_support::{
 		parameter_types,
-		weights::{constants, Weight},
+		weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight},
 	};
 
 	parameter_types! {
 		/// Executing a NO-OP `System::remarks` Extrinsic.
-		pub const ExtrinsicBaseWeight: Weight = Weight::from_ref_time(constants::WEIGHT_REF_TIME_PER_NANOS).saturating_mul(125_000);
+		pub const ExtrinsicBaseWeight: Weight = Weight::from_ref_time(WEIGHT_REF_TIME_PER_NANOS).saturating_mul(125_000);
 	}
 
 	#[cfg(test)]
 	mod test_weights {
-		use frame_support::weights::constants;
+		use frame_support::weights::{
+			constants::{
+				ExtrinsicBaseWeight, WEIGHT_REF_TIME_PER_MICROS, WEIGHT_REF_TIME_PER_MILLIS,
+			},
+			Weight,
+		};
 
 		/// Checks that the weight exists and is sane.
 		// NOTE: If this test fails but you are sure that the generated values are fine,
 		// you can delete it.
 		#[test]
 		fn sane() {
-			let w = super::constants::ExtrinsicBaseWeight::get();
+			let w = ExtrinsicBaseWeight::get();
 
 			// At least 10 µs.
 			assert!(
 				w.ref_time() >=
-					Weight::from_ref_time(constants::WEIGHT_REF_TIME_PER_MICROS)
+					Weight::from_ref_time(WEIGHT_REF_TIME_PER_MICROS)
 						.saturating_mul(10)
 						.ref_time(),
 				"Weight should be at least 10 µs."
 			);
 			// At most 1 ms.
 			assert!(
-				w.ref_time() <=
-					Weight::from_ref_time(constants::WEIGHT_REF_TIME_PER_MILLIS).ref_time(),
+				w.ref_time() <= Weight::from_ref_time(WEIGHT_REF_TIME_PER_MILLIS).ref_time(),
 				"Weight should be at most 1 ms."
 			);
 		}
