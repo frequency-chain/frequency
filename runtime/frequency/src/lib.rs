496beec897d53231ab5c5b363398d801a6c6fde9
John Mela <john.n.mela@gmail.com>
2023-02-15 17:52:27 -0500

    Expose frequency-tx-pallet calls

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -810,7 +810,7 @@ construct_runtime!(
 		Messages: pallet_messages::{Pallet, Call, Storage, Event<T>} = 61,
 		Schemas: pallet_schemas::{Pallet, Call, Storage, Event<T>, Config} = 62,
 		Capacity: pallet_capacity::{Pallet, Call, Storage, Event<T>} = 63,
-		FrequencyTxPayment: pallet_frequency_tx_payment::{Pallet, Event<T>} = 64,
+		FrequencyTxPayment: pallet_frequency_tx_payment::{Pallet, Call, Event<T>} = 64,
 	}
 );
 
