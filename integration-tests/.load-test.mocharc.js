df1ade30aa33463978013cb2a3074054ee3a092f
Wil Wade <wil.wade@unfinished.com>
2023-02-21 14:03:13 -0500

    WIP: Semi-working?

--- integration-tests/.load-test.mocharc.js
+++ integration-tests/.load-test.mocharc.js
@@ -3,8 +3,8 @@ module.exports = {
     extension: ["ts"],
     parallel: false,
     require:  ['ts-node/register','scaffolding/rootHooks.ts', 'scaffolding/extrinsicHelpers.ts'],
-    spec: ["./{,!(node_modules|load-tests)/**}/*.test.ts"],
-    timeout: 2000,
+    spec: ["./load-tests/**/*.test.ts"],
+    timeout: 60_000,
 }
 
 global.mochaConfig = module.exports;
