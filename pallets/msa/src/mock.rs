7018c77416e3c48d155857eea948e36057cfa987
Robert La Ferla <robert@onemsg.co>
2023-02-28 17:33:03 -0500

    Added upsert_schema_permissions() (#1131)
    # Goal
    The goal of this PR is to add upsert schema permissions functionality
    for `grant_delegation()`
    
    Closes #876

--- pallets/msa/src/mock.rs
+++ pallets/msa/src/mock.rs
@@ -1,5 +1,7 @@
 use crate::{self as pallet_msa, types::EMPTY_FUNCTION, AddProvider};
-use common_primitives::{msa::MessageSourceId, node::BlockNumber, utils::wrap_binary_data};
+use common_primitives::{
+	msa::MessageSourceId, node::BlockNumber, schema::SchemaId, utils::wrap_binary_data,
+};
 use frame_support::{
 	assert_ok,
 	dispatch::DispatchError,
@@ -228,8 +230,19 @@ pub fn create_and_sign_add_provider_payload(
 	delegator_pair: sr25519::Pair,
 	provider_msa: MessageSourceId,
 ) -> (MultiSignature, AddProvider) {
+	create_and_sign_add_provider_payload_with_schemas(delegator_pair, provider_msa, None)
+}
+
+/// Creates and signs an `AddProvider` struct using the provided delegator keypair, provider MSA and schema ids
+/// # Returns
+/// (MultiSignature, AddProvider) - Returns a tuple with the signature and the AddProvider struct
+pub fn create_and_sign_add_provider_payload_with_schemas(
+	delegator_pair: sr25519::Pair,
+	provider_msa: MessageSourceId,
+	schema_ids: Option<Vec<SchemaId>>,
+) -> (MultiSignature, AddProvider) {
 	let expiration: BlockNumber = 10;
-	let add_provider_payload = AddProvider::new(provider_msa, None, expiration);
+	let add_provider_payload = AddProvider::new(provider_msa, schema_ids, expiration);
 	let encode_add_provider_data = wrap_binary_data(add_provider_payload.encode());
 	let signature: MultiSignature = delegator_pair.sign(&encode_add_provider_data).into();
 	(signature, add_provider_payload)
