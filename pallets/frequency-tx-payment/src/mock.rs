2666f8fd4074ef2fc48076f52ee7dd2f80243984
enddy <enddy.code@gmail.com>
2023-02-28 11:46:14 -0800

    feat(tx-payment): add ability to use Capacity (#1138)
    # Goal
    Add the ability to use Capacity for transacting.
    
    This implementation wraps the substrate transaction-payment
    pallet, and it is used to toggle between token transactions
    and capacity transactions.
    
    When Capacity is used to transact, tipping is
    set to zero regardless if a user attempts to tip
    with Capacity. Token transaction tipping is
    still supported.
    
    Closes #1026 
    
    https://www.loom.com/share/b0d3a0df837547dfa7b0195db51cd0e7

--- pallets/frequency-tx-payment/src/mock.rs
+++ pallets/frequency-tx-payment/src/mock.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate as pallet_frequency_tx_payment;
+use common_primitives::msa::MessageSourceId;
 
 use common_primitives::{
 	node::{AccountId, ProposalProvider},
@@ -10,7 +11,8 @@ use pallet_transaction_payment::CurrencyAdapter;
 use sp_core::{ConstU8, H256};
 use sp_runtime::{
 	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup, SaturatedConversion},
+	traits::{BlakeTwo256, Convert, IdentityLookup, SaturatedConversion},
+	AccountId32,
 };
 
 use frame_support::{
@@ -18,7 +20,6 @@ use frame_support::{
 	traits::{ConstU16, ConstU64},
 	weights::WeightToFee as WeightToFeeTrait,
 };
-use sp_runtime::{traits::Convert, AccountId32};
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
@@ -32,11 +33,95 @@ frame_support::construct_runtime!(
 		{
 			System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
 			Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+			Msa: pallet_msa::{Pallet, Call, Storage, Event<T>},
+			Capacity: pallet_capacity::{Pallet, Call, Storage, Event<T>},
 			TransactionPayment: pallet_transaction_payment::{Pallet, Storage, Event<T>},
 			FrequencyTxPayment: pallet_frequency_tx_payment::{Pallet, Call, Event<T>},
-			Msa: pallet_msa::{Pallet, Call, Storage, Event<T>},
 		}
 );
+
+parameter_types! {
+	pub const MaxSchemaGrantsPerDelegation: u32 = 30;
+}
+
+impl Clone for MaxSchemaGrantsPerDelegation {
+	fn clone(&self) -> Self {
+		MaxSchemaGrantsPerDelegation {}
+	}
+}
+
+impl Eq for MaxSchemaGrantsPerDelegation {
+	fn assert_receiver_is_total_eq(&self) -> () {}
+}
+
+impl PartialEq for MaxSchemaGrantsPerDelegation {
+	fn eq(&self, _other: &Self) -> bool {
+		true
+	}
+}
+
+impl sp_std::fmt::Debug for MaxSchemaGrantsPerDelegation {
+	fn fmt(&self, _: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
+		Ok(())
+	}
+}
+pub struct TestAccountId;
+
+impl Convert<u64, AccountId> for TestAccountId {
+	fn convert(_x: u64) -> AccountId32 {
+		AccountId32::new([1u8; 32])
+	}
+}
+pub struct Schemas;
+impl SchemaValidator<SchemaId> for Schemas {
+	fn are_all_schema_ids_valid(_schema_id: &Vec<SchemaId>) -> bool {
+		true
+	}
+
+	fn set_schema_count(_n: SchemaId) {}
+}
+
+pub struct CouncilProposalProvider;
+
+impl ProposalProvider<u64, RuntimeCall> for CouncilProposalProvider {
+	fn propose(
+		_who: u64,
+		_threshold: u32,
+		_proposal: Box<RuntimeCall>,
+	) -> Result<(u32, u32), DispatchError> {
+		Ok((1u32, 1u32))
+	}
+
+	fn propose_with_simple_majority(
+		_who: u64,
+		_proposal: Box<RuntimeCall>,
+	) -> Result<(u32, u32), DispatchError> {
+		Ok((1u32, 1u32))
+	}
+
+	#[cfg(any(feature = "runtime-benchmarks", feature = "test"))]
+	fn proposal_count() -> u32 {
+		1u32
+	}
+}
+
+impl pallet_msa::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type WeightInfo = ();
+	type ConvertIntoAccountId32 = TestAccountId;
+	type MaxPublicKeysPerMsa = ConstU8<255>;
+	type MaxSchemaGrantsPerDelegation = MaxSchemaGrantsPerDelegation;
+	type MaxProviderNameSize = ConstU32<16>;
+	type SchemaValidator = Schemas;
+	type MortalityWindowSize = ConstU32<100>;
+	type MaxSignaturesPerBucket = ConstU32<4000>;
+	type NumberOfBuckets = ConstU32<2>;
+	type Proposal = RuntimeCall;
+	type ProposalProvider = CouncilProposalProvider;
+	type CreateProviderViaGovernanceOrigin = EnsureSigned<u64>;
+	/// This MUST ALWAYS be MaxSignaturesPerBucket * NumberOfBuckets.
+	type MaxSignaturesStored = ConstU32<8000>;
+}
 
 pub struct BlockWeights;
 impl Get<frame_system::limits::BlockWeights> for BlockWeights {
@@ -93,86 +178,6 @@ impl pallet_balances::Config for Test {
 }
 
 parameter_types! {
-	pub const MaxSchemaGrantsPerDelegation: u32 = 30;
-}
-impl Clone for MaxSchemaGrantsPerDelegation {
-	fn clone(&self) -> Self {
-		MaxSchemaGrantsPerDelegation {}
-	}
-}
-impl Eq for MaxSchemaGrantsPerDelegation {
-	fn assert_receiver_is_total_eq(&self) -> () {}
-}
-impl PartialEq for MaxSchemaGrantsPerDelegation {
-	fn eq(&self, _other: &Self) -> bool {
-		true
-	}
-}
-impl sp_std::fmt::Debug for MaxSchemaGrantsPerDelegation {
-	fn fmt(&self, _: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
-		Ok(())
-	}
-}
-
-pub struct TestAccountId;
-impl Convert<u64, AccountId> for TestAccountId {
-	fn convert(_x: u64) -> AccountId32 {
-		AccountId32::new([1u8; 32])
-	}
-}
-
-pub struct Schemas;
-impl SchemaValidator<SchemaId> for Schemas {
-	fn are_all_schema_ids_valid(_schema_id: &Vec<SchemaId>) -> bool {
-		true
-	}
-
-	fn set_schema_count(_n: SchemaId) {}
-}
-
-pub struct CouncilProposalProvider;
-
-impl ProposalProvider<u64, RuntimeCall> for CouncilProposalProvider {
-	fn propose(
-		_who: u64,
-		_threshold: u32,
-		_proposal: Box<RuntimeCall>,
-	) -> Result<(u32, u32), DispatchError> {
-		Ok((1u32, 1u32))
-	}
-
-	fn propose_with_simple_majority(
-		_who: u64,
-		_proposal: Box<RuntimeCall>,
-	) -> Result<(u32, u32), DispatchError> {
-		Ok((1u32, 1u32))
-	}
-
-	#[cfg(any(feature = "runtime-benchmarks", feature = "test"))]
-	fn proposal_count() -> u32 {
-		1u32
-	}
-}
-
-impl pallet_msa::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
-	type ConvertIntoAccountId32 = TestAccountId;
-	type MaxPublicKeysPerMsa = ConstU8<255>;
-	type MaxSchemaGrantsPerDelegation = MaxSchemaGrantsPerDelegation;
-	type MaxProviderNameSize = ConstU32<16>;
-	type SchemaValidator = Schemas;
-	type MortalityWindowSize = ConstU32<100>;
-	type MaxSignaturesPerBucket = ConstU32<4000>;
-	type NumberOfBuckets = ConstU32<2>;
-	type Proposal = RuntimeCall;
-	type ProposalProvider = CouncilProposalProvider;
-	type CreateProviderViaGovernanceOrigin = EnsureSigned<u64>;
-	/// This MUST ALWAYS be MaxSignaturesPerBucket * NumberOfBuckets.
-	type MaxSignaturesStored = ConstU32<8000>;
-}
-
-parameter_types! {
 	pub static WeightToFee: u64 = 1;
 	pub static TransactionByteFee: u64 = 1;
 	static ExtrinsicBaseWeight: Weight = Weight::zero();
@@ -205,9 +210,26 @@ impl pallet_transaction_payment::Config for Test {
 	type OperationalFeeMultiplier = ConstU8<5>;
 }
 
+impl pallet_capacity::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type WeightInfo = ();
+	type Currency = pallet_balances::Pallet<Self>;
+	type TargetValidator = ();
+	type MinimumStakingAmount = ConstU64<5>;
+	type MaxUnlockingChunks = ConstU32<4>;
+
+	#[cfg(feature = "runtime-benchmarks")]
+	type BenchmarkHelper = ();
+
+	type UnstakingThawPeriod = ConstU16<2>;
+	type MaxEpochLength = ConstU64<100>;
+	type EpochNumber = u32;
+}
+
 impl Config for Test {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
+	type Capacity = Capacity;
 }
 
 pub struct ExtBuilder {
@@ -266,6 +288,40 @@ impl ExtBuilder {
 		.assimilate_storage(&mut t)
 		.unwrap();
 
+		let mut t: sp_io::TestExternalities = t.into();
+
+		// Create MSA account 1 - 6 and add Balance to them with Capacity balance
+		t.execute_with(|| {
+			let msa_accounts: Vec<(
+				<Test as frame_system::Config>::AccountId,
+				<Test as pallet_balances::Config>::Balance,
+			)> = vec![
+				(1, 10 * self.balance_factor),
+				(2, 20 * self.balance_factor),
+				(3, 30 * self.balance_factor),
+				(4, 40 * self.balance_factor),
+				(5, 50 * self.balance_factor),
+				(6, 60 * self.balance_factor),
+			];
+			msa_accounts.iter().for_each(|(account, balance)| {
+				let msa_id = create_msa_account(account.clone());
+				create_capacity_for(msa_id, balance.clone());
+			});
+		});
+
 		t.into()
 	}
+}
+
+fn create_msa_account(account_id: <Test as frame_system::Config>::AccountId) -> MessageSourceId {
+	pub const EMPTY_FUNCTION: fn(MessageSourceId) -> DispatchResult = |_| Ok(());
+	let (msa_id, _) = Msa::create_account(account_id, EMPTY_FUNCTION).unwrap();
+
+	msa_id
+}
+
+fn create_capacity_for(target: MessageSourceId, amount: u64) {
+	let mut capacity_details = Capacity::get_capacity_for(target).unwrap_or_default();
+	capacity_details.deposit(amount, Capacity::get_current_epoch()).unwrap();
+	Capacity::set_capacity_for(target, capacity_details);
 }
