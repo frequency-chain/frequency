15e3c1affee4a2293d536d9e9e7ce88b2a5ff249
shannonwells <shannonwells@users.noreply.github.com>
2023-03-07 14:16:04 -0800

    remove deleted function from CapacityEligibleCalls list after rebase

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -873,8 +873,8 @@ impl Contains<RuntimeCall> for CapacityEligibleCalls {
 	fn contains(call: &RuntimeCall) -> bool {
 		{
 			match call {
-				RuntimeCall::Msa(MsaCall::create_sponsored_account_with_delegation { .. }) => true,
 				RuntimeCall::Msa(MsaCall::add_public_key_to_msa { .. }) => true,
+				RuntimeCall::Msa(MsaCall::create_sponsored_account_with_delegation { .. }) => true,
 				RuntimeCall::Msa(MsaCall::grant_delegation { .. }) => true,
 				RuntimeCall::Messages(MessagesCall::add_ipfs_message { .. }) => true,
 				RuntimeCall::Messages(MessagesCall::add_onchain_message { .. }) => true,
