db81e4fa142087d8b5cd37b181852f3bb8407803
Robert La Ferla <robert@onemsg.co>
2023-03-10 13:07:17 -0500

    Consolidated mock runtimes (#1212)
    # Goal
    The goal of this PR is consolidate the mock runtimes in the MSA pallet
    yet run slightly different configurations needed by different tests.
    
    Closes  #1209

--- pallets/msa/src/signature_registry_tests.rs
+++ pallets/msa/src/signature_registry_tests.rs
@@ -1,197 +1,32 @@
 use crate::{
-	self as pallet_msa,
-	mock::{create_account, generate_test_signature, new_test_ext, run_to_block},
-	types::AddKeyData,
-	Config, Error, PayloadSignatureRegistryPointer,
+	mock::*, types::AddKeyData, Config, Error, PayloadSignatureRegistryPointer,
+	SignatureRegistryPointer,
 };
 
-use frame_support::{
-	assert_noop, assert_ok,
-	dispatch::DispatchError,
-	parameter_types,
-	traits::{ConstU16, ConstU32, ConstU64, EitherOfDiverse, Everything, Get},
-};
-use frame_system::EnsureRoot;
-use pallet_collective;
+use frame_support::{assert_noop, assert_ok};
 
-use sp_core::{sr25519, Encode, Pair, H256};
-use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, ConvertInto, IdentityLookup},
-	MultiSignature,
-};
+use common_primitives::{node::BlockNumber, utils::wrap_binary_data};
 
-pub use common_runtime::constants::*;
+use sp_core::{sr25519, Encode, Pair};
+use sp_runtime::MultiSignature;
 
-use common_primitives::{
-	msa::SignatureRegistryPointer,
-	node::{AccountId, BlockNumber},
-	utils::wrap_binary_data,
-};
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	set_max_signature_stored(20);
+	set_max_public_keys_per_msa(10);
 
-pub use pallet_msa::Call as MsaCall;
-
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
-
-// Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		Msa: pallet_msa::{Pallet, Call, Storage, Event<T>},
-		Schemas: pallet_schemas::{Pallet, Call, Storage, Event<T>},
-		Council: pallet_collective::<Instance1>::{Pallet, Call, Config<T,I>, Storage, Event<T>, Origin<T>},
-	}
-);
-
-// See https://paritytech.github.io/substrate/master/pallet_collective/index.html for
-// the descriptions of these configs.
-type CouncilCollective = pallet_collective::Instance1;
-impl pallet_collective::Config<CouncilCollective> for Test {
-	type RuntimeOrigin = RuntimeOrigin;
-	type Proposal = RuntimeCall;
-	type RuntimeEvent = RuntimeEvent;
-	type MotionDuration = CouncilMotionDuration;
-	type MaxProposals = CouncilMaxProposals;
-	type MaxMembers = CouncilMaxMembers;
-	type DefaultVote = pallet_collective::PrimeDefaultVote;
-	type WeightInfo = ();
+	let t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
 }
 
-impl frame_system::Config for Test {
-	type BaseCallFilter = Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = AccountId;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
-}
-
-impl pallet_schemas::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
-	type MinSchemaModelSizeBytes = ConstU32<10>;
-	type SchemaModelMaxBytesBoundedVecLimit = ConstU32<10>;
-	type MaxSchemaRegistrations = ConstU16<10>;
-	// The proposal type
-	type Proposal = RuntimeCall;
-	// The Council proposal provider interface
-	type ProposalProvider = CouncilProposalProvider;
-	// The origin that is allowed to create schemas via governance
-	type CreateSchemaViaGovernanceOrigin = EitherOfDiverse<
-		EnsureRoot<AccountId>,
-		pallet_collective::EnsureProportionMoreThan<AccountId, CouncilCollective, 1, 2>,
-	>;
-}
-
-parameter_types! {
-	pub const MaxPublicKeysPerMsa: u8 = 10;
-	pub const MaxProviderNameSize: u32 = 16;
-	pub const MaxSchemas: u32 = 5;
-}
-
-parameter_types! {
-	pub const MaxSchemaGrantsPerDelegation: u32 = 30;
-}
-
-impl Clone for MaxSchemaGrantsPerDelegation {
-	fn clone(&self) -> Self {
-		MaxSchemaGrantsPerDelegation {}
-	}
-}
-
-impl Eq for MaxSchemaGrantsPerDelegation {
-	fn assert_receiver_is_total_eq(&self) -> () {}
-}
-
-impl PartialEq for MaxSchemaGrantsPerDelegation {
-	fn eq(&self, _other: &Self) -> bool {
-		true
-	}
-}
-
-impl sp_std::fmt::Debug for MaxSchemaGrantsPerDelegation {
-	fn fmt(&self, _: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
-		Ok(())
-	}
-}
-
-pub struct CouncilProposalProvider;
-impl pallet_msa::ProposalProvider<AccountId, RuntimeCall> for CouncilProposalProvider {
-	fn propose(
-		who: AccountId,
-		threshold: u32,
-		proposal: Box<RuntimeCall>,
-	) -> Result<(u32, u32), DispatchError> {
-		let length_bound: u32 = proposal.using_encoded(|p| p.len() as u32);
-		Council::do_propose_proposed(who, threshold, proposal, length_bound)
-	}
-
-	fn propose_with_simple_majority(
-		who: AccountId,
-		proposal: Box<RuntimeCall>,
-	) -> Result<(u32, u32), DispatchError> {
-		let threshold: u32 = ((Council::members().len() / 2) + 1) as u32;
-		let length_bound: u32 = proposal.using_encoded(|p| p.len() as u32);
-		Council::do_propose_proposed(who, threshold, proposal, length_bound)
-	}
-
-	#[cfg(any(feature = "runtime-benchmarks", feature = "test"))]
-	fn proposal_count() -> u32 {
-		Council::proposal_count()
-	}
-}
-
-impl pallet_msa::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
-	type ConvertIntoAccountId32 = ConvertInto;
-	// The proposal type
-	type Proposal = RuntimeCall;
-	// The Council proposal provider interface
-	type ProposalProvider = CouncilProposalProvider;
-	type MaxPublicKeysPerMsa = MaxPublicKeysPerMsa;
-	type MaxSchemaGrantsPerDelegation = MaxSchemaGrantsPerDelegation;
-	type MaxProviderNameSize = MaxProviderNameSize;
-	type SchemaValidator = Schemas;
-	type MortalityWindowSize = ConstU32<100>;
-	type MaxSignaturesStored = ConstU32<20>;
-	// The origin that is allowed to create providers via governance
-	// It has to be this way so benchmarks will pass in CI.
-	type CreateProviderViaGovernanceOrigin = EitherOfDiverse<
-		EnsureRoot<AccountId>,
-		pallet_collective::EnsureMembers<AccountId, CouncilCollective, 1>,
-	>;
-}
 #[test]
 pub fn cannot_register_too_many_signatures() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
 		let mortality_block: BlockNumber = 3;
 
-		let limit: u32 = <Test as Config>::MaxSignaturesStored::get();
+		let limit: u32 = <Test as Config>::MaxSignaturesStored::get().unwrap_or(0);
 		for _i in 0..limit {
 			let sig = &generate_test_signature();
 			assert_ok!(Msa::register_signature(sig, mortality_block.into()));
@@ -242,7 +77,7 @@ pub fn stores_signature_and_increments_count() {
 		let mut newest: MultiSignature = signature_1.clone();
 
 		// Fill up the registry
-		let limit: u32 = <Test as Config>::MaxSignaturesStored::get();
+		let limit: u32 = <Test as Config>::MaxSignaturesStored::get().unwrap_or(0);
 		for _i in 2..limit {
 			let sig = &generate_test_signature();
 			assert_ok!(Msa::register_signature(sig, mortality_block.into()));
@@ -283,7 +118,7 @@ pub fn clears_stale_signatures_after_mortality_limit() {
 		System::set_block_number(1);
 		let mortality_block: BlockNumber = 3;
 
-		let limit: u32 = <Test as Config>::MaxSignaturesStored::get();
+		let limit: u32 = <Test as Config>::MaxSignaturesStored::get().unwrap_or(0);
 		for _i in 0..limit {
 			let sig = &generate_test_signature();
 			assert_ok!(Msa::register_signature(sig, mortality_block.into()));
