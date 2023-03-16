ad65bfa48cecf756c1e5ba1ca8587e416d070e4e
Shannon Wells <shannonwells@users.noreply.github.com>
2023-03-16 14:16:58 -0700

    Replenishment integration tests (#1248)
    # Goal
    The goal of this PR is to add end-to-end tests for capacity replenishment.
    Had to remove some test assertions from transaction tests that were too strict.
    
    Closes #890

--- integration-tests/scaffolding/extrinsicHelpers.ts
+++ integration-tests/scaffolding/extrinsicHelpers.ts
@@ -4,8 +4,8 @@ import { KeyringPair } from "@polkadot/keyring/types";
 import { Compact, u128, u16, u32, u64, Vec } from "@polkadot/types";
 import { FrameSystemAccountInfo, SpRuntimeDispatchError } from "@polkadot/types/lookup";
 import { AnyNumber, AnyTuple, Codec, IEvent, ISubmittableResult } from "@polkadot/types/types";
-import { firstValueFrom, filter, map, pipe, tap } from "rxjs";
-import { devAccounts, log, Sr25519Signature } from "./helpers";
+import {firstValueFrom, filter, map, pipe, tap} from "rxjs";
+import {devAccounts, getBlockNumber, log, Sr25519Signature} from "./helpers";
 import { connect, connectPromise } from "./apiConnection";
 import { CreatedBlock, DispatchError, Event, SignedBlock } from "@polkadot/types/interfaces";
 import { IsEvent } from "@polkadot/types/metadata/decorate/types";
@@ -311,7 +311,7 @@ export class ExtrinsicHelper {
     }
 
     public static addOnChainMessage(keys: KeyringPair, schemaId: any, payload: string): Extrinsic {
-        return new Extrinsic(() => ExtrinsicHelper.api.tx.messages.addOnChainMessage(schemaId, payload), keys, ExtrinsicHelper.api.events.messages.MessagesStored);
+        return new Extrinsic(() => ExtrinsicHelper.api.tx.messages.addOnchainMessage(null, schemaId, payload), keys, ExtrinsicHelper.api.events.messages.MessagesStored);
     }
 
     /** Capacity Extrinsics **/
@@ -328,5 +328,18 @@ export class ExtrinsicHelper {
 
     public static withdrawUnstaked(keys: KeyringPair): Extrinsic {
         return new Extrinsic(() => ExtrinsicHelper.api.tx.capacity.withdrawUnstaked(), keys, ExtrinsicHelper.api.events.capacity.StakeWithdrawn);
+    }
+
+    public static async mine() {
+      let res: CreatedBlock = await firstValueFrom(ExtrinsicHelper.api.rpc.engine.createBlock(true, true));
+      ExtrinsicHelper.api.rpc.engine.finalizeBlock(res.blockHash);
+    }
+
+    public static async run_to_block(blockNumber: number) {
+      let currentBlock = await getBlockNumber();
+      while (currentBlock < blockNumber) {
+        ExtrinsicHelper.mine();
+        currentBlock = await getBlockNumber();
+      }
     }
 }
