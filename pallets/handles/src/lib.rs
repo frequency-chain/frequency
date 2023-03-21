bd6e8657bce78a2d41ad701b436bbf9118af3f56
Robert La Ferla <robert@onemsg.co>
2023-03-21 11:31:07 -0400

    Initial scaffolding for new handles pallet

---
+++ pallets/handles/src/lib.rs
@@ -0,0 +1,16 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {}
+}
