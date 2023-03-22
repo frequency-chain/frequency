ea7a97ac10f258011bc6ccbf41a32fbdd973002e
Robert La Ferla <robert@onemsg.co>
2023-03-22 17:43:06 -0400

    Mock now compiles.  Added homoglyph test. WIP.

--- pallets/handles/src/tests/homoglyph_tests.rs
+++ pallets/handles/src/tests/homoglyph_tests.rs
@@ -1,1 +1,9 @@
+use unicode_normalization::UnicodeNormalization;
 
+#[test]
+fn canonical_test() {
+	let input = "A Α А Ꭺ ᗅ ᴀ ꓮ Ａ 𐊠 𝐀 𝐴 𝑨 𝒜 𝓐 𝔄 𝔸 𝕬 𝖠 𝗔 𝘈 𝘼 𝙰 𝚨 𝛢 𝜜 𝝖 𝞐";
+	let normalized = input.nfc().collect::<String>(); // normalize using NFC
+	let result = normalized.to_lowercase(); // convert to lower case
+	println!("{}", result);
+}
--- pallets/handles/src/tests/mock.rs
+++ pallets/handles/src/tests/mock.rs
@@ -1,13 +1,48 @@
-use crate as pallet_template;
-use frame_support::traits::{ConstU16, ConstU64};
+use crate as pallet_handles;
+use common_primitives::msa::{MessageSourceId, MsaLookup, MsaValidator};
+use frame_support::{
+	traits::{ConstU16, ConstU32, ConstU64},
+};
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
+	DispatchError,
 };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
+
+pub struct MsaInfoHandler;
+
+impl MsaLookup for MsaInfoHandler {
+	type AccountId = u64;
+
+	fn get_msa_id(key: &Self::AccountId) -> Option<MessageSourceId> {
+		if *key == 1000 {
+			return None
+		}
+		if *key == 2000 {
+			return Some(2000 as MessageSourceId)
+		}
+		Some(get_msa_from_account(*key) as MessageSourceId)
+	}
+}
+
+impl MsaValidator for MsaInfoHandler {
+	type AccountId = u64;
+
+	fn ensure_valid_msa_key(key: &Self::AccountId) -> Result<MessageSourceId, DispatchError> {
+		if *key == 1000 {
+			return Err(DispatchError::Other("some error"))
+		}
+		if *key == 2000 {
+			return Ok(2000)
+		}
+
+		Ok(get_msa_from_account(*key))
+	}
+}
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
@@ -17,7 +52,7 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system,
-		TemplateModule: pallet_template,
+		Handles: pallet_handles::{Pallet, Call, Storage, Event<T>},
 	}
 );
 
@@ -48,11 +83,27 @@ impl frame_system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
-impl pallet_template::Config for Test {
+impl pallet_handles::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+
+	/// Weight information for extrinsics in this pallet.
+	// type WeightInfo: WeightInfo;
+
+	/// A type that will supply MSA related information
+	type MsaInfoProvider = MsaInfoHandler;
+
+	/// The minimum suffix value
+	type HandleSuffixMin = ConstU32<2>;
+
+	/// The maximum suffix value
+	type HandleSuffixMax = ConstU32<30>;
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
 	frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
+
+pub fn get_msa_from_account(account_id: u64) -> u64 {
+	account_id + 100
 }
