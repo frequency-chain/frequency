4de55e07c383c4624185e1b4b13156dac74e4d2c
Joe Caputo <joseph.caputo@unfinished.com>
2023-03-22 15:07:02 -0400

    1267 benchmarking writes pallet collective twice (#1268)
    # Goal
    The goal of this PR is to:
    1. Account for multiple instances of `pallet_collective` when
    benchmarking & using weights.
    2. Cleanup/enhance benchmarking scripts
    
    Closes #1267
    
    # Checklist
    - [ ] Chain spec updated
    - [ ] Custom RPC OR Runtime API added/changed? Updated js/api-augment.
    - [ ] Design doc(s) updated
    - [ ] Tests added
    - [ ] Benchmarks added
    - [x] Weights updated
    
    ---------
    
    Co-authored-by: Frequency CI [bot] <do-not-reply@users.noreply.github.com>
    # Conflicts:
    #	Makefile
    #	scripts/run_benchmarks.sh

---
+++ scripts/run_benchmarks.sh
@@ -0,0 +1,207 @@
+#!/usr/bin/env bash
+
+export RUST_LOG=info
+THIS_DIR=$( dirname -- "$0"; )
+PROJECT="${THIS_DIR}/.."
+PROFILE=production
+PROFILE_DIR=${PROFILE}
+ALL_EXTERNAL_PALLETS=( \
+  orml_vesting \
+  pallet_balances \
+  pallet_collator_selection \
+  pallet_collective \
+  pallet_democracy \
+  pallet_multisig \
+  pallet_preimage \
+  pallet_scheduler \
+  pallet_session \
+  pallet_timestamp \
+  pallet_treasury \
+  pallet_utility \
+)
+ALL_CUSTOM_PALLETS=( \
+  messages \
+  msa \
+  schemas \
+)
+
+declare -a CUSTOM_PALLETS
+declare -a EXTERNAL_PALLETS
+skip_build=false
+VERBOSE=
+
+function exit_err() { echo "❌ 💔" ; exit 1; }
+
+function usage() {
+  cat << EOI
+  Usage: $( basename ${1} ) [-d <dir>] [-p <pallet] [-s] [-t <profile>] [-v]
+         $( basename ${1} ) [-d <dir>] [-s] [-t] [-v] [<pallet1> [... <palletN>]]
+
+         -d <dir>     Sets top-level repository directory to <dir>.
+                      Default: parent directory of script
+
+        -h            Display this message and exit.
+
+        -s            Skip the build step; use existing binary for the current profile
+
+        -t <profile>  Use '--profile=<profile>' in the build step & for locating the
+                      resulting binary. Valid targets are: dev,production,release,bench-dev
+
+                      (NOTE: using the 'bench-dev' profile will generate a warning in the WASM build.
+                       this can safely be ignored. 'bench-dev' is a clone of 'release' and is useful
+                       for running benchmarks locally while not overwriting any existing 'release' target.)
+
+        -v            Verbose mode. All shell commands are echoed.
+
+        <palletX>     To run for multiple specific pallets. If no pallets
+                      specified, will run all benchmarks.
+
+EOI
+}
+
+function is_external_pallet() {
+  for p in "${ALL_EXTERNAL_PALLETS[@]}"
+  do
+     if [ "${1}" == "${p}" ]
+     then
+        return 0
+     fi
+  done
+
+  return 1
+}
+
+function is_custom_pallet() {
+  for p in "${ALL_CUSTOM_PALLETS[@]}"
+  do
+     if [ "${1}" == "${p}" ]
+     then
+        return 0
+     fi
+  done
+
+  return 1
+}
+
+while getopts 'dh:p:st:v' flag; do
+  case "${flag}" in
+    d)
+      # Set project directory
+      PROJECT="${OPTARG}"
+      ;;
+    h)
+      usage ${0}
+      exit 0
+      ;;
+    s)
+      # Skip build.
+      skip_build=true
+      ;;
+    t)
+      # Set target profile
+      case ${OPTARG} in
+        production|release|dev|bench-dev)
+          PROFILE="${OPTARG}"
+          PROFILE_DIR=${PROFILE}
+          # For historical reasons, cargo puts dev builds in the "debug" directory
+          if [ ${PROFILE} = "dev" ]
+          then
+            PROFILE_DIR=debug
+          fi
+          ;;
+        *) echo "Unrecognized target profile: ${OPTARG}"
+           usage ${0}
+           exit_err
+           ;;
+      esac
+      ;;
+    v)
+      # Echo all executed commands.
+      VERBOSE=1
+      set -x
+      ;;
+    ?)
+      # Unrecognized option. getopts will log the error
+      usage ${0}
+      exit_err
+      ;;
+    *)
+      # Exit early.
+      echo "Bad options. Check Script."
+      usage ${0}
+      exit_err
+      ;;
+  esac
+done
+shift $(( ${OPTIND} - 1 ))
+
+for pallet in "${@}"
+do
+   if is_external_pallet ${pallet}
+   then
+      EXTERNAL_PALLETS=( "${EXTERNAL_PALLETS[@]}" "${pallet}" )
+   elif is_custom_pallet ${pallet}
+   then
+      CUSTOM_PALLETS=( "${CUSTOM_PALLETS[@]}" "${pallet}" )
+   else
+      echo "Unrecognized pallet: ${pallet}"
+      exit_err
+   fi
+done
+
+if [[ ${#EXTERNAL_PALLETS[@]} == 0 && ${#CUSTOM_PALLETS[@]} == 0 ]]
+then
+  EXTERNAL_PALLETS=( "${ALL_EXTERNAL_PALLETS[@]}" )
+  CUSTOM_PALLETS=( ${ALL_CUSTOM_PALLETS[@]} )
+fi
+
+RUNTIME=${PROJECT}/target/${PROFILE_DIR}/frequency
+BENCHMARK="${RUNTIME} benchmark "
+
+echo "Running benchmarks for the following pallets:\
+${EXTERNAL_PALLETS[@]} \
+${CUSTOM_PALLETS[@]}"
+
+
+function run_benchmark() {
+  echo "Running benchmarks for ${1}"
+  set -x
+  ${BENCHMARK} pallet \
+  --pallet=${1} \
+  --extrinsic "*" \
+  --chain="frequency-bench" \
+  --execution=wasm \
+  --heap-pages=4096 \
+  --wasm-execution=compiled \
+  --steps=${2} \
+  --repeat=${3} \
+  --output=${4} \
+  --template=${5}
+  if [ -z "${VERBOSE}" ]
+  then
+    set +x
+  fi
+}
+
+if [[ ${skip_build} == false ]]
+then
+  CMD="cargo build --profile=${PROFILE} --features=runtime-benchmarks,all-frequency-features --workspace"
+  echo ${CMD}
+  ${CMD} || exit_err
+fi
+
+for external_pallet in "${EXTERNAL_PALLETS[@]}"; do
+  output=${PROJECT}/runtime/common/src/weights
+  steps=50
+  repeat=20
+  template=${PROJECT}/.maintain/runtime-weight-template.hbs
+  run_benchmark ${external_pallet} ${steps} ${repeat} ${output} ${template} || exit_err
+done
+
+for pallet_name in "${CUSTOM_PALLETS[@]}"; do
+  steps=20
+  repeat=10
+  template=${PROJECT}/.maintain/frame-weight-template.hbs
+  output=${PROJECT}/pallets/${pallet_name/_/-}/src/weights.rs
+  run_benchmark pallet_${pallet_name} ${steps} ${repeat} ${output} ${template} || exit_err
+done
