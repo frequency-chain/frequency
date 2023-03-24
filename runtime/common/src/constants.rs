5746e161a2c5780d3548ebd04d4c2d314d1fc870
Aramik <aramikm@gmail.com>
2023-03-24 10:59:51 -0700

    staeful-storage: reduced paginated page size

--- runtime/common/src/constants.rs
+++ runtime/common/src/constants.rs
@@ -304,8 +304,8 @@ parameter_types! {
 parameter_types! {
 	/// The maximum size of a page (in bytes) for an Itemized storage model (64KB)
 	pub const MaxItemizedPageSizeBytes: u32 = 64 * 1024;
-	/// The maximum size of a page (in bytes) for a Paginated storage model (2KB)
-	pub const MaxPaginatedPageSizeBytes: u32 = 2 * 1024;
+	/// The maximum size of a page (in bytes) for a Paginated storage model (1KB)
+	pub const MaxPaginatedPageSizeBytes: u32 = 1 * 1024;
 	/// The maximum size of a single item in an itemized storage model (in bytes)
 	pub const MaxItemizedBlobSizeBytes: u32 = 1024;
 	/// The maximum number of pages in a Paginated storage model
