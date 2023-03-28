8fd2267239350e00ee252f8469d4eca356e7c664
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2023-03-28 21:31:16 +0800

    Update integration test scripts and options (#1259)
    # Goal
    The goal of this PR is to update the run_integration_tests.sh script to:
    
    1. Handle signals and clean up.
    2. Add a command line argument to skip the js build.
    
    Change Explanations:
    
    Makefile:
    - Added targets to enable skipping js build.
      - integration-test = no arguments
      - integration-test-only = `-s` argument to skip js build
      - integration-test-load = `load` argument
    - integration-test-load-only = `-s load` arguments to skip js build for
    load tests
    
    run_integration_tests.sh:
    - Modify `cleanup` not to use the kill_freq.sh script. As signals are
    asynchronous, `cleanup` does not know how to navigate to the scripts
    directory using relative paths. It's a small script, so the commands are
    included directly in `cleanup`.
    - Fix syntax errors so that `getopts` will correctly parse the `-s`
    option.
    - Add `if-then` conditional for `SKIP_JS_BUILD`.
    - Remove `if-then` conditional which is handled by `cleanup` on
    receiving the `EXIT` signal when the script finishes.
    - Refactor `SHOULD_KILL` and only populate `PID` when the script starts
    the Frequency node.
    # Conflicts:
    #	scripts/run_integration_tests.sh

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,5 +1,48 @@
 #!/usr/bin/env bash
 
+function get_frequency_pid () {
+    lsof -i tcp:9933 | grep frequency | xargs | awk '{print $2}'
+}
+
+function cleanup () {
+    local signal="$1"
+
+    case "$signal" in
+        TERM|INT)
+            # Catch TERM and INT signals and exit gracefully
+            echo "Caught signal ${signal}; exiting..."
+            exit
+            ;;
+        EXIT)
+            # kill_freq.sh is not used here because we do not know what directory
+            # the script is in when a signal is received. Therefore, we do not
+            # know how to navigate to the kill_freq.sh script with relative paths.
+            if [ -n "${PID}" ]
+            then
+                kill -9 ${PID}
+                echo "Frequency has been killed. 💀"
+            else
+                echo "Frequency was not started by integration-test."
+            fi
+            ;;
+    esac
+}
+
+RUNDIR=$(dirname ${0})
+SKIP_JS_BUILD=
+trap 'cleanup EXIT' EXIT
+trap 'cleanup TERM' TERM
+trap 'cleanup INT' INT
+
+while getopts "s" OPTNAME
+do
+    case "${OPTNAME}" in
+        "s") SKIP_JS_BUILD=1
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
 TEST="test"
 START="start"
 
@@ -14,39 +57,59 @@ echo "You can 'tail -f frequency.log' in another terminal to see both side-by-si
 echo ""
 echo -e "Checking to see if Frequency is running..."
 
-PID=$(lsof -i tcp:9933 | grep frequency | grep -v grep | xargs | awk '{print $2}')
-
-SHOULD_KILL=false
-
-if [ -z "$PID" ]
+if [ -n "$( get_frequency_pid )" ]
 then
-    make build-local
-    echo -e "Starting a Frequency Node..."
-    make $START >& frequency.log &
-    SHOULD_KILL=true
+    echo "Frequency is already running."
+else
+    echo "Building local Frequency executable..."
+    if ! make build-local
+    then
+        echo "Error building Frequency executable; aborting."
+        exit 1
+    fi
+
+    echo "Starting a Frequency Node with ${START}..."
+    case ${START} in
+        "start") ${RUNDIR}/init.sh start-frequency-instant >& frequency.log &
+        ;;
+        "start-manual") ${RUNDIR}/init.sh start-frequency-manual >& frequency.log &
+        ;;
+    esac
+
+    declare -i timeout_secs=30
+    declare -i i=0
+    while (( !PID && i < timeout_secs ))
+    do
+        PID=$( get_frequency_pid )
+        sleep 1
+        (( i += 1 ))
+    done
+
+    if [ -z "${PID}" ]
+    then
+        echo "Unable to find or start a Frequency node; aborting."
+        exit 1
+    fi
+    echo "---------------------------------------------"
+    echo "Frequency running here:"
+    echo "PID: ${PID}"
+    echo "---------------------------------------------"
 fi
 
-while [ -z "$PID" ]
-do
-    sleep 3
-    PID=$(lsof -i tcp:9933 | grep frequency | grep -v grep | xargs | awk '{print $2}')
-    echo "Waiting for 9933 to be open..."
-done
-
-echo "---------------------------------------------"
-echo "Frequency running here:"
-echo "PID: ${PID}"
-echo "---------------------------------------------"
-
-echo "Building js/api-augment..."
-cd js/api-augment
-npm i
-npm run fetch:local
-npm run --silent build
-cd dist
-echo "Packaging up into js/api-augment/dist/frequency-chain-api-augment-0.0.0.tgz"
-npm pack --silent
-cd ../../..
+if [ "${SKIP_JS_BUILD}" = "1" ]
+then
+    echo "Skipping js/api-augment build"
+else
+    echo "Building js/api-augment..."
+    cd js/api-augment
+    npm i
+    npm run fetch:local
+    npm run --silent build
+    cd dist
+    echo "Packaging up into js/api-augment/dist/frequency-chain-api-augment-0.0.0.tgz"
+    npm pack --silent
+    cd ../../..
+fi
 
 
 cd integration-tests
@@ -57,9 +120,3 @@ echo "---------------------------------------------"
 echo "Starting Tests..."
 echo "---------------------------------------------"
 WS_PROVIDER_URL="ws://127.0.0.1:9944" npm run $TEST
-
-if $SHOULD_KILL
-then
-   pwd
-   ../scripts/kill_freq.sh
-fi
