82854ed6af7d958c30e712fc6d031c887c4eea5f
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-03-30 12:47:20 -0500

    weifghts

--- pallets/handles/src/benchmarking.rs
+++ pallets/handles/src/benchmarking.rs
@@ -2,6 +2,7 @@
 
 use super::*;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_support::assert_ok;
 use frame_system::RawOrigin;
 use sp_core::crypto::KeyTypeId;
 use sp_runtime::RuntimeAppPublic;
@@ -20,13 +21,17 @@ mod app_sr25519 {
 type SignerId = app_sr25519::Public;
 
 fn create_signed_claims_payload<T: Config>(
-	handle_size: u32,
+	char_size: u32,
+	byte_size: u32,
 ) -> (ClaimHandlePayload, MultiSignature, T::AccountId) {
 	let delegator_account_public = SignerId::generate_pair(None);
 	// create a generic handle example with expanding size
 	let base_handle = b"b".to_vec();
 	let mut handle = base_handle.clone();
-	for _ in 0..handle_size {
+	for _ in 0..char_size {
+		if handle.len() >= byte_size as usize {
+			break
+		}
 		handle.append(&mut base_handle.clone());
 	}
 	let handle_claims_payload = ClaimHandlePayload::new(handle);
@@ -40,11 +45,18 @@ fn create_signed_claims_payload<T: Config>(
 benchmarks! {
 	claim_handle {
 		// create a generic handle example with expanding size 3 to 32 bytes
-		let s in 2..HANDLE_BASE_BYTES_MAX;
+		let c in HANDLE_BASE_CHARS_MIN .. HANDLE_BASE_CHARS_MAX-1;
+		let b in HANDLE_BASE_BYTES_MIN .. HANDLE_BASE_BYTES_MAX-1;
 		let caller: T::AccountId = whitelisted_caller();
-		let (payload, proof, key) = create_signed_claims_payload::<T>(s);
+		let delegator_msa_id = 1u64;
+		let (payload, proof, key) = create_signed_claims_payload::<T>(c, b);
+		assert_ok!(T::MsaBenchmarkHelper::add_key(delegator_msa_id.into(), caller.clone()));
+		assert_ok!(T::MsaBenchmarkHelper::add_key(delegator_msa_id.into(), key.clone()));
+
 	}: _(RawOrigin::Signed(caller.clone()), key.clone(), proof, payload)
 	verify {
+		let stored_handle = Handles::<T>::get_handle_for_msa(delegator_msa_id.into());
+		assert!(stored_handle.is_some());
 	}
 
 	impl_benchmark_test_suite!(Handles, crate::mock::new_tester(), crate::mock::Test,);
