fcb7edc2ccd75d75419272eb7fb6b1e8fce462d7
Robert La Ferla <robert@onemsg.co>
2023-04-03 11:23:16 -0400

    Added claim_and_retire_handle_happy_path()

--- pallets/handles/src/tests/handle_retirements_tests.rs
+++ pallets/handles/src/tests/handle_retirements_tests.rs
@@ -1,8 +1,64 @@
 use crate::{tests::mock::*, Error};
-use common_primitives::{handles::*, utils::wrap_binary_data};
-use frame_support::assert_noop;
+use codec::Decode;
+use common_primitives::{handles::*, msa::MessageSourceId, utils::wrap_binary_data};
+use frame_support::{assert_noop, assert_ok};
 use sp_core::{sr25519, Encode, Pair};
 use sp_runtime::MultiSignature;
+use numtoa::*;
+
+#[test]
+fn claim_and_retire_handle_happy_path() {
+	new_test_ext().execute_with(|| {
+		let base_handle_str = "test1";
+
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) = get_signed_claims_payload(&alice, base_handle_str.as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof,
+			payload
+		));
+		let msa_id = MessageSourceId::decode(&mut &alice.public().encode()[..]).unwrap();
+		let handle = Handles::get_handle_for_msa(msa_id);
+		assert!(handle.is_some());
+
+		// Compose the full display handle from the base handle, "." delimeter and suffix
+		let handle_result = handle.unwrap();
+		let suffix = handle_result.suffix;
+		let mut full_handle_vec: Vec<u8> = vec![];
+		full_handle_vec.extend(base_handle_str.as_bytes());
+		full_handle_vec.extend(b"."); // The delimeter
+		let mut buff = [0u8; SUFFIX_MAX_DIGITS];
+		full_handle_vec.extend(suffix.numtoa(10, &mut buff)); // Use base 10
+
+		// Retirement Payload
+		let payload = RetireHandlePayload::new(full_handle_vec.clone());
+		let encoded_payload = wrap_binary_data(payload.encode());
+		let proof: MultiSignature = alice.sign(&encoded_payload).into();
+
+		// Retire the handle
+		assert_ok!(
+			Handles::retire_handle(
+				RuntimeOrigin::signed(alice.public().into()),
+				alice.public().into(),
+				proof.clone(),
+				payload.clone()
+			)
+		);
+
+		// Try to retire again which should fail
+		assert_noop!(
+			Handles::retire_handle(
+				RuntimeOrigin::signed(alice.public().into()),
+				alice.public().into(),
+				proof,
+				payload
+			),
+			Error::<Test>::MSAHandleDoesNotExist
+		);
+	});
+}
 
 #[test]
 fn retire_handle_no_handle() {
--- pallets/handles/src/tests/mod.rs
+++ pallets/handles/src/tests/mod.rs
@@ -1,7 +1,7 @@
 pub mod mock;
 
+mod converter_tests;
 mod handle_creation_tests;
 mod handle_retirements_tests;
 mod other_tests;
 mod suffix_creation_tests;
-mod converter_tests;
