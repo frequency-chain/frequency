915262a1498d1f72421167d7e93507c0e2c66313
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-03 08:22:57 -0500

    add more unit tests

--- pallets/handles/src/tests/handle_creation_tests.rs
+++ pallets/handles/src/tests/handle_creation_tests.rs
@@ -29,3 +29,81 @@ fn claim_handle() {
 		));
 	});
 }
+
+#[test]
+fn claim_handle_already_claimed() {
+	new_test_ext().execute_with(|| {
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) = get_signed_claims_payload(&alice, "test1".as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof,
+			payload.clone()
+		));
+
+		// Try to claim the same handle again with a different account
+		let (payload, proof) = get_signed_claims_payload(&alice, "test1".as_bytes().to_vec());
+		assert_noop!(
+			Handles::claim_handle(
+				RuntimeOrigin::signed(alice.public().into()),
+				alice.public().into(),
+				proof,
+				payload
+			),
+			Error::<Test>::MSAHandleAlreadyExists
+		);
+	});
+}
+
+#[test]
+fn claim_handle_already_claimed_with_different_case() {
+	new_test_ext().execute_with(|| {
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) = get_signed_claims_payload(&alice, "test1".as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof,
+			payload.clone()
+		));
+
+		// Try to claim the same handle again with a different account
+		let (payload, proof) = get_signed_claims_payload(&alice, "TEST1".as_bytes().to_vec());
+		assert_noop!(
+			Handles::claim_handle(
+				RuntimeOrigin::signed(alice.public().into()),
+				alice.public().into(),
+				proof,
+				payload
+			),
+			Error::<Test>::MSAHandleAlreadyExists
+		);
+	});
+}
+
+#[test]
+fn claim_handle_already_claimed_with_homoglyph() {
+	new_test_ext().execute_with(|| {
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) = get_signed_claims_payload(&alice, "test1".as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof,
+			payload.clone()
+		));
+
+		// Try to claim the same handle again with a different account
+		let (payload, proof) = get_signed_claims_payload(&alice, "tést1".as_bytes().to_vec());
+		assert_noop!(
+			Handles::claim_handle(
+				RuntimeOrigin::signed(alice.public().into()),
+				alice.public().into(),
+				proof,
+				payload
+			),
+			Error::<Test>::MSAHandleAlreadyExists
+		);
+	});
+}
