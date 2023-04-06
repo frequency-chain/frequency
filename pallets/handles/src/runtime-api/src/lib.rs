4aabb2a2dc333121e07b1a7e31d68dae7dab10c7
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-06 12:33:32 -0500

    fix ci

--- pallets/handles/src/runtime-api/src/lib.rs
+++ pallets/handles/src/runtime-api/src/lib.rs
@@ -20,6 +20,7 @@ use common_primitives::{
 	handles::{HandleResponse, HandleSuffix},
 	msa::MessageSourceId,
 };
+use sp_std::vec::Vec;
 
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime files (the `runtime` folder)
