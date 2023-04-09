4f59d3976fdd725ebd3dbf9aac7156db06d5d20d
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-08 19:40:35 -0500

    add more tests

--- pallets/handles/src/tests/handles_replay_attack_test.rs
+++ pallets/handles/src/tests/handles_replay_attack_test.rs
@@ -132,7 +132,119 @@ fn test_handle_retire_replay_attack() {
 		// Try to retire the same handle again which should fail
 		assert_noop!(
 			Handles::retire_handle(RuntimeOrigin::signed(alice.public().into()),),
-			Error::<Test>::HandleNotRegistered
+			Error::<Test>::MSAHandleDoesNotExist
 		);
+	});
+}
+
+#[test]
+fn test_handle_claim_replay_attack_with_different_payload() {
+	new_test_ext().execute_with(|| {
+		let base_handle_str = "test1";
+
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) =
+			get_signed_claims_payload(&alice, base_handle_str.as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof.clone(),
+			payload.clone()
+		));
+		let msa_id = MessageSourceId::decode(&mut &alice.public().encode()[..]).unwrap();
+		let handle = Handles::get_handle_for_msa(msa_id);
+		assert!(handle.is_some());
+
+		// Compose the full display handle from the base handle, "." delimeter and suffix
+		let handle_result = handle.unwrap();
+		let suffix: HandleSuffix = handle_result.suffix;
+		let mut full_handle_vec: Vec<u8> = vec![];
+		full_handle_vec.extend(base_handle_str.as_bytes());
+		full_handle_vec.extend(b"."); // The delimeter
+		let mut buff = [0u8; SUFFIX_MAX_DIGITS];
+		full_handle_vec.extend(suffix.numtoa(10, &mut buff)); // Use base 10
+
+		// Try to claim the same handle again with a different payload which should fail
+		let (payload2, proof2) = get_signed_claims_payload(&alice, b"another-handle".to_vec());
+		assert_noop!(
+			Handles::claim_handle(
+				RuntimeOrigin::signed(alice.public().into()),
+				alice.public().into(),
+				proof2,
+				payload2
+			),
+			Error::<Test>::MSAHandleAlreadyExists
+		);
+	});
+}
+
+#[test]
+fn test_handle_retire_replay_attack_with_different_account() {
+	new_test_ext().execute_with(|| {
+		let base_handle_str = "test1";
+
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) =
+			get_signed_claims_payload(&alice, base_handle_str.as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof.clone(),
+			payload.clone()
+		));
+		let msa_id = MessageSourceId::decode(&mut &alice.public().encode()[..]).unwrap();
+		let handle = Handles::get_handle_for_msa(msa_id);
+		assert!(handle.is_some());
+
+		// Try to retire the handle with a different account which should fail
+		let bob = sr25519::Pair::from_seed(&[1; 32]);
+		assert_noop!(
+			Handles::retire_handle(RuntimeOrigin::signed(bob.public().into()),),
+			Error::<Test>::MSAHandleDoesNotExist
+		);
+	});
+}
+
+#[test]
+fn test_handle_claim_and_retire_with_multiple_accounts() {
+	new_test_ext().execute_with(|| {
+		let base_handle_str = "test1";
+
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let (payload, proof) =
+			get_signed_claims_payload(&alice, base_handle_str.as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof.clone(),
+			payload.clone()
+		));
+
+		// Get suffix
+		let msa_id = MessageSourceId::decode(&mut &alice.public().encode()[..]).unwrap();
+		let handle = Handles::get_handle_for_msa(msa_id);
+		assert!(handle.is_some());
+		let handle_result = handle.unwrap();
+		let prev_suffix: HandleSuffix = handle_result.suffix;
+
+		// Try to claim the handle with a different account which should not fail
+		let bob = sr25519::Pair::from_seed(&[1; 32]);
+		let (payload2, proof2) =
+			get_signed_claims_payload(&bob, base_handle_str.as_bytes().to_vec());
+		assert_ok!(Handles::claim_handle(
+			RuntimeOrigin::signed(bob.public().into()),
+			bob.public().into(),
+			proof2,
+			payload2
+		));
+
+		// Suffix for the new account should be different than the previous one
+		let msa_id = MessageSourceId::decode(&mut &bob.public().encode()[..]).unwrap();
+		let handle = Handles::get_handle_for_msa(msa_id);
+		assert!(handle.is_some());
+		let handle_result = handle.unwrap();
+		let suffix: HandleSuffix = handle_result.suffix;
+
+		assert_ne!(prev_suffix, suffix);
 	});
 }
