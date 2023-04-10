a63b3afe1ca524b95abc87337ccb3105daab13a3
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-10 16:58:19 -0500

    Revert "purge not needed storage"
    This reverts commit ee35d81bce98ddfa434c87a2a535a19a0052afa9.

--- pallets/handles/src/lib.rs
+++ pallets/handles/src/lib.rs
@@ -114,8 +114,22 @@ pub mod pallet {
 	#[pallet::generate_store(pub (super) trait Store)]
 	pub struct Pallet<T>(_);
 
+	/// - Keys: k1: Canonical base handle, k2: Suffix
+	/// - Value: MSA id
+	#[pallet::storage]
+	#[pallet::getter(fn get_msa_id_for_canonical_and_suffix)]
+	pub type CanonicalBaseHandleAndSuffixToMSAId<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		Handle,
+		Twox64Concat,
+		HandleSuffix,
+		MessageSourceId,
+		OptionQuery,
+	>;
+
 	/// - Key: MSA id
-	/// - Value: Display name i.e. full handle ( base_handle + "." + suffix)
+	/// - Value: Display name
 	#[pallet::storage]
 	#[pallet::getter(fn get_display_name_for_msa_id)]
 	pub type MSAIdToDisplayName<T: Config> =
@@ -589,6 +603,12 @@ pub mod pallet {
 				suffix_sequence_index as usize,
 			);
 
+			// Store canonical handle and suffix to MSA id
+			CanonicalBaseHandleAndSuffixToMSAId::<T>::insert(
+				canonical_handle.clone(),
+				suffix,
+				delegator_msa_id,
+			);
 			// Store canonical handle to suffix sequence index
 			CanonicalBaseHandleToSuffixIndex::<T>::set(
 				canonical_handle.clone(),
@@ -639,7 +659,16 @@ pub mod pallet {
 			let is_past_min_lifetime = current_block >= expiration;
 			ensure!(is_past_min_lifetime, Error::<T>::HandleWithinMortalityPeriod);
 
+			let (base_handle_str, suffix_num) =
+				HandleConverter::split_display_name(display_name_str);
+
+			let canonical_handle_str = HandleConverter::convert_to_canonical(&base_handle_str);
+			let canonical_handle_vec = canonical_handle_str.as_bytes().to_vec();
+			let canonical_handle: Handle = canonical_handle_vec.try_into().unwrap();
+
+			// Remove handle from storage but not from CanonicalBaseHandleToSuffixIndex because retired handles can't be reused
 			MSAIdToDisplayName::<T>::remove(delegator_msa_id);
+			CanonicalBaseHandleAndSuffixToMSAId::<T>::remove(canonical_handle, suffix_num);
 
 			Ok(display_name_str.as_bytes().to_vec())
 		}
