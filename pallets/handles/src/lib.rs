64f35379d9fdf5bda050454a4e32efd29f9348ad
Robert La Ferla <robert@onemsg.co>
2023-04-10 15:48:45 -0400

    Fixed formatting.

--- pallets/handles/src/lib.rs
+++ pallets/handles/src/lib.rs
@@ -425,7 +425,8 @@ pub mod pallet {
 			let canonical_handle_vec = canonical_handle_str.as_bytes().to_vec();
 			let canonical_handle: Handle = canonical_handle_vec.try_into().unwrap();
 			let suffix_index =
-				Self::get_next_suffix_index_for_canonical_handle(canonical_handle.clone()).unwrap_or_default();
+				Self::get_next_suffix_index_for_canonical_handle(canonical_handle.clone())
+					.unwrap_or_default();
 
 			// Generate suffixes from the next available suffix index
 			let mut suffix_generator = SuffixGenerator::new(
