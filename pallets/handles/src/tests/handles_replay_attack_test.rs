756b4931e7b47f4f0385daaac27b5985281208c9
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-17 12:17:09 -0500

    better naming

--- pallets/handles/src/tests/handles_replay_attack_test.rs
+++ pallets/handles/src/tests/handles_replay_attack_test.rs
@@ -6,7 +6,7 @@ use numtoa::*;
 use sp_core::{sr25519, Encode, Pair};
 
 #[test]
-fn test_handle_claim_replay_attack_with_different_account() {
+fn test_claim_handle_fails_if_claim_is_replayed_with_different_account() {
 	new_test_ext().execute_with(|| {
 		let base_handle_str = "test1";
 
@@ -48,7 +48,7 @@ fn test_handle_claim_replay_attack_with_different_account() {
 }
 
 #[test]
-fn test_handle_claim_replay_attack_with_different_payload() {
+fn test_claim_handle_fails_with_replay_attack_using_different_payload() {
 	new_test_ext().execute_with(|| {
 		let base_handle_str = "test1";
 
@@ -91,7 +91,7 @@ fn test_handle_claim_replay_attack_with_different_payload() {
 }
 
 #[test]
-fn test_handle_retire_replay_attack_with_different_account() {
+fn test_retire_handle_fails_with_replay_attack_using_different_account() {
 	new_test_ext().execute_with(|| {
 		let base_handle_str = "test1";
 
@@ -119,52 +119,7 @@ fn test_handle_retire_replay_attack_with_different_account() {
 }
 
 #[test]
-fn test_base_handle_claim_and_retire_with_multiple_accounts() {
-	new_test_ext().execute_with(|| {
-		let base_handle_str = "test1";
-
-		let alice = sr25519::Pair::from_seed(&[0; 32]);
-		let expiration = 100;
-		let (payload, proof) =
-			get_signed_claims_payload(&alice, base_handle_str.as_bytes().to_vec(), expiration);
-		assert_ok!(Handles::claim_handle(
-			RuntimeOrigin::signed(alice.public().into()),
-			alice.public().into(),
-			proof.clone(),
-			payload.clone()
-		));
-
-		// Get suffix
-		let msa_id = MessageSourceId::decode(&mut &alice.public().encode()[..]).unwrap();
-		let handle = Handles::get_handle_for_msa(msa_id);
-		assert!(handle.is_some());
-		let handle_result = handle.unwrap();
-		let prev_suffix: HandleSuffix = handle_result.suffix;
-
-		// Try to claim the base andle with a different account which should not fail
-		let bob = sr25519::Pair::from_seed(&[1; 32]);
-		let (payload2, proof2) =
-			get_signed_claims_payload(&bob, base_handle_str.as_bytes().to_vec(), expiration);
-		assert_ok!(Handles::claim_handle(
-			RuntimeOrigin::signed(bob.public().into()),
-			bob.public().into(),
-			proof2,
-			payload2
-		));
-
-		// Suffix for the new account should be different than the previous one
-		let msa_id = MessageSourceId::decode(&mut &bob.public().encode()[..]).unwrap();
-		let handle = Handles::get_handle_for_msa(msa_id);
-		assert!(handle.is_some());
-		let handle_result = handle.unwrap();
-		let suffix: HandleSuffix = handle_result.suffix;
-
-		assert_ne!(prev_suffix, suffix);
-	});
-}
-
-#[test]
-fn test_handle_claim_for_other_msa_with_no_existing_handle() {
+fn test_claim_handle_fails_when_claiming_for_another_msa_with_no_existing_handle() {
 	new_test_ext().execute_with(|| {
 		let base_handle_str = "test1";
 
@@ -187,7 +142,7 @@ fn test_handle_claim_for_other_msa_with_no_existing_handle() {
 }
 
 #[test]
-fn test_handle_claim_proof_expired() {
+fn test_claim_handle_fails_when_proof_has_expired() {
 	new_test_ext().execute_with(|| {
 		let base_handle_str = "test1";
 
@@ -210,7 +165,7 @@ fn test_handle_claim_proof_expired() {
 }
 
 #[test]
-fn test_handle_claim_proof_not_yet_valid() {
+fn test_claim_handle_fails_when_proof_is_not_yet_valid() {
 	new_test_ext().execute_with(|| {
 		let base_handle_str = "test1";
 
