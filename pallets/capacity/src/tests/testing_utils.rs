ea99358ff0d542a1a0a811c1e85e3ba95fd9b247
Wil Wade <wil.wade@unfinished.com>
2023-04-21 11:32:45 -0400

    Capacity Epoch not incrementing when starting chain from block 0

--- pallets/capacity/src/tests/testing_utils.rs
+++ pallets/capacity/src/tests/testing_utils.rs
@@ -28,6 +28,16 @@ pub fn run_to_block(n: u64) {
 		Capacity::on_initialize(System::block_number());
 	}
 }
+// Remove capacity on_initialize, needed to emulate pre-existing blockheight
+pub fn system_run_to_block(n: u64) {
+	while System::block_number() < n {
+		if System::block_number() > 1 {
+			System::on_finalize(System::block_number());
+		}
+		System::set_block_number(System::block_number() + 1);
+		System::on_initialize(System::block_number());
+	}
+}
 
 pub fn register_provider(target_id: MessageSourceId, name: String) {
 	let name = Vec::from(name).try_into().expect("error");
