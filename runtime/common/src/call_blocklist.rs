f2e2543c99572f61abd318827fd7ef152ef70cf8
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-26 17:04:23 -0500

    clean

--- runtime/common/src/call_blocklist.rs
+++ runtime/common/src/call_blocklist.rs
@@ -1,17 +1,20 @@
-use sp_std::collections::btree_map::BTreeMap;
+use sp_std::{collections::btree_map::BTreeMap, vec::Vec};
 
 pub fn blocked_calls() -> BTreeMap<&'static str, Vec<&'static str>> {
 	let mut blocked_calls = BTreeMap::new();
-	blocked_calls.insert(
-		"Msa",
-		vec![
-			"create_provider",
-			"revoke_delegation_by_delegator",
-			"revoke_delegation_by_provider",
-			"delete_msa_public_key",
-			"retire_msa",
-		],
-	);
-	blocked_calls.insert("Handles", vec!["retire_handle"]);
+	// following msa calls are blocked for Msa pallet to be included in Utility batches
+	let mut block_list_msa = Vec::new();
+	block_list_msa.push("create_provider");
+	block_list_msa.push("revoke_delegation_by_delegator");
+	block_list_msa.push("revoke_delegation_by_provider");
+	block_list_msa.push("delete_msa_public_key");
+	block_list_msa.push("retire_msa");
+
+	// following handles calls are blocked for Msa pallet to be included in Utility batches
+	let mut block_list_handles = Vec::new();
+	block_list_handles.push("retire_handle");
+
+	blocked_calls.insert("Msa", block_list_msa);
+	blocked_calls.insert("Handles", block_list_handles);
 	blocked_calls
 }
