402792443830dfc18c28c0bfc99557bc2f926fda
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-27 12:00:14 -0500

    separate out tests

--- integration-tests/miscellaneous/utilityBatch.test.ts
+++ integration-tests/miscellaneous/utilityBatch.test.ts
@@ -25,15 +25,17 @@ describe("Utility Batch Filtering", function () {
         assert.notEqual(eventMap, undefined, "should return an eventMap");
     });
 
-    it("should fail to execute ❌ batch with disallowed calls", async function () {
+    it("should fail to execute ❌ batchAll with disallowed calls", async function () {
         // bad batch: with a mix of allowed and disallowed calls
         const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
         badBatch.push(ExtrinsicHelper.api.tx.balances.transfer(recipient.address, 1000))
         badBatch.push(ExtrinsicHelper.api.tx.system.remark("Hello From Batch"))
         badBatch.push(ExtrinsicHelper.api.tx.handles.retireHandle())
         badBatch.push(ExtrinsicHelper.api.tx.msa.retireMsa())
-        const batchAll = ExtrinsicHelper.executeUtilityBatchAll(sender, badBatch);
         let error: any;
+
+        // batchAll
+        const batchAll = ExtrinsicHelper.executeUtilityBatchAll(sender, badBatch);
         try {
             await batchAll.fundAndSend();
         } catch (err) {
@@ -41,28 +43,38 @@ describe("Utility Batch Filtering", function () {
             assert.notEqual(error, undefined, " batchAll should return an error");
         }
         assert.notEqual(error, undefined, " batchAll should return an error");
-
-        const batch = ExtrinsicHelper.executeUtilityBatch(sender, badBatch);
-        try {
-            await batch.fundAndSend();
-        }
-        catch (err) {
-            error = err;
-            assert.notEqual(error, undefined, "batch should return an error");
-        }
-        assert.notEqual(error, undefined, "batch should return an error");
-
-        const forceBatch = ExtrinsicHelper.executeUtilityForceBatch(sender, badBatch);
-        try {
-            await forceBatch.fundAndSend();
-        }
-        catch (err) {
-            error = err;
-            assert.notEqual(error, undefined, "forceBatch should return an error");
-        }
-        assert.notEqual(error, undefined, "forceBatch should return an error");
     });
-    
+
+    it("should fail to execute ❌ batch with disallowed calls", async function () {
+        // bad batch: with a mix of allowed and disallowed calls
+        const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        badBatch.push(ExtrinsicHelper.api.tx.balances.transfer(recipient.address, 1000))
+        badBatch.push(ExtrinsicHelper.api.tx.system.remark("Hello From Batch"))
+        badBatch.push(ExtrinsicHelper.api.tx.handles.retireHandle())
+        badBatch.push(ExtrinsicHelper.api.tx.msa.retireMsa())
+
+        // batch
+        const batch = ExtrinsicHelper.executeUtilityBatch(sender, badBatch);
+        let [ok, eventMap] = await batch.fundAndSend();
+        assert.equal(ok, undefined, "should not return an ok event");
+        assert.equal(eventMap["utility.BatchCompleted"], undefined, "should return an eventMap");
+    });
+
+    it("should fail to execute ❌ forceBatch with disallowed calls", async function () {
+        // bad batch: with a mix of allowed and disallowed calls
+        const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        badBatch.push(ExtrinsicHelper.api.tx.balances.transfer(recipient.address, 1000))
+        badBatch.push(ExtrinsicHelper.api.tx.system.remark("Hello From Batch"))
+        badBatch.push(ExtrinsicHelper.api.tx.handles.retireHandle())
+        badBatch.push(ExtrinsicHelper.api.tx.msa.retireMsa())
+
+        // forceBatch
+        const forceBatch = ExtrinsicHelper.executeUtilityForceBatch(sender, badBatch);
+        let [ok, eventMap] = await forceBatch.fundAndSend();
+        assert.equal(ok, undefined, "should not return an ok event");
+        assert.equal(eventMap["utility.BatchCompleted"], undefined, "should return an eventMap");
+    });
+
     it("should fail to execute ❌ batch  with `Pays::No` calls", async function () {
         // bad batch: with frequency related Pays::No call
         const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
