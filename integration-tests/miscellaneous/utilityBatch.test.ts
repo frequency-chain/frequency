69f820f94321f91d31d238da99d815e2de1b3c62
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-04-27 10:17:55 -0500

    add targetted integration tests

--- integration-tests/miscellaneous/utilityBatch.test.ts
+++ integration-tests/miscellaneous/utilityBatch.test.ts
@@ -41,5 +41,68 @@ describe("Utility Batch Filtering", function () {
             assert.notEqual(error, undefined, "should return an error");
         }
         assert.notEqual(error, undefined, "should return an error");
-    });    
+    });
+    
+    it("should fail to execute ❌ batch  with `Pays::No` calls", async function () {
+        // bad batch: with frequency related Pays::No call
+        const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        badBatch.push(ExtrinsicHelper.api.tx.msa.retireMsa())
+        const batch = ExtrinsicHelper.executeUtilityBatchAll(sender, badBatch);
+        let error: any;
+        try {
+            await batch.fundAndSend();
+        } catch (err) {
+            error = err;
+            assert.notEqual(error, undefined, "should return an error");
+        }
+        assert.notEqual(error, undefined, "should return an error");    
+    });
+
+    it("should fail to execute ❌ batch with `Pays::Yes` `create_provider`call blocked by Frequency", async function () {
+        // bad batch: with frequency related Pays::Yes call
+        const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        badBatch.push(ExtrinsicHelper.api.tx.msa.createProvider("I am a ba(tch)d provider"))
+        const batch = ExtrinsicHelper.executeUtilityBatchAll(sender, badBatch);
+        let error: any;
+        try {
+            await batch.fundAndSend();
+        } catch (err) {
+            error = err;
+            assert.notEqual(error, undefined, "should return an error");
+        }
+        assert.notEqual(error, undefined, "should return an error");
+    });
+
+    it("should fail to execute ❌ batch with `Pays::Yes` `create_schema` call blocked by Frequency", async function () {
+        // bad batch: with frequency related Pays::Yes call
+        const badBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        badBatch.push(ExtrinsicHelper.api.tx.msa.createProvider("I am a ba(tch)d provider"))
+        const batch = ExtrinsicHelper.executeUtilityBatchAll(sender, badBatch);
+        let error: any;
+        try {
+            await batch.fundAndSend();
+        } catch (err) {
+            error = err;
+            assert.notEqual(error, undefined, "should return an error");
+        }
+        assert.notEqual(error, undefined, "should return an error");
+    });
+
+    it("should fail to execute ❌ batch with nested batch", async function () {
+        // batch with nested batch
+        const nestedBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        const innerBatch: SubmittableExtrinsic<ApiTypes>[] = [];
+        innerBatch.push(ExtrinsicHelper.api.tx.balances.transfer(recipient.address, 1000))
+        innerBatch.push(ExtrinsicHelper.api.tx.system.remark("Hello From Batch"))
+        nestedBatch.push(ExtrinsicHelper.api.tx.utility.batch(innerBatch))
+        const batch = ExtrinsicHelper.executeUtilityBatchAll(sender, nestedBatch);
+        let error: any;
+        try {
+            await batch.fundAndSend();
+        } catch (err) {
+            error = err;
+            assert.notEqual(error, undefined, "should return an error");
+        }
+        assert.notEqual(error, undefined, "should return an error");
+    });
 });
