e3620b79f3a5e6d1efc6d42b540ea45d64787eea
Robert La Ferla <robert@onemsg.co>
2023-05-03 11:07:32 -0400

    fix: increment spec_version because metadata is newer

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -467,7 +467,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: spec_name!("frequency"),
 	impl_name: create_runtime_str!("frequency"),
 	authoring_version: 1,
-	spec_version: 33,
+	spec_version: 34,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
