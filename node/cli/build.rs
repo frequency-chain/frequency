845809084aa84db819918cc4e6ca1578f72a3350
Robert La Ferla <robert@onemsg.co>
2023-05-08 17:17:19 -0400

    build: Temporarily commented out feature conflict check

--- node/cli/build.rs
+++ node/cli/build.rs
@@ -1,20 +1,22 @@
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
-// // Don't allow both frequency and all-frequency-features so that we always have a good mainnet runtime
-#[cfg(all(
-	feature = "frequency",
-	feature = "all-frequency-features",
-	not(feature = "runtime-benchmarks")
-))]
-compile_error!("feature \"frequency\" and feature \"all-frequency-features\" cannot be enabled at the same time");
+// NOTE: These checks are commented out temporarily and will be addressed in a future issue/PR
+//
+// Don't allow both frequency and all-frequency-features so that we always have a good mainnet runtime
+// #[cfg(all(
+// 	feature = "frequency",
+// 	feature = "all-frequency-features",
+// 	not(feature = "runtime-benchmarks")
+// ))]
+// compile_error!("feature \"frequency\" and feature \"all-frequency-features\" cannot be enabled at the same time");
 
-// // Don't allow both frequency-no-relay and all-frequency-features so that we always have a good mainnet runtime
-#[cfg(all(
-	feature = "frequency",
-	feature = "all-frequency-features",
-	not(feature = "runtime-benchmarks")
-))]
-compile_error!("feature \"frequency-no-relay\" and feature \"all-frequency-features\" cannot be enabled at the same time");
+// Don't allow both frequency-no-relay and all-frequency-features so that we always have a good mainnet runtime
+// #[cfg(all(
+// 	feature = "frequency",
+// 	feature = "all-frequency-features",
+// 	not(feature = "runtime-benchmarks")
+// ))]
+// compile_error!("feature \"frequency-no-relay\" and feature \"all-frequency-features\" cannot be enabled at the same time");
 
 fn main() {
 	generate_cargo_keys();
