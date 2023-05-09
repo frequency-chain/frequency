86910e714d62efcdd33851a06175073506e881fe
Robert La Ferla <robert@onemsg.co>
2023-05-09 13:25:44 -0400

    fix: added other feature flags for conflict check

--- node/cli/build.rs
+++ node/cli/build.rs
@@ -10,11 +10,27 @@ compile_error!("feature \"frequency\" and \"all-frequency-features\" cannot be e
 
 // Don't allow both frequency-no-relay and all-frequency-features (except for lint/check) so that we always have a good mainnet runtime
 #[cfg(all(
-	feature = "frequency",
+	feature = "frequency-no-relay",
 	feature = "all-frequency-features",
 	not(feature = "frequency-lint-check")
 ))]
-compile_error!("feature \"frequency\" and \"all-frequency-features\" cannot be enabled at the same time except for lint/check with \"frequency-lint-check\"");
+compile_error!("feature \"frequency-no-relay\" and \"all-frequency-features\" cannot be enabled at the same time except for lint/check with \"frequency-lint-check\"");
+
+// Don't allow both frequency-rococo-local and all-frequency-features (except for lint/check) so that we always have a good mainnet runtime
+#[cfg(all(
+	feature = "frequency-rococo-local",
+	feature = "all-frequency-features",
+	not(feature = "frequency-lint-check")
+))]
+compile_error!("feature \"frequency-rococo-local\" and \"all-frequency-features\" cannot be enabled at the same time except for lint/check with \"frequency-lint-check\"");
+
+// Don't allow both frequency-rococo-testnet and all-frequency-features (except for lint/check) so that we always have a good mainnet runtime
+#[cfg(all(
+	feature = "frequency-rococo-testnet",
+	feature = "all-frequency-features",
+	not(feature = "frequency-lint-check")
+))]
+compile_error!("feature \"frequency-rococo-testnet\" and \"all-frequency-features\" cannot be enabled at the same time except for lint/check with \"frequency-lint-check\"");
 
 fn main() {
 	generate_cargo_keys();
