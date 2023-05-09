1b8e681ec5d5d27807e9c10f157333108171ff2f
Robert La Ferla <robert@onemsg.co>
2023-05-09 14:17:11 -0400

    fix: fixed typo

--- node/cli/build.rs
+++ node/cli/build.rs
@@ -2,11 +2,15 @@ use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_change
 
 // Don't allow both frequency and all-frequency-features (except for lint/check) so that we always have a good mainnet runtime
 #[cfg(all(
-	not(feature="frequency-lint-check"),
-	feature=""all-frequency-features,
+	not(feature = "frequency-lint-check"),
+	feature = "all-frequency-features",
 	any(
-	  feature="frequency", feature="frequency-no-relay", feature="frequency-rococo-local", feature="frequency-rococo-testnet"
-  )))]
+		feature = "frequency",
+		feature = "frequency-no-relay",
+		feature = "frequency-rococo-local",
+		feature = "frequency-rococo-testnet"
+	)
+))]
 compile_error!("\"all-frequency-features\" cannot be enabled at the same time as another main feature except for a lint/check with \"frequency-lint-check\"");
 
 fn main() {
