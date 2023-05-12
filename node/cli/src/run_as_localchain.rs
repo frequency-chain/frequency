b27a9ca54a97f28a904700726d9cfa73bd212474
Robert La Ferla <robert@onemsg.co>
2023-05-12 11:52:17 -0400

    fix: more concise conditionals

--- node/cli/src/run_as_localchain.rs
+++ node/cli/src/run_as_localchain.rs
@@ -14,12 +14,12 @@ pub fn run_as_localchain(cli: Cli) -> sc_service::Result<(), sc_cli::Error> {
 	let runner = cli.create_runner(&cli.run.normalize())?;
 
 	runner.run_node_until_exit(|config| async move {
-		if cli.instant_sealing == true {
+		if cli.instant_sealing {
 			log::error!(
 				"The --instant-sealing option is deprecated.  Use --sealing=instant instead."
 			);
 			process::exit(1);
-		} else if cli.manual_sealing == true {
+		} else if cli.manual_sealing {
 			log::error!(
 				"The --manual-sealing option is deprecated.  Use --sealing=manual instead."
 			);
