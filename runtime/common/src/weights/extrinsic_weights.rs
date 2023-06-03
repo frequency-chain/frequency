0fdb9acb93327511022033e0385551088bc4d626
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2023-06-02 18:30:18 -0600

    fix: debug

--- runtime/common/src/weights/extrinsic_weights.rs
+++ runtime/common/src/weights/extrinsic_weights.rs
@@ -1,4 +1,3 @@
-
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
 //! DATE: 2023-06-02 (Y/M/D)
 //! HOSTNAME: `benchmark-runner-qpqf8-4dvlm`, CPU: `Intel(R) Xeon(R) Platinum 8375C CPU @ 2.90GHz`
