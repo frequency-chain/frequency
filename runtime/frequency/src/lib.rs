36e86454455ebd54f5ec4237013b3079dca311ef
Shannon Wells <shannonwells@users.noreply.github.com>
2023-06-14 16:07:25 -0700

    Feat/staking rewards rewards provider #1572 (#1598)
    The goal of this PR is to implement a really basic version of the
    StakingRewardsProvider in the Capacity pallet and in the test mock,
    neither of which is actively used.
    
    Closes #1572
    
    Does not include anything to do with setting and storing RewardPoolInfo
    when each new Era starts.
    
    - [x] Design doc(s) updated
    - [x] Tests added

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -94,7 +94,6 @@ pub use common_runtime::{
 };
 use frame_support::traits::Contains;
 
-// use common_primitives::capacity::StakingRewardsProvider;
 #[cfg(feature = "try-runtime")]
 use frame_support::traits::{TryStateSelect, UpgradeCheckSelect};
 
@@ -504,7 +503,7 @@ impl pallet_capacity::Config for Runtime {
 	type RewardEra = RewardEra;
 	type EraLength = ConstU32<{ 14 * DAYS }>;
 	type StakingRewardsPastErasMax = ConstU32<26u32>; // 1 year
-												  // type RewardsProvider = StakingRewardsProvider;
+	type RewardsProvider = Capacity;
 }
 
 impl pallet_schemas::Config for Runtime {
