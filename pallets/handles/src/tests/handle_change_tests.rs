bdd8daf7fafd096d611022f02dccc788121156cd
Robert La Ferla <robert@onemsg.co>
2023-07-28 11:56:50 -0400

    feat: Added unhappy path to unit tests

--- pallets/handles/src/tests/handle_change_tests.rs
+++ pallets/handles/src/tests/handle_change_tests.rs
@@ -1,13 +1,13 @@
-use crate::{tests::mock::*, Event};
+use crate::{tests::mock::*, Error, Event};
 use codec::Decode;
 use common_primitives::msa::MessageSourceId;
-use frame_support::assert_ok;
+use frame_support::{assert_err, assert_ok};
 use sp_core::{sr25519, Encode, Pair};
 
 #[test]
 fn change_handle_happy_path() {
 	new_test_ext().execute_with(|| {
-		let handle = "test1";
+		let handle = "MyHandle";
 		let alice = sr25519::Pair::from_seed(&[0; 32]);
 		let expiry = 100;
 		let (payload, proof) =
@@ -24,10 +24,10 @@ fn change_handle_happy_path() {
 		let full_handle = create_full_handle_for_index(handle, 0);
 		System::assert_last_event(Event::HandleClaimed { msa_id, handle: full_handle }.into());
 
-		let new_handle = "test2";
+		let new_handle = "MyNewHandle";
 		let (payload, proof) =
 			get_signed_claims_payload(&alice, new_handle.as_bytes().to_vec(), expiry);
-        assert_ok!(Handles::change_handle(
+		assert_ok!(Handles::change_handle(
 			RuntimeOrigin::signed(alice.public().into()),
 			alice.public().into(),
 			proof,
@@ -35,5 +35,22 @@ fn change_handle_happy_path() {
 		));
 		let changed_handle = create_full_handle_for_index(new_handle, 0);
 		System::assert_last_event(Event::HandleClaimed { msa_id, handle: changed_handle }.into());
+	});
+}
+
+#[test]
+fn change_handle_no_handle() {
+	new_test_ext().execute_with(|| {
+		let handle = "MyNewHandleWithoutAnOldOne";
+		let alice = sr25519::Pair::from_seed(&[0; 32]);
+		let expiry = 100;
+		let (payload, proof) =
+			get_signed_claims_payload(&alice, handle.as_bytes().to_vec(), expiry);
+		assert_err!(Handles::change_handle(
+			RuntimeOrigin::signed(alice.public().into()),
+			alice.public().into(),
+			proof,
+			payload
+		), Error::<Test>::MSAHandleDoesNotExist);
 	});
 }
