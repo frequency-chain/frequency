e729d20e50a0f48e2db81ff6186d3142e2cece83
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-09-19 13:13:27 -0500

    update spec version for rococo

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -263,7 +263,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("frequency-rococo"),
 	impl_name: create_runtime_str!("frequency"),
 	authoring_version: 1,
-	spec_version: 53,
+	spec_version: 54,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
