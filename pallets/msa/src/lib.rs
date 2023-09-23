160dda40a04165d819eedb36597b3ae96c85edf2
Joe Caputo <joseph.caputo@unfinished.com>
2023-09-23 17:49:01 -0400

    fix: relect complete delegation revocation in individual schema revoked_at block numbers

--- pallets/msa/src/lib.rs
+++ pallets/msa/src/lib.rs
@@ -1329,7 +1329,13 @@ impl<T: Config> Pallet<T> {
 
 		let mut schema_list = Vec::new();
 		for (schema_id, revoked_at) in schema_permissions {
-			schema_list.push(SchemaGrant { schema_id, revoked_at });
+			let mut actual_revoked_at = revoked_at;
+			if provider_info.revoked_at > T::BlockNumber::zero() &&
+				(revoked_at > provider_info.revoked_at || revoked_at == T::BlockNumber::zero())
+			{
+				actual_revoked_at = provider_info.revoked_at;
+			}
+			schema_list.push(SchemaGrant { schema_id, revoked_at: actual_revoked_at });
 		}
 		Ok(Some(schema_list))
 	}
