79a96d026782a85647942780e9415beb2e042c52
Wil Wade <wil.wade@unfinished.com>
2023-10-17 17:25:55 -0400

    Adding try catches around the rootHooks

--- e2e/scaffolding/rootHooks.ts
+++ e2e/scaffolding/rootHooks.ts
@@ -5,15 +5,23 @@ import { getRootFundingSource } from "./funding";
 
 export const mochaHooks = {
   async beforeAll() {
-    await cryptoWaitReady();
-    await ExtrinsicHelper.initialize();
+    try {
+      await cryptoWaitReady();
+      await ExtrinsicHelper.initialize();
+    } catch(e) {
+      console.error("Failed to run beforeAll root hook: ", e);
+    }
   },
   async afterAll() {
-    // Any key created using helpers `createKeys` is kept in the module
-    // then any value remaining is drained here at the end
-    const rootAddress = getRootFundingSource().keys.address;
-    await drainFundedKeys(rootAddress);
-    await ExtrinsicHelper.api.disconnect();
-    await ExtrinsicHelper.apiPromise.disconnect();
+    try {
+      // Any key created using helpers `createKeys` is kept in the module
+      // then any value remaining is drained here at the end
+      const rootAddress = getRootFundingSource().keys.address;
+      await drainFundedKeys(rootAddress);
+      await ExtrinsicHelper.api.disconnect();
+      await ExtrinsicHelper.apiPromise.disconnect();
+    } catch(e) {
+      console.error("Failed to run afterAll root hook: ", e);
+    }
   }
 }
