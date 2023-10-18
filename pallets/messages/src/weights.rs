9e9fafdf8b817814ee1d24209784e171d88dd356
Aramik <aramikm@gmail.com>
2023-10-18 13:07:03 -0700

    Run benchmarks [run-benchmarks messages]

--- pallets/messages/src/weights.rs
+++ pallets/messages/src/weights.rs
@@ -40,6 +40,7 @@
 // --output=./scripts/../pallets/messages/src/weights.rs
 // --template=./scripts/../.maintain/frame-weight-template.hbs
 
+
 #![cfg_attr(rustfmt, rustfmt_skip)]
 #![allow(unused_parens)]
 #![allow(unused_imports)]
