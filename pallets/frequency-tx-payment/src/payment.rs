cda4bc464e076fa8cf5f4d6cfeec30c468f8a99a
Matthew Orris <--help>
2023-11-21 14:48:52 -0700

    fix: lints

--- pallets/frequency-tx-payment/src/payment.rs
+++ pallets/frequency-tx-payment/src/payment.rs
@@ -1,5 +1,5 @@
 use common_primitives::msa::MsaValidator;
-use frame_support::traits::tokens::{Balance, fungible::Inspect as InspectFungible};
+use frame_support::traits::tokens::{fungible::Inspect as InspectFungible, Balance};
 use sp_std::marker::PhantomData;
 
 use super::*;
