169e0a4a4078f8e16831ab2331b0fb76451d0f22
shannonwells <shannonwells@users.noreply.github.com>
2023-12-12 14:42:42 -0800

    updates after rebase

--- common/primitives/src/capacity.rs
+++ common/primitives/src/capacity.rs
@@ -1,8 +1,5 @@
 use crate::msa::MessageSourceId;
-use codec::{Encode, MaxEncodedLen};
 use frame_support::traits::tokens::Balance;
-use scale_info::TypeInfo;
-use sp_api::Decode;
 use sp_runtime::DispatchError;
 
 /// A trait for checking that a target MSA can be staked to.
@@ -54,22 +51,4 @@ pub trait Replenishable {
 
 	/// Checks if an account can be replenished.
 	fn can_replenish(msa_id: MessageSourceId) -> bool;
-}
-
-#[derive(
-	Clone, Copy, Debug, Decode, Encode, TypeInfo, Eq, MaxEncodedLen, PartialEq, PartialOrd,
-)]
-/// The type of staking a given Staking Account is doing.
-pub enum StakingType {
-	/// Staking account targets Providers for capacity only, no token reward
-	MaximumCapacity,
-	/// Staking account targets Providers and splits reward between capacity to the Provider
-	/// and token for the account holder
-	ProviderBoost,
-}
-
-impl Default for StakingType {
-	fn default() -> Self {
-		StakingType::MaximumCapacity
-	}
 }
