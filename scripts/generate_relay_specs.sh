ef20bcd1ad7efb1d063d1ea5c385e3b0660bd131
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2023-12-26 08:34:40 -0700

    fix: Update Docker image version and relay chain spec (#1808)
    # Goal
    Issue #1801 reported that the local parachain wasn't making blocks when
    following the directions for setting up a local relay chain.
    `rococo-local.json`, the chain spec file used to setup the local relay
    chain development environment was out of date with the recent versions
    of the polkadot-sdk and needed to be updated.
    
    Closes #1801 
    
    # Checklist
    - [ ] Chain spec updated
    - [ ] Custom RPC OR Runtime API added/changed? Updated js/api-augment.
    - [ ] Design doc(s) updated
    - [ ] Tests added
    - [ ] Benchmarks added
    - [ ] Weights updated

--- scripts/generate_relay_specs.sh
+++ scripts/generate_relay_specs.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker run parity/polkadot:v1.0.0 build-spec --disable-default-bootnode --chain rococo-local --raw > ./resources/rococo-local.json
+docker run parity/polkadot:v1.1.0 build-spec --disable-default-bootnode --chain rococo-local --raw > ./resources/rococo-local.json
 
