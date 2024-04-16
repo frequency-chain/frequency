7b60a18041a118cde0f6e85ea75ffa08275ea201
Wil Wade <wil.wade@amplica.io>
2024-04-16 10:03:50 -0400

    CI update: e2e Testnet Paseo (#1934)
    # Goal
    The goal of this PR is to add e2e CI tests against Testnet Paseo.
    
    Tests passing in CI:
    https://github.com/LibertyDSNP/frequency/actions/runs/8636089744
    
    Closes #1898 
    
    # Checklist
    - [x] Tests added
    
    ## Things Done
    - Added new paseo e2e tests
    - Update e2e to use Node 20
    - Switch to use tsx instead of ts-node in both e2e and api-augment
    - Update packages for both e2e and api-augment
    
    ---------
    
    Co-authored-by: Joe Caputo <joseph.caputo@unfinished.com>

--- e2e/scaffolding/env.ts
+++ e2e/scaffolding/env.ts
@@ -4,11 +4,18 @@ export const verbose = process.env.VERBOSE_TESTS === 'true' || process.env.VERBO
 const CHAIN_ENVIRONMENT = {
   DEVELOPMENT: 'dev',
   ROCOCO_TESTNET: 'rococo-testnet',
+  PASEO_TESTNET: 'paseo-testnet',
   ROCOCO_LOCAL: 'rococo-local',
+  PASEO_LOCAL: 'paseo-local',
 };
 
 export function isTestnet() {
-  return process.env.CHAIN_ENVIRONMENT === CHAIN_ENVIRONMENT.ROCOCO_TESTNET;
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+      return true;
+  }
+  return false;
 }
 
 export function isDev() {
@@ -16,8 +23,61 @@ export function isDev() {
 }
 
 export function hasRelayChain() {
-  return (
-    process.env.CHAIN_ENVIRONMENT === CHAIN_ENVIRONMENT.ROCOCO_LOCAL ||
-    process.env.CHAIN_ENVIRONMENT === CHAIN_ENVIRONMENT.ROCOCO_TESTNET
-  );
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+    case CHAIN_ENVIRONMENT.ROCOCO_LOCAL:
+    case CHAIN_ENVIRONMENT.PASEO_LOCAL:
+      return true;
+  }
+  return false;
+}
+
+export function getGraphChangeSchema() {
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+      return 53;
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+      return 8;
+  }
+  return null;
+}
+export function getBroadcastSchema() {
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+      return 51;
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+      return 2;
+  }
+  return null;
+}
+
+export function getDummySchema() {
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+      return 52;
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+      return 12;
+  }
+  return null;
+}
+
+export function getAvroChatMessagePaginatedSchema() {
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+      return 55;
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+      return 14;
+  }
+  return null;
+}
+
+export function getAvroChatMessageItemizedSchema() {
+  switch (process.env.CHAIN_ENVIRONMENT) {
+    case CHAIN_ENVIRONMENT.ROCOCO_TESTNET:
+      return 54;
+    case CHAIN_ENVIRONMENT.PASEO_TESTNET:
+      return 13;
+  }
+  return null;
 }
