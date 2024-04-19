6648d2023e80c111b1726160316c56136ed9127c
Wil Wade <wil.wade@amplica.io>
2024-04-19 20:03:13 +0000

    Add Proxy Pallet

--- runtime/common/src/weights/mod.rs
+++ runtime/common/src/weights/mod.rs
@@ -26,6 +26,7 @@ pub mod pallet_collective_technical_committee;
 pub mod pallet_democracy;
 pub mod pallet_multisig;
 pub mod pallet_preimage;
+pub mod pallet_proxy;
 pub mod pallet_scheduler;
 pub mod pallet_session;
 pub mod pallet_timestamp;
