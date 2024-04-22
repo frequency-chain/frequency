3734db9769eeaa4976ad950a059ec0639460e7b0
shannonwells <shannonwells@users.noreply.github.com>
2024-04-22 13:48:52 -0700

    cleanup for PR

--- pallets/capacity/src/tests/provider_boost_tests.rs
+++ pallets/capacity/src/tests/provider_boost_tests.rs
@@ -27,11 +27,7 @@ fn provider_boost_works() {
 		// Check that the capacity generated is correct. (5% of amount staked, since 10% is what's in the mock)
 		let capacity_details = Capacity::get_capacity_for(target).unwrap();
 		assert_eq!(capacity_details.total_capacity_issued, 1u64);
-
-		// assert_eq!(boost_account.unlocking.len(), 0);
-		// assert_eq!(staking_account.last_rewards_claimed_at, None);
-		// assert_eq!(staking_account.stake_change_unlocking.len(), 0);
-
+		
 		let events = staking_events();
 		assert_eq!(
 			events.first().unwrap(),
@@ -58,35 +54,7 @@ fn provider_boost_updates_boost_account_details() {
 		let boost_details: StakingDetails<Test> =
 			Capacity::get_staking_account_for(account).unwrap();
 		assert_eq!(boost_details.active, 500);
-
-		// TODO: staking history checks
-		// let staking_history: StakingHistory<Test> = Capacity::get_staking_history_for(account).unwrap();
-		// assert!(staking_history.last_rewards_claimed_at.is_none());
-		// assert_eq!(staking_history.boost_history.len(), 1);
-		// let expected_history = StakingHistory { reward_era: 0, total_staked: 500 };
-		// let actual_history = staking_history.get(0).unwrap();
-		// assert_eq!(actual_history, &expected_history);
 	})
-}
-
-#[test]
-fn provider_boost_adjusts_reward_pool_total() {
-	new_test_ext().execute_with(|| {
-		// TODO: when work resumes on reward pool branch
-		// let account = 600;
-		// let target: MessageSourceId = 1;
-		// let amount = 500;
-		// register_provider(target, String::from("Foo"));
-		// assert_ok!(Capacity::provider_boost(RuntimeOrigin::signed(account), target, amount));
-		//
-		// let reward_pool_info = Capacity::get_reward_pool_for_era(0).unwrap();
-		// assert_eq!(reward_pool_info, RewardPoolInfo {
-		// 	total_staked_token: 500,
-		// 	total_reward_pool: 50,
-		// 	unclaimed_balance: 50,
-		// });
-		assert!(true);
-	});
 }
 
 #[test]
