037f9dd44abc64379a4881b08c6e1fe4b6e0933f
shannonwells <shannonwells@users.noreply.github.com>
2024-04-29 16:13:14 -0700

    explain 'magic' constant

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -496,6 +496,7 @@ impl pallet_capacity::Config for Runtime {
 	type StakingRewardsPastErasMax = ConstU32<30u32>;
 	type RewardsProvider = Capacity;
 	type MaxRetargetsPerRewardEra = ConstU32<16>;
+	// Value determined by desired inflation rate limits for chosen economic model
 	type RewardPoolEachEra = ConstU128<{ currency::CENTS.saturating_mul(172_602_740u128) }>;
 	type RewardPercentCap = CapacityRewardCap;
 }
