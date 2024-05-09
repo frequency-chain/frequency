fadcd269971ab50d692b6a0590edd3b71775fcd3
Wil Wade <wil.wade@amplica.io>
2024-05-09 08:45:45 -0400

    Use the same prettierrc for both e2e and api-augment

--- js/api-augment/definitions/messages.ts
+++ js/api-augment/definitions/messages.ts
@@ -1,41 +1,41 @@
 export default {
   rpc: {
     getBySchemaId: {
-      description: "Get messages by schemaId paginated",
+      description: 'Get messages by schemaId paginated',
       params: [
         {
-          name: "schema_id",
-          type: "SchemaId",
+          name: 'schema_id',
+          type: 'SchemaId',
         },
         {
-          name: "pagination",
-          type: "BlockPaginationRequest",
+          name: 'pagination',
+          type: 'BlockPaginationRequest',
         },
       ],
-      type: "BlockPaginationResponseMessage",
+      type: 'BlockPaginationResponseMessage',
     },
   },
   types: {
     BlockPaginationRequest: {
-      from_block: "BlockNumber", // inclusive
-      from_index: "u32", // starts from 0
-      to_block: "BlockNumber", // exclusive
-      page_size: "u32",
+      from_block: 'BlockNumber', // inclusive
+      from_index: 'u32', // starts from 0
+      to_block: 'BlockNumber', // exclusive
+      page_size: 'u32',
     },
     MessageResponse: {
-      payload: "Option<Vec<u8>>", //  Serialized data in a user-defined schema format
-      cid: "Option<Vec<u8>>", // The content address for an IPFS payload
-      provider_msa_id: "MessageSourceId", //  Message source account id of the Provider
-      msa_id: "Option<MessageSourceId>", //  Message source account id (the original source)
-      index: "u16", // index in block to get total order
-      block_number: "BlockNumber",
-      payload_length: "Option<u32>", // Length of IPFS payload file
+      payload: 'Option<Vec<u8>>', //  Serialized data in a user-defined schema format
+      cid: 'Option<Vec<u8>>', // The content address for an IPFS payload
+      provider_msa_id: 'MessageSourceId', //  Message source account id of the Provider
+      msa_id: 'Option<MessageSourceId>', //  Message source account id (the original source)
+      index: 'u16', // index in block to get total order
+      block_number: 'BlockNumber',
+      payload_length: 'Option<u32>', // Length of IPFS payload file
     },
     BlockPaginationResponseMessage: {
-      content: "Vec<MessageResponse>",
-      has_next: "bool",
-      next_block: "Option<BlockNumber>",
-      next_index: "Option<u32>",
+      content: 'Vec<MessageResponse>',
+      has_next: 'bool',
+      next_block: 'Option<BlockNumber>',
+      next_index: 'Option<u32>',
     },
   },
   runtime: {
@@ -43,32 +43,32 @@ export default {
       {
         methods: {
           get_messages_by_schema_and_block: {
-            description: "Retrieve the messages for a particular schema and block number",
+            description: 'Retrieve the messages for a particular schema and block number',
             params: [
               {
-                name: "schema_id",
-                type: "SchemaId",
+                name: 'schema_id',
+                type: 'SchemaId',
               },
               {
-                name: "schema_payload_location",
-                type: "PayloadLocation",
+                name: 'schema_payload_location',
+                type: 'PayloadLocation',
               },
               {
-                name: "block_number",
-                type: "BlockNumber",
+                name: 'block_number',
+                type: 'BlockNumber',
               },
             ],
-            type: "Vec<MessageResponse>",
+            type: 'Vec<MessageResponse>',
           },
           get_schema_by_id: {
-            description: "Retrieve a schema by id",
+            description: 'Retrieve a schema by id',
             params: [
               {
-                name: "schema_id",
-                type: "SchemaId",
+                name: 'schema_id',
+                type: 'SchemaId',
               },
             ],
-            type: "Option<SchemaResponse>",
+            type: 'Option<SchemaResponse>',
           },
         },
         version: 1,
--- js/api-augment/definitions/msa.ts
+++ js/api-augment/definitions/msa.ts
@@ -1,63 +1,63 @@
 export default {
   rpc: {
     checkDelegations: {
-      description: "Test a list of MSAs to see if they have delegated to the provider MSA",
+      description: 'Test a list of MSAs to see if they have delegated to the provider MSA',
       params: [
         {
-          name: "delegator_msa_ids",
-          type: "Vec<DelegatorId>",
+          name: 'delegator_msa_ids',
+          type: 'Vec<DelegatorId>',
         },
         {
-          name: "provider_msa_id",
-          type: "ProviderId",
+          name: 'provider_msa_id',
+          type: 'ProviderId',
         },
         {
-          name: "block_number",
-          type: "BlockNumber",
+          name: 'block_number',
+          type: 'BlockNumber',
         },
         {
-          name: "schema_id",
-          type: "Option<SchemaId>",
+          name: 'schema_id',
+          type: 'Option<SchemaId>',
         },
       ],
-      type: "Vec<(DelegatorId, bool)>",
+      type: 'Vec<(DelegatorId, bool)>',
     },
     grantedSchemaIdsByMsaId: {
-      description: "Fetch the list of schema ids that a delegator has granted to provider",
+      description: 'Fetch the list of schema ids that a delegator has granted to provider',
       params: [
         {
-          name: "delegator_msa_id",
-          type: "DelegatorId",
+          name: 'delegator_msa_id',
+          type: 'DelegatorId',
         },
         {
-          name: "provider_msa_id",
-          type: "ProviderId",
+          name: 'provider_msa_id',
+          type: 'ProviderId',
         },
       ],
-      type: "Option<Vec<SchemaGrantResponse>>",
+      type: 'Option<Vec<SchemaGrantResponse>>',
     },
     getKeysByMsaId: {
-      description: "Fetch Keys for an MSA Id",
+      description: 'Fetch Keys for an MSA Id',
       params: [
         {
-          name: "msa_id",
-          type: "MessageSourceId",
+          name: 'msa_id',
+          type: 'MessageSourceId',
         },
       ],
-      type: "Option<KeyInfoResponse>",
+      type: 'Option<KeyInfoResponse>',
     },
   },
   types: {
-    MessageSourceId: "u64",
-    DelegatorId: "MessageSourceId",
-    ProviderId: "MessageSourceId",
+    MessageSourceId: 'u64',
+    DelegatorId: 'MessageSourceId',
+    ProviderId: 'MessageSourceId',
     KeyInfoResponse: {
-      msa_keys: "Vec<AccountId>",
-      msa_id: "MessageSourceId",
+      msa_keys: 'Vec<AccountId>',
+      msa_id: 'MessageSourceId',
     },
     SchemaGrantResponse: {
-      schema_id: "SchemaId",
-      revoked_at: "BlockNumber",
+      schema_id: 'SchemaId',
+      revoked_at: 'BlockNumber',
     },
   },
   runtime: {
@@ -66,41 +66,41 @@ export default {
         methods: {
           has_delegation: {
             description:
-              "Check to see if a delegation existed between the given delegator and provider at a given block",
+              'Check to see if a delegation existed between the given delegator and provider at a given block',
             params: [
               {
-                name: "delegator_id",
-                type: "DelegatorId",
+                name: 'delegator_id',
+                type: 'DelegatorId',
               },
               {
-                name: "provider_id",
-                type: "ProviderId",
+                name: 'provider_id',
+                type: 'ProviderId',
               },
               {
-                name: "block_number",
-                type: "BlockNumber",
+                name: 'block_number',
+                type: 'BlockNumber',
               },
               {
-                name: "schema_id",
-                type: "Option<SchemaId>",
+                name: 'schema_id',
+                type: 'Option<SchemaId>',
               },
             ],
-            type: "bool",
+            type: 'bool',
           },
           get_granted_schemas_by_msa_id: {
             description:
-              "Get the list of schema ids (if any) that exist in any delegation between the delegator and provider",
+              'Get the list of schema ids (if any) that exist in any delegation between the delegator and provider',
             params: [
               {
-                name: "delegator_id",
-                type: "DelegatorId",
+                name: 'delegator_id',
+                type: 'DelegatorId',
               },
               {
-                name: "provider_id",
-                type: "ProviderId",
+                name: 'provider_id',
+                type: 'ProviderId',
               },
             ],
-            type: "Option<Vec<SchemaId>>",
+            type: 'Option<Vec<SchemaId>>',
           },
         },
         version: 1,
@@ -109,41 +109,41 @@ export default {
         methods: {
           has_delegation: {
             description:
-              "Check to see if a delegation existed between the given delegator and provider at a given block",
+              'Check to see if a delegation existed between the given delegator and provider at a given block',
             params: [
               {
-                name: "delegator_id",
-                type: "DelegatorId",
+                name: 'delegator_id',
+                type: 'DelegatorId',
               },
               {
-                name: "provider_id",
-                type: "ProviderId",
+                name: 'provider_id',
+                type: 'ProviderId',
               },
               {
-                name: "block_number",
-                type: "BlockNumber",
+                name: 'block_number',
+                type: 'BlockNumber',
               },
               {
-                name: "schema_id",
-                type: "Option<SchemaId>",
+                name: 'schema_id',
+                type: 'Option<SchemaId>',
               },
             ],
-            type: "bool",
+            type: 'bool',
           },
           get_granted_schemas_by_msa_id: {
             description:
-              "Get the list of schema ids (if any) that exist in any delegation between the delegator and provider",
+              'Get the list of schema ids (if any) that exist in any delegation between the delegator and provider',
             params: [
               {
-                name: "delegator_id",
-                type: "DelegatorId",
+                name: 'delegator_id',
+                type: 'DelegatorId',
               },
               {
-                name: "provider_id",
-                type: "ProviderId",
+                name: 'provider_id',
+                type: 'ProviderId',
               },
             ],
-            type: "Option<Vec<SchemaGrantResponse>>",
+            type: 'Option<Vec<SchemaGrantResponse>>',
           },
         },
         version: 2,
