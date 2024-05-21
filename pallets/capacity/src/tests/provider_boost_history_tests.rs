b3191c8985111916d9bc85186afa4b6f168481e5
Shannon Wells <shannonwells@users.noreply.github.com>
2024-05-21 16:46:24 -0700

    Feat/check unclaimed rewards 1969 (#1972)
    The goal of this PR is to implement `list_unclaimed_rewards`, and also
    one that is lighter weight, `has_unclaimed_rewards`, which returns a
    `bool` and which `unstake` extrinsic uses. Unstake now fails if there
    are any unclaimed rewards.
    
    Closes #1969
    Closes #1578

--- pallets/capacity/src/tests/provider_boost_history_tests.rs
+++ pallets/capacity/src/tests/provider_boost_history_tests.rs
@@ -33,17 +33,17 @@ fn multiple_provider_boosts_updates_history_correctly() {
 		// should update era 1 history
 		let mut history = Capacity::get_staking_history_for(staker).unwrap();
 		assert_eq!(history.count(), 1);
-		assert_eq!(history.get_staking_amount_for_era(&1u32).unwrap(), &700u64);
+		assert_eq!(history.get_entry_for_era(&1u32).unwrap(), &700u64);
 
-		system_run_to_block(9);
-		run_to_block(10);
+		system_run_to_block(10);
+		run_to_block(11);
 
 		assert_ok!(Capacity::provider_boost(RuntimeOrigin::signed(staker), target, 200));
 
 		// should add an era 2 history
 		history = Capacity::get_staking_history_for(staker).unwrap();
 		assert_eq!(history.count(), 2);
-		assert_eq!(history.get_staking_amount_for_era(&2u32).unwrap(), &900u64);
+		assert_eq!(history.get_entry_for_era(&2u32).unwrap(), &900u64);
 	})
 }
 
@@ -74,10 +74,10 @@ fn provider_boost_history_add_era_balance_adds_entries_and_deletes_old_if_full()
 	let new_era: <Test as Config>::RewardEra = 99;
 	pbh.add_era_balance(&new_era, &1000u64);
 	assert_eq!(pbh.count(), bound as usize);
-	assert!(pbh.get_staking_amount_for_era(&new_era).is_some());
+	assert!(pbh.get_entry_for_era(&new_era).is_some());
 
 	let first_era: <Test as Config>::RewardEra = 1;
-	assert!(pbh.get_staking_amount_for_era(&first_era).is_none());
+	assert!(pbh.get_entry_for_era(&first_era).is_none());
 }
 
 #[test]
@@ -87,7 +87,7 @@ fn provider_boost_history_add_era_balance_in_same_era_updates_entry() {
 	let era = 2u32;
 	pbh.add_era_balance(&era, &add_amount);
 	pbh.add_era_balance(&era, &add_amount);
-	assert_eq!(pbh.get_staking_amount_for_era(&era).unwrap(), &2_000u64)
+	assert_eq!(pbh.get_entry_for_era(&era).unwrap(), &2_000u64)
 }
 
 #[test]
@@ -96,7 +96,7 @@ fn provider_boost_history_subtract_era_balance_in_same_era_updates_entry() {
 	let era = 2u32;
 	pbh.add_era_balance(&era, &1000u64);
 	pbh.subtract_era_balance(&era, &300u64);
-	assert_eq!(pbh.get_staking_amount_for_era(&(era)).unwrap(), &700u64);
+	assert_eq!(pbh.get_entry_for_era(&(era)).unwrap(), &700u64);
 }
 
 #[test]
@@ -106,7 +106,7 @@ fn provider_boost_history_add_era_balance_in_new_era_correctly_adds_value() {
 	let era = 2u32;
 	pbh.add_era_balance(&era, &add_amount);
 	pbh.add_era_balance(&(era + 1), &add_amount);
-	assert_eq!(pbh.get_staking_amount_for_era(&(era + 1)).unwrap(), &2_000u64)
+	assert_eq!(pbh.get_entry_for_era(&(era + 1)).unwrap(), &2_000u64)
 }
 
 #[test]
@@ -116,8 +116,17 @@ fn provider_boost_history_subtract_era_balance_in_new_era_correctly_subtracts_va
 	pbh.add_era_balance(&era, &1000u64);
 
 	pbh.subtract_era_balance(&(era + 1), &400u64);
-	assert_eq!(pbh.get_staking_amount_for_era(&(era + 1)).unwrap(), &600u64);
+	assert_eq!(pbh.get_entry_for_era(&(era + 1)).unwrap(), &600u64);
 
 	pbh.subtract_era_balance(&(era + 2), &600u64);
-	assert!(pbh.get_staking_amount_for_era(&(era + 2)).unwrap().is_zero());
+	assert!(pbh.get_entry_for_era(&(era + 2)).unwrap().is_zero());
+}
+
+#[test]
+fn provider_boost_history_subtract_all_balance_on_only_entry_returns_some_0() {
+	let mut pbh = ProviderBoostHistory::<Test>::new();
+	let era = 22u32;
+	let amount = 1000u64;
+	pbh.add_era_balance(&era, &amount);
+	assert_eq!(pbh.subtract_era_balance(&(era), &amount), Some(0usize));
 }
