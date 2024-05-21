863e26a5493bdee87e900f6ac5de0e669b922552
Shannon Wells <shannonwells@users.noreply.github.com>
2024-05-21 16:46:24 -0700

    Feat/check unclaimed rewards 1969 (#1972)
    The goal of this PR is to implement `list_unclaimed_rewards`, and also
    one that is lighter weight, `has_unclaimed_rewards`, which returns a
    `bool` and which `unstake` extrinsic uses. Unstake now fails if there
    are any unclaimed rewards.
    
    Closes #1969
    Closes #1578

--- pallets/capacity/src/tests/rewards_provider_tests.rs
+++ pallets/capacity/src/tests/rewards_provider_tests.rs
@@ -1,10 +1,15 @@
 use super::mock::*;
 use crate::{
-	CurrentEraInfo, Error, RewardEraInfo, StakingDetails, StakingRewardClaim,
-	StakingRewardsProvider, StakingType::*,
+	CurrentEraInfo, Error, ProviderBoostHistories, ProviderBoostHistory, RewardEraInfo,
+	StakingDetails, StakingRewardClaim, StakingRewardsProvider, StakingType::*,
+	UnclaimedRewardInfo,
 };
-use frame_support::assert_err;
+use frame_support::{assert_err, assert_ok, traits::Len};
 
+use crate::tests::testing_utils::{
+	run_to_block, set_era_and_reward_pool, setup_provider, system_run_to_block,
+};
+use common_primitives::msa::MessageSourceId;
 use sp_core::H256;
 
 #[test]
@@ -96,4 +101,195 @@ fn era_staking_reward_implementation() {
 			tc.expected_reward
 		);
 	}
+}
+
+#[test]
+fn check_for_unclaimed_rewards_returns_empty_set_when_no_staking() {
+	new_test_ext().execute_with(|| {
+		let account = 500u64;
+		let history: ProviderBoostHistory<Test> = ProviderBoostHistory::new();
+		ProviderBoostHistories::<Test>::set(account, Some(history));
+		let rewards = Capacity::list_unclaimed_rewards(&account).unwrap();
+		assert!(rewards.is_empty())
+	})
+}
+#[test]
+fn check_for_unclaimed_rewards_returns_empty_set_when_only_staked_this_era() {
+	new_test_ext().execute_with(|| {
+		system_run_to_block(5);
+		set_era_and_reward_pool(5u32, 1u32, 1000u64);
+		let account = 500u64;
+		let mut history: ProviderBoostHistory<Test> = ProviderBoostHistory::new();
+		history.add_era_balance(&5u32, &100u64);
+		ProviderBoostHistories::<Test>::set(account, Some(history));
+		let rewards = Capacity::list_unclaimed_rewards(&account).unwrap();
+		assert!(rewards.is_empty())
+	})
+}
+
+// Check that eligible amounts are only for what's staked an entire era.
+#[test]
+fn check_for_unclaimed_rewards_has_eligible_rewards() {
+	new_test_ext().execute_with(|| {
+		let account = 10_000u64;
+		let target: MessageSourceId = 10;
+		let amount = 1_000u64;
+
+		// staking 1k as of block 1, era 1 (1-10)
+		setup_provider(&account, &target, &amount, ProviderBoost);
+
+		// staking 2k as of block 11, era 2  (11-20)
+		run_to_block(11);
+		assert_ok!(Capacity::provider_boost(RuntimeOrigin::signed(account), target, amount));
+
+		//  staking 3k as of era 4, block 31, first block of era (31-40)
+		run_to_block(31);
+		assert_ok!(Capacity::provider_boost(RuntimeOrigin::signed(account), target, amount));
+
+		run_to_block(51);
+		assert_eq!(Capacity::get_current_era().era_index, 6u32);
+		assert_eq!(Capacity::get_current_era().started_at, 51u32);
+
+		assert!(Capacity::get_reward_pool_for_era(0u32).is_none());
+		assert!(Capacity::get_reward_pool_for_era(1u32).is_some());
+		assert!(Capacity::get_reward_pool_for_era(6u32).is_some());
+
+		// rewards for era 6 should not be returned; era 6 is current era and therefore ineligible.
+		// eligible amounts for rewards for eras should be:  1=0, 2=1k, 3=2k, 4=2k, 5=3k
+		let rewards = Capacity::list_unclaimed_rewards(&account).unwrap();
+		assert_eq!(rewards.len(), 5usize);
+		let expected_info: [UnclaimedRewardInfo<Test>; 5] = [
+			UnclaimedRewardInfo {
+				reward_era: 1u32,
+				expires_at_block: 61,
+				eligible_amount: 0,
+				earned_amount: 0,
+			},
+			UnclaimedRewardInfo {
+				reward_era: 2u32,
+				expires_at_block: 71,
+				eligible_amount: 1000,
+				earned_amount: 4,
+			},
+			UnclaimedRewardInfo {
+				reward_era: 3u32,
+				expires_at_block: 81,
+				eligible_amount: 2_000,
+				earned_amount: 8,
+			},
+			UnclaimedRewardInfo {
+				reward_era: 4u32,
+				expires_at_block: 91,
+				eligible_amount: 2000,
+				earned_amount: 8,
+			},
+			UnclaimedRewardInfo {
+				reward_era: 5u32,
+				expires_at_block: 101,
+				eligible_amount: 3_000,
+				earned_amount: 11,
+			},
+		];
+		for i in 0..=4 {
+			assert_eq!(rewards.get(i).unwrap(), &expected_info[i]);
+		}
+
+		run_to_block(61);
+		let rewards = Capacity::list_unclaimed_rewards(&account).unwrap();
+		// the earliest era should no longer be stored.
+		assert_eq!(rewards.len(), 5usize);
+		assert_eq!(rewards.get(0).unwrap().reward_era, 2u32);
+
+		// there was no change in stake, so the eligible and earned amounts should be the same as in
+		// reward era 5.
+		assert_eq!(
+			rewards.get(4).unwrap(),
+			&UnclaimedRewardInfo {
+				reward_era: 6u32,
+				expires_at_block: 111,
+				eligible_amount: 3_000,
+				earned_amount: 11,
+			}
+		)
+	})
+}
+
+// check that if an account boosted and then let it run for more than the number
+// of  history retention eras, eligible rewards are correct.
+#[test]
+fn check_for_unclaimed_rewards_returns_correctly_for_old_single_boost() {
+	new_test_ext().execute_with(|| {
+		let account = 10_000u64;
+		let target: MessageSourceId = 10;
+		let amount = 1_000u64;
+
+		assert!(!Capacity::has_unclaimed_rewards(&account));
+
+		// boost 1k as of block 1, era 1
+		setup_provider(&account, &target, &amount, ProviderBoost);
+		assert!(!Capacity::has_unclaimed_rewards(&account));
+
+		run_to_block(71);
+		assert_eq!(Capacity::get_current_era().era_index, 8u32);
+		assert_eq!(Capacity::get_current_era().started_at, 71u32);
+
+		let rewards = Capacity::list_unclaimed_rewards(&account).unwrap();
+		// the earliest era should no longer be stored.
+		assert_eq!(rewards.len(), 5usize);
+		for i in 0..=4 {
+			let expected_era: u32 = i + 3;
+			let expected_info: UnclaimedRewardInfo<Test> = UnclaimedRewardInfo {
+				reward_era: expected_era.into(),
+				expires_at_block: (expected_era * 10u32 + 51u32).into(),
+				eligible_amount: 1000,
+				earned_amount: 4,
+			};
+			assert_eq!(rewards.get(i as usize).unwrap(), &expected_info);
+		}
+	})
+}
+
+#[test]
+fn has_unclaimed_rewards_works() {
+	new_test_ext().execute_with(|| {
+		let account = 10_000u64;
+		let target: MessageSourceId = 10;
+		let amount = 1_000u64;
+
+		assert!(!Capacity::has_unclaimed_rewards(&account));
+
+		// staking 1k as of block 1, era 1
+		setup_provider(&account, &target, &amount, ProviderBoost);
+		assert!(!Capacity::has_unclaimed_rewards(&account));
+
+		// staking 2k as of block 11, era 2
+		run_to_block(11);
+		assert_ok!(Capacity::provider_boost(RuntimeOrigin::signed(account), target, amount));
+		assert!(Capacity::has_unclaimed_rewards(&account));
+
+		//  staking 3k as of era 4, block 31
+		run_to_block(31);
+		assert!(Capacity::has_unclaimed_rewards(&account));
+
+		run_to_block(61);
+		assert!(Capacity::has_unclaimed_rewards(&account));
+	})
+}
+
+#[test]
+fn has_unclaimed_rewards_returns_true_with_old_single_boost() {
+	new_test_ext().execute_with(|| {
+		let account = 10_000u64;
+		let target: MessageSourceId = 10;
+		let amount = 1_000u64;
+
+		assert!(!Capacity::has_unclaimed_rewards(&account));
+
+		// boost 1k as of block 1, era 1
+		setup_provider(&account, &target, &amount, ProviderBoost);
+		assert!(!Capacity::has_unclaimed_rewards(&account));
+
+		run_to_block(71);
+		assert!(Capacity::has_unclaimed_rewards(&account));
+	});
 }
