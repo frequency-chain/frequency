c53af044457590e8778e7c11b9cc827fa6a943ee
Aramik <aramikm@gmail.com>
2024-05-29 10:44:06 -0700

    fixed e2e ci

--- e2e/miscellaneous/frequency.test.ts
+++ e2e/miscellaneous/frequency.test.ts
@@ -26,7 +26,7 @@ describe('Frequency', function () {
       const beforeBlockNumber = await getBlockNumber();
 
       const extrinsic = new Extrinsic(
-        () => ExtrinsicHelper.api.tx.balances.transfer(keypairB.address, 1n * DOLLARS),
+        () => ExtrinsicHelper.api.tx.balances.transferKeepAlive(keypairB.address, 1n * DOLLARS),
         keypairA,
         ExtrinsicHelper.api.events.balances.Transfer
       );
@@ -56,7 +56,7 @@ describe('Frequency', function () {
       const nonce = await getNonce(keypairB);
       for (let i = 0; i < 10; i += 2) {
         const extrinsic = new Extrinsic(
-          () => ExtrinsicHelper.api.tx.balances.transfer(keypairA.address, 1n * DOLLARS),
+          () => ExtrinsicHelper.api.tx.balances.transferKeepAlive(keypairA.address, 1n * DOLLARS),
           keypairB,
           ExtrinsicHelper.api.events.balances.Transfer
         );
@@ -71,7 +71,7 @@ describe('Frequency', function () {
       // applying the missing nonce values to next transactions to unblock the stuck ones
       for (const missing of missingNonce) {
         const extrinsic = new Extrinsic(
-          () => ExtrinsicHelper.api.tx.balances.transfer(keypairA.address, 1n * DOLLARS),
+          () => ExtrinsicHelper.api.tx.balances.transferKeepAlive(keypairA.address, 1n * DOLLARS),
           keypairB,
           ExtrinsicHelper.api.events.balances.Transfer
         );
