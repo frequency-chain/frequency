4ff3d84f5fa98def01aa2a15c72d6671360a8f35
Wil Wade <wil.wade@amplica.io>
2024-07-22 13:27:33 +0000

    Change release notes based on RC vs full release

--- tools/ci/release-notes/build-release-notes.sh
+++ tools/ci/release-notes/build-release-notes.sh
@@ -5,9 +5,11 @@ polkadot_version=$1
 changelog=$2
 runtime_mainnet_info=$3
 runtime_paseo_info=$4
+is_full_release=$5
 
 CHANGELOG="$changelog" \
     POLKADOT_VERSION="$polkadot_version" \
     RUNTIME_MAINNET_INFO="$runtime_mainnet_info" \
     RUNTIME_PASEO_INFO="$runtime_paseo_info" \
+    IS_FULL_RELEASE="$is_full_release" \
     tera -a --env --env-key env --env-only --template release-notes.md.tera
