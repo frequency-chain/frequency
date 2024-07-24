4b8c864653a38bfefccf912607df081c51e83a8b
eNddy <enddynayn@gmail.com>
2024-07-24 08:06:08 -0600

    feat(runtime): add metadata signed-extension
    Add a new signed extension that enables the metadata hash
    verification feature approved under RFC 0078. This enhancement will
    support the new generic ledger hardware wallet app and future
    hardware wallets within the Polkadot ecosystem.

--- runtime/frequency/build.rs
+++ runtime/frequency/build.rs
@@ -1,12 +1,14 @@
-use substrate_wasm_builder::WasmBuilder;
-
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
-	// VSCode Users: Uncomment the following line to disable the ANSI color codes.
-	// The OUTPUT pane does not understand ANSI color codes and will show garbage without this.
-	// std::env::set_var("WASM_BUILD_NO_COLOR", "1");
-	WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
+	substrate_wasm_builder::WasmBuilder::build_using_defaults()
+}
+
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
+		.enable_metadata_hash("FRQCY", 8)
 		.build()
 }
+
+#[cfg(not(feature = "std"))]
+fn main() {}
