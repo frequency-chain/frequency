83be6dd3b1549db22741a17bf8f98632db79442a
Wil Wade <wil.wade@amplica.io>
2024-07-30 13:25:50 +0000

    e2e test formatting

--- e2e/capacity/provider_boost.test.ts
+++ e2e/capacity/provider_boost.test.ts
@@ -1,40 +1,44 @@
-import "@frequency-chain/api-augment";
-import assert from "assert";
+import '@frequency-chain/api-augment';
+import assert from 'assert';
 import { getFundingSource } from '../scaffolding/funding';
 import {
-  createKeys, createMsaAndProvider,
-  CENTS, DOLLARS, createAndFundKeypair, boostProvider,
+  createKeys,
+  createMsaAndProvider,
+  CENTS,
+  DOLLARS,
+  createAndFundKeypair,
+  boostProvider,
 } from '../scaffolding/helpers';
 
 const fundingSource = getFundingSource('capacity-provider-boost');
 
-describe("Capacity: provider_boost extrinsic", function() {
+describe('Capacity: provider_boost extrinsic', function () {
   const providerBalance = 2n * DOLLARS;
 
-  it("happy path succeeds", async function () {
-    const stakeKeys = createKeys("booster");
-    const provider = await createMsaAndProvider(fundingSource, stakeKeys, "Provider1", providerBalance);
-    const booster = await createAndFundKeypair(fundingSource, 5n * DOLLARS, "booster");
+  it('happy path succeeds', async function () {
+    const stakeKeys = createKeys('booster');
+    const provider = await createMsaAndProvider(fundingSource, stakeKeys, 'Provider1', providerBalance);
+    const booster = await createAndFundKeypair(fundingSource, 5n * DOLLARS, 'booster');
     await assert.doesNotReject(boostProvider(fundingSource, booster, provider, 1n * DOLLARS));
   });
 
-  it("fails when they are a Maximized Capacity staker", async function() {
-    const stakeKeys = createKeys("booster");
-    const provider = await createMsaAndProvider(fundingSource, stakeKeys, "Provider1", providerBalance);
+  it('fails when they are a Maximized Capacity staker', async function () {
+    const stakeKeys = createKeys('booster');
+    const provider = await createMsaAndProvider(fundingSource, stakeKeys, 'Provider1', providerBalance);
     await assert.rejects(boostProvider(fundingSource, stakeKeys, provider, 1n * DOLLARS));
   });
 
-  it("fails when they don't have enough token", async function() {
-    const stakeKeys = createKeys("booster");
-    const provider = await createMsaAndProvider(fundingSource, stakeKeys, "Provider1", providerBalance);
-    const booster = await createAndFundKeypair(fundingSource, 1n * DOLLARS, "booster");
+  it("fails when they don't have enough token", async function () {
+    const stakeKeys = createKeys('booster');
+    const provider = await createMsaAndProvider(fundingSource, stakeKeys, 'Provider1', providerBalance);
+    const booster = await createAndFundKeypair(fundingSource, 1n * DOLLARS, 'booster');
     await assert.rejects(boostProvider(booster, booster, provider, 1n * DOLLARS));
   });
 
-  it("they can boost multiple times", async function () {
-    const stakeKeys = createKeys("booster");
-    const provider = await createMsaAndProvider(fundingSource, stakeKeys, "Provider1", providerBalance);
-    const booster = await createAndFundKeypair(fundingSource, 10n * DOLLARS, "booster");
+  it('they can boost multiple times', async function () {
+    const stakeKeys = createKeys('booster');
+    const provider = await createMsaAndProvider(fundingSource, stakeKeys, 'Provider1', providerBalance);
+    const booster = await createAndFundKeypair(fundingSource, 10n * DOLLARS, 'booster');
     await assert.doesNotReject(boostProvider(fundingSource, booster, provider, 1n * DOLLARS));
     await assert.doesNotReject(boostProvider(fundingSource, booster, provider, 1n * DOLLARS));
     await assert.doesNotReject(boostProvider(fundingSource, booster, provider, 1n * DOLLARS));
