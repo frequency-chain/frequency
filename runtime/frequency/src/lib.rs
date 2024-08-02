777b20fbd0f32b83ccca04e8372bb5f7dfb871ab
Wil Wade <wil.wade@amplica.io>
2024-08-02 16:28:55 +0000

    Enable Passkey everywhere

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -950,7 +950,6 @@ impl pallet_frequency_tx_payment::Config for Runtime {
 }
 
 /// Configurations for passkey pallet
-#[cfg(any(not(feature = "frequency"), feature = "frequency-lint-check"))]
 impl pallet_passkey::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
@@ -1243,8 +1242,6 @@ construct_runtime!(
 		Capacity: pallet_capacity::{Pallet, Call, Storage, Event<T>, FreezeReason} = 64,
 		FrequencyTxPayment: pallet_frequency_tx_payment::{Pallet, Call, Event<T>} = 65,
 		Handles: pallet_handles::{Pallet, Call, Storage, Event<T>} = 66,
-		// Currently enabled only under feature flag
-		#[cfg(any(not(feature = "frequency"), feature = "frequency-lint-check"))]
 		Passkey: pallet_passkey::{Pallet, Call, Storage, Event<T>, ValidateUnsigned} = 67,
 	}
 );
@@ -1281,8 +1278,7 @@ mod benches {
 		[pallet_time_release, TimeRelease]
 		[pallet_capacity, Capacity]
 		[pallet_frequency_tx_payment, FrequencyTxPayment]
-		// Todo: uncomment after removing the feature flag
-		// [pallet_passkey, Passkey]
+		[pallet_passkey, Passkey]
 	);
 }
 
