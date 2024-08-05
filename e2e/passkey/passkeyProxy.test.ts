5de1c78e9bcafe837170389500ac214ee134ca84
Wil Wade <wil.wade@amplica.io>
2024-08-05 19:52:37 +0000

    Fixing passkey test
    Passkey check only balance not nonce and don't pre-fund it
    
    Working consistently

--- e2e/passkey/passkeyProxy.test.ts
+++ e2e/passkey/passkeyProxy.test.ts
@@ -1,6 +1,6 @@
 import '@frequency-chain/api-augment';
 import assert from 'assert';
-import { createAndFundKeypair, getNextEpochBlock, getNonce } from '../scaffolding/helpers';
+import { createAndFundKeypair, createKeys, getBlockNumber, getNonce } from '../scaffolding/helpers';
 import { KeyringPair } from '@polkadot/keyring/types';
 import { ExtrinsicHelper } from '../scaffolding/extrinsicHelpers';
 import { getFundingSource } from '../scaffolding/funding';
@@ -14,8 +14,8 @@ describe('Passkey Pallet Tests', function () {
     let receiverKeys: KeyringPair;
 
     beforeEach(async function () {
-      fundedKeys = await createAndFundKeypair(fundingSource, 100_000_000n);
-      receiverKeys = await createAndFundKeypair(fundingSource);
+      fundedKeys = await createAndFundKeypair(fundingSource, 1_000_000_000n);
+      receiverKeys = createKeys('passkey-receiver');
     });
 
     it('should fail due to unsupported call', async function () {
@@ -35,7 +35,7 @@ describe('Passkey Pallet Tests', function () {
     it('should fail to transfer balance due to bad account ownership proof', async function () {
       const accountPKey = fundedKeys.publicKey;
       const nonce = await getNonce(fundedKeys);
-      const transferCalls = ExtrinsicHelper.api.tx.balances.transferKeepAlive(receiverKeys.publicKey, 0n);
+      const transferCalls = ExtrinsicHelper.api.tx.balances.transferKeepAlive(receiverKeys.address, 0n);
       const { passKeyPrivateKey, passKeyPublicKey, passkeySignature } = createPassKeyAndSignAccount(accountPKey);
       const accountSignature = fundedKeys.sign('badPasskeyPublicKey');
       const passkeyCall = await createPassKeyCall(accountPKey, nonce, accountSignature, transferCalls);
@@ -48,7 +48,7 @@ describe('Passkey Pallet Tests', function () {
     it('should fail to transfer balance due to bad passkey signature', async function () {
       const accountPKey = fundedKeys.publicKey;
       const nonce = await getNonce(fundedKeys);
-      const transferCalls = ExtrinsicHelper.api.tx.balances.transferKeepAlive(receiverKeys.publicKey, 0n);
+      const transferCalls = ExtrinsicHelper.api.tx.balances.transferKeepAlive(receiverKeys.address, 0n);
       const { passKeyPrivateKey, passKeyPublicKey, passkeySignature } = createPassKeyAndSignAccount(accountPKey);
       const accountSignature = fundedKeys.sign(u8aWrapBytes(passKeyPublicKey));
       const passkeyCall = await createPassKeyCall(accountPKey, nonce, accountSignature, transferCalls);
@@ -61,17 +61,15 @@ describe('Passkey Pallet Tests', function () {
     it('should transfer small balance from fundedKeys to receiverKeys', async function () {
       const accountPKey = fundedKeys.publicKey;
       const nonce = await getNonce(fundedKeys);
-      const transferCalls = ExtrinsicHelper.api.tx.balances.transferKeepAlive(receiverKeys.publicKey, 100n);
+      const transferCalls = ExtrinsicHelper.api.tx.balances.transferKeepAlive(receiverKeys.address, 100_000_000n);
       const { passKeyPrivateKey, passKeyPublicKey, passkeySignature } = createPassKeyAndSignAccount(accountPKey);
       const accountSignature = fundedKeys.sign(u8aWrapBytes(passKeyPublicKey));
       const passkeyCall = await createPassKeyCall(accountPKey, nonce, accountSignature, transferCalls);
       const passkeyPayload = await createPasskeyPayload(passKeyPrivateKey, passKeyPublicKey, passkeyCall, false);
       const passkeyProxy = ExtrinsicHelper.executePassKeyProxy(fundedKeys, passkeyPayload);
       assert.doesNotReject(passkeyProxy.fundAndSendUnsigned(fundingSource));
-      await ExtrinsicHelper.runToBlock(await getNextEpochBlock());
+      await ExtrinsicHelper.runToBlock((await getBlockNumber()) + 2);
       const receiverBalance = await ExtrinsicHelper.getAccountInfo(receiverKeys.address);
-      const nonceAfter = await getNonce(fundedKeys);
-      assert.equal(nonce + 1, nonceAfter);
       assert(receiverBalance.data.free.toBigInt() > 0n);
     });
   });
