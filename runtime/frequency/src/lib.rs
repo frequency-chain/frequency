eac1832c8bf838a43f5d8363fb029159c2909940
Aramik <aramikm@gmail.com>
2024-08-06 11:18:59 -0700

    booster: feedback 1 (#2114)
    # Goal
    The goal of this PR is to address some feedbacks
    
    - added feature flags for test only methods
    - added a constant
    - small refactor

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -578,7 +578,7 @@ impl pallet_capacity::Config for Runtime {
 	type EraLength = ConstU32<{ 14 * DAYS }>;
 	type ProviderBoostHistoryLimit = ConstU32<30u32>;
 	type RewardsProvider = Capacity;
-	type MaxRetargetsPerRewardEra = ConstU32<16>;
+	type MaxRetargetsPerRewardEra = ConstU32<2>;
 	// Value determined by desired inflation rate limits for chosen economic model
 	type RewardPoolEachEra = ConstU128<{ currency::CENTS.saturating_mul(172_602_740u128) }>;
 	type RewardPercentCap = CapacityRewardCap;
