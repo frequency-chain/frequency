babdc05f296c1e5cdf0b0b216bddbf8c9ba71584
enddynayn <enddynayn@users.noreply.github.com>
2024-10-08 17:22:26 +0000

    Update weights

--- runtime/common/src/weights/block_weights.rs
+++ runtime/common/src/weights/block_weights.rs
@@ -16,8 +16,8 @@
 // limitations under the License.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 32.0.0
-//! DATE: 2024-10-03 (Y/M/D)
-//! HOSTNAME: `ip-10-173-10-189`, CPU: `Intel(R) Xeon(R) Platinum 8375C CPU @ 2.90GHz`
+//! DATE: 2024-10-08 (Y/M/D)
+//! HOSTNAME: `ip-10-173-4-193`, CPU: `Intel(R) Xeon(R) Platinum 8375C CPU @ 2.90GHz`
 //!
 //! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Frequency Development (No Relay)`
 //! WARMUPS: `10`, REPEAT: `100`
@@ -43,17 +43,17 @@ parameter_types! {
 	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 350_924, 384_247
-	///   Average:  358_294
-	///   Median:   355_119
-	///   Std-Dev:  7697.14
+	///   Min, Max: 346_771, 383_256
+	///   Average:  354_117
+	///   Median:   351_035
+	///   Std-Dev:  7175.59
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 377_175
-	///   95th: 374_701
-	///   75th: 360_077
+	///   99th: 372_003
+	///   95th: 368_633
+	///   75th: 355_960
 	pub const BlockExecutionWeight: Weight =
-		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(358_294), 0);
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(354_117), 0);
 }
 
 #[cfg(test)]
