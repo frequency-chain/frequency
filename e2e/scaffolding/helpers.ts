458ebb3b5c1c5be80df86316041ba11887b829df
shannonwells <shannonwells@users.noreply.github.com>
2024-10-10 14:43:16 -0700

    please the linters

--- e2e/scaffolding/helpers.ts
+++ e2e/scaffolding/helpers.ts
@@ -50,8 +50,7 @@ export interface Sr25519Signature {
 export const TEST_EPOCH_LENGTH = 50;
 export const CENTS = 1000000n;
 export const DOLLARS = 100n * CENTS;
-export const BOOST_ADJUSTMENT = 2n;  // divide by 2 or 50% of Maximum Capacity
-
+export const BOOST_ADJUSTMENT = 2n; // divide by 2 or 50% of Maximum Capacity
 
 export function getTokenPerCapacity(): bigint {
   // Perbil
@@ -509,7 +508,7 @@ export async function getOrCreateGraphChangeSchema(source: KeyringPair): Promise
       'OnChain',
       [],
       'test.graphChangeSchema'
-    )
+    );
     const { target: createSchemaEvent, eventMap } = await op.fundAndSend(source);
     assertExtrinsicSuccess(eventMap);
     if (createSchemaEvent) {
