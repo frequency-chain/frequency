e026573d76ce9c3f3bdd7a10cd87ccc710b1ba0f
Wil Wade <wil.wade@projectliberty.io>
2024-10-29 18:21:21 +0000

    Update polkadot-js/api for api-augment

---
+++ js/api-augment/types.d.ts
@@ -0,0 +1,10 @@
+// Unknown is causing these to be left out of the generated types.
+// Adding them here for now.
+
+declare global {
+  type SpConsensusSlotsSlotDuration = bigint;
+  type CommonPrimitivesMsaDelegatorId = bigint;
+  type CommonPrimitivesMsaProviderId = bigint;
+}
+
+export {};
