0c8012160254943282959b8f14aed1a8487d2f52
Aramik <aramikm@gmail.com>
2024-11-20 16:42:59 -0800

    added more functionality

--- js/schemas/test/schemas.test.ts
+++ js/schemas/test/schemas.test.ts
@@ -1,5 +1,12 @@
 import assert from 'assert';
-import { FULL_NAME_TO_ID, ID_TO_SCHEMA_FULL_NAME, ID_TO_SCHEMA_INFO, SchemaInfo } from '../schemas';
+import {
+  FULL_NAME_TO_ID,
+  getAllVersionsFromSchemaName,
+  ID_TO_SCHEMA_FULL_NAME,
+  ID_TO_SCHEMA_INFO,
+  NAME_TO_ID_ACTIVE,
+  SchemaInfo,
+} from '../schemas';
 
 describe('schemas', function () {
   it('should be able to successfully get schemas from Id', function () {
@@ -34,5 +41,15 @@ describe('schemas', function () {
     assert.equal(info.payloadLocation, expected.payloadLocation);
     assert.equal(info.appendOnly, expected.appendOnly);
     assert.equal(info.signatureRequired, expected.signatureRequired);
+  });
+
+  it('should be able to successfully get all schema version from the schema name', function () {
+    const versions = getAllVersionsFromSchemaName('dsnp.tombstone');
+    assert.deepEqual(versions, [ID_TO_SCHEMA_INFO.get(1), ID_TO_SCHEMA_INFO.get(16)]);
+  });
+
+  it('should be able to successfully get active schema version from the schema name', function () {
+    const id = NAME_TO_ID_ACTIVE.get('dsnp.tombstone');
+    assert.equal(id, 16);
   });
 });
