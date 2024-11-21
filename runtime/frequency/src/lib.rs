1fe2a414f1d6dfc092c3c43cffe0401a5d3c0378
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-11-21 08:04:18 -0600

    address feedback

--- runtime/frequency/src/lib.rs
+++ runtime/frequency/src/lib.rs
@@ -655,7 +655,7 @@ impl pallet_time_release::Config for Runtime {
 impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
-	#[cfg(any(not(feature = "frequency-no-relay"), feature = "frequency-lint-check"))]
+	#[cfg(not(feature = "frequency-no-relay"))]
 	type OnTimestampSet = Aura;
 	#[cfg(feature = "frequency-no-relay")]
 	type OnTimestampSet = ();
