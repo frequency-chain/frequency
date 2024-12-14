0cb751e65671374bb6b9c0a659cd5c06af52e234
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2024-12-13 17:50:20 -0700

    feat: update magic debugger code

--- runtime/frequency/build.rs
+++ runtime/frequency/build.rs
@@ -1,7 +1,8 @@
 // Succinct Proof of Concept
 // #![no_main]
 // use sp1_zkvm;
-#![feature(core_intrinsics)]
+// #![feature(core_intrinsics)]
+use core::time::Duration;
 
 // Duplicated here to reduce the build dependencies
 #[allow(unused)]
@@ -15,11 +16,23 @@ const TOKEN_DECIMALS: u8 = 8;
 
 #[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
+	// Magic to attach debugger to the build process
+	// from here: https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#attaching-debugger-to-the-current-process-rust
+	let url = format!("vscode://vadimcn.vscode-lldb/launch/config?{{'request':'attach','pid':{}}}", std::process::id());
+	std::process::Command::new("code").arg("--open-url").arg(url).output().unwrap();
+	let one_sec = Duration::from_millis(10000);
+	std::thread::sleep(one_sec); // Wait for debugger to attach
 	substrate_wasm_builder::WasmBuilder::build_using_defaults()
 }
 
 #[cfg(all(feature = "std", feature = "metadata-hash", feature = "frequency"))]
 fn main() {
+	// Magic to attach debugger to the build process
+	// from here: https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#attaching-debugger-to-the-current-process-rust
+	let url = format!("vscode://vadimcn.vscode-lldb/launch/config?{{'request':'attach','pid':{}}}", std::process::id());
+	std::process::Command::new("code").arg("--open-url").arg(url).output().unwrap();
+	let one_sec = Duration::from_millis(10000);
+	std::thread::sleep(one_sec); // Wait for debugger to attach
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
 		.enable_metadata_hash(FREQUENCY_TOKEN, TOKEN_DECIMALS)
 		.build()
@@ -27,6 +40,12 @@ fn main() {
 
 #[cfg(all(feature = "std", feature = "metadata-hash", feature = "frequency-testnet"))]
 fn main() {
+	// Magic to attach debugger to the build process
+	// from here: https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#attaching-debugger-to-the-current-process-rust
+	let url = format!("vscode://vadimcn.vscode-lldb/launch/config?{{'request':'attach','pid':{}}}", std::process::id());
+	std::process::Command::new("code").arg("--open-url").arg(url).output().unwrap();
+	let one_sec = Duration::from_millis(10000);
+	std::thread::sleep(one_sec); // Wait for debugger to attach
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
 		.enable_metadata_hash(FREQUENCY_TESTNET_TOKEN, TOKEN_DECIMALS)
 		.build()
@@ -42,8 +61,9 @@ fn main() {
 	// from here: https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#attaching-debugger-to-the-current-process-rust
 	let url = format!("vscode://vadimcn.vscode-lldb/launch/config?{{'request':'attach','pid':{}}}", std::process::id());
 	std::process::Command::new("code").arg("--open-url").arg(url).output().unwrap();
-	std::thread::sleep_ms(10000); // Wait for debugger to attach
-	std::intrinsics::breakpoint();
+	let one_sec = Duration::from_millis(10000);
+	std::thread::sleep(one_sec); // Wait for debugger to attach
+	// std::intrinsics::breakpoint();
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
 		.enable_metadata_hash(FREQUENCY_LOCAL_TOKEN, TOKEN_DECIMALS)
 		.build()
