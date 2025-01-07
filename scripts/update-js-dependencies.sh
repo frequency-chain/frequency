bfdf9804e90fbf4c1c663f0252c0924d4c755f1c
Wil Wade <wil.wade@projectliberty.io>
2025-01-07 09:02:28 -0500

    Add basic js dependency update script

---
+++ scripts/update-js-dependencies.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "Updating js/api-augment"
+cd js/api-augment
+rm package-lock.json
+npx npm-check-updates -u
+npm i
+npm run build
+cd dist
+npm pack
+cd ../../..
+
+echo "Updating js/schemas"
+cd js/schemas
+rm package-lock.json
+npx npm-check-updates -u
+npm i
+cd ../..
+
+echo "Updating e2e"
+cd e2e
+rm package-lock.json
+npx npm-check-updates -u
+npm i ../js/api-augment/dist/frequency-chain-api-augment-0.0.0.tgz
+cd ..
+
+echo "Updating tools/state-copy"
+cd tools/state-copy
+rm package-lock.json
+npx npm-check-updates -u
+npm i
+cd ../..
+
+echo "Updating tools/genesis-data"
+cd tools/genesis-data
+rm package-lock.json
+npx npm-check-updates -u
+npm i
+cd ../..
