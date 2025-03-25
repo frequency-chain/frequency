043d7a077da1df101541120f56caa89634287b39
Aramik <aramikm@gmail.com>
2025-03-24 18:03:32 -0700

    key-migration: get token keys

--- tools/eth-migration/README.md
+++ tools/eth-migration/README.md
@@ -1,14 +1,17 @@
 # Eth migrations tools
+Some scripts and tools to facilitate the key migration
 
-
-# Get Onchain msa id and keys
+## Get Onchain msa id and keys
 `npm run control-keys`
 
 
-# Get Offchain Indexed msa id and keys
+## Get Offchain Indexed msa id and keys
 `npm run offchain-keys`
 
-## Sort and Compare to see any discrepancies
+### Sort and Compare to see any discrepancies
 1. `sort -t',' -k1,1n -k2,2 onchain-keys.txt >> onchain-keys-sorted.txt`
 2. `sort -t',' -k1,1n -k2,2 offchain-keys.txt >> offchain-keys-sorted.txt`
 3. `diff onchain-keys-sorted.txt offchain-keys-sorted.txt`
+
+## Get Token keys
+`npm run token-keys`
---
+++ tools/eth-migration/fetch-token-keys.mjs
@@ -0,0 +1,115 @@
+// Only way to silence PolkadotJS API warnings we don't want
+console.warn = () => {};
+
+import { ApiPromise, WsProvider } from "@polkadot/api";
+
+const SOURCE_URL = "wss://1.rpc.frequency.xyz";
+const BATCH_SIZE = 1000;
+
+const options = {
+	rpc: {
+		msa: {
+			getKeysByMsaId: {
+				description: 'Fetch Keys for an MSA Id',
+				params: [
+					{
+						name: 'msa_id',
+						type: 'MessageSourceId',
+					},
+				],
+				type: 'Option<KeyInfoResponse>',
+			},
+		},
+	},
+	types: {
+		MessageSourceId: 'u64',
+		KeyInfoResponse: {
+			msa_keys: 'Vec<AccountId>',
+			msa_id: 'MessageSourceId',
+		},
+	},
+};
+
+// 0x26aa394eea5630e07c48ae0c9558cef7
+//   b99d880ec681799c0cf30e8886371da9
+//   005c2cfa803bd987e8f26e9843d3f16f
+//   a29da12763d36885f88f7553561d06ad462c478c1577892e1fe273233717b172
+
+export async function fetchTokenKeysFromState(sourceUrl) {
+	// Connect to the state source
+	const sourceProvider = new WsProvider(sourceUrl);
+	const sourceApi = await ApiPromise.create({provider: sourceProvider, ...options});
+
+	// system pallet + account
+	const prefixKeys = "0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9";
+
+	let result = await sourceApi.rpc.state.getKeysPaged(prefixKeys, BATCH_SIZE);
+
+	while (result.length === BATCH_SIZE) {
+		await printPublicKeysAndTokens(sourceApi, getPublicKeys(result));
+
+		const lastKey = result[result.length - 1];
+		result = await sourceApi.rpc.state.getKeysPaged(prefixKeys, BATCH_SIZE, lastKey);
+	}
+
+	await printPublicKeysAndTokens(sourceApi, getPublicKeys(result));
+}
+
+function getPublicKeys(results) {
+	const publicKeys = [];
+	for (const r of results) {
+		const wholeKey = r.toHex();
+		const publicKey = wholeKey.substring(wholeKey.length - 64);
+		publicKeys.push(`0x${publicKey}`);
+	}
+	return publicKeys;
+}
+
+export async function printPublicKeysAndTokens(sourceApi, publicKeys) {
+	let promises = [];
+	for (const publicKey of publicKeys) {
+		promises.push(sourceApi.query.system.account(publicKey));
+	}
+
+	if (promises.length > 0) {
+		printResults(await Promise.all(promises), publicKeys);
+	}
+}
+
+// {
+// 	"publicKey": "0x488e14e049fd59b77e5821c4bb71fc9c4b8ced4c19ed27b3949e91b6274f1400",
+// 	"values": {
+// 		"nonce": 0,
+// 			"consumers": 0,
+// 			"providers": 1,
+// 			"sufficients": 0,
+// 			"data": {
+// 			"free": 5000000000,
+// 			"reserved": 0,
+// 			"frozen": 0,
+// 			"flags": "0x80000000000000000000000000000000"
+// 		}
+// 	}
+// }
+function printResults(results, keys) {
+	for (let i = 0; i < results.length ; i++) {
+		const obj = {
+			publicKey: keys[i],
+			values: results[i],
+		};
+		console.log(`${JSON.stringify(obj)}`);
+	}
+}
+
+
+async function main() {
+	try {
+		await fetchTokenKeysFromState(SOURCE_URL);
+		process.exit(0);
+	} catch (error) {
+		console.error("Error:", error);
+		process.exit(1);
+	}
+}
+
+main();
