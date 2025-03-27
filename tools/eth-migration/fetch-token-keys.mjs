49c414ddff489b2fcb5bfe932f379f46e914aeb2
Aramik <aramikm@gmail.com>
2025-03-27 12:33:27 -0700

    apply feedback

--- tools/eth-migration/fetch-token-keys.mjs
+++ tools/eth-migration/fetch-token-keys.mjs
@@ -3,7 +3,7 @@ console.warn = () => {};
 
 import { ApiPromise, WsProvider } from "@polkadot/api";
 
-const SOURCE_URL = "wss://1.rpc.frequency.xyz";
+const SOURCE_URL = process.env["FREQUENCY_URL"] || "wss://1.rpc.frequency.xyz";
 const BATCH_SIZE = 1000;
 
 const options = {
@@ -30,56 +30,9 @@ const options = {
 	},
 };
 
-// 0x26aa394eea5630e07c48ae0c9558cef7
-//   b99d880ec681799c0cf30e8886371da9
-//   005c2cfa803bd987e8f26e9843d3f16f
-//   a29da12763d36885f88f7553561d06ad462c478c1577892e1fe273233717b172
-
-export async function fetchTokenKeysFromState(sourceUrl) {
-	// Connect to the state source
-	const sourceProvider = new WsProvider(sourceUrl);
-	const sourceApi = await ApiPromise.create({provider: sourceProvider, ...options});
-
-	// system pallet + account
-	const prefixKeys = "0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9";
-
-	let result = await sourceApi.rpc.state.getKeysPaged(prefixKeys, BATCH_SIZE);
-
-	while (result.length === BATCH_SIZE) {
-		await printPublicKeysAndTokens(sourceApi, getPublicKeys(result));
-
-		const lastKey = result[result.length - 1];
-		result = await sourceApi.rpc.state.getKeysPaged(prefixKeys, BATCH_SIZE, lastKey);
-	}
-
-	await printPublicKeysAndTokens(sourceApi, getPublicKeys(result));
-}
-
-function getPublicKeys(results) {
-	const publicKeys = [];
-	for (const r of results) {
-		const wholeKey = r.toHex();
-		const publicKey = wholeKey.substring(wholeKey.length - 64);
-		publicKeys.push(`0x${publicKey}`);
-	}
-	return publicKeys;
-}
-
-export async function printPublicKeysAndTokens(sourceApi, publicKeys) {
-	let accountPromises = [];
-	let controlKeyPromises = [];
-	for (const publicKey of publicKeys) {
-		accountPromises.push(sourceApi.query.system.account(publicKey));
-		controlKeyPromises.push(sourceApi.query.msa.publicKeyToMsaId(publicKey));
-	}
-
-	if (accountPromises.length > 0) {
-		printResults(publicKeys, await Promise.all(accountPromises), await Promise.all(controlKeyPromises));
-	}
-}
-
 // {
 // 	"publicKey": "0x488e14e049fd59b77e5821c4bb71fc9c4b8ced4c19ed27b3949e91b6274f1400",
+//  "msaId": 1,
 // 	"values": {
 // 		"nonce": 0,
 // 			"consumers": 0,
@@ -93,17 +46,25 @@ export async function printPublicKeysAndTokens(sourceApi, publicKeys) {
 // 		}
 // 	}
 // }
-function printResults(publicKeys, accountResults, controlKeyResult) {
-	for (let i = 0; i < accountResults.length ; i++) {
-		const obj = {
-			publicKey: publicKeys[i],
-			msaId: controlKeyResult[i].isSome ? controlKeyResult[i].unwrap().toNumber() : 0,
-			values: accountResults[i],
-		};
-		console.log(`${JSON.stringify(obj)}`);
-	}
-}
+export async function fetchTokenKeysFromState(sourceUrl) {
+	// Connect to the state source
+	const sourceProvider = new WsProvider(sourceUrl);
+	const sourceApi = await ApiPromise.create({provider: sourceProvider, ...options});
 
+	let result;
+	do {
+		result = await sourceApi.query.system.account.entriesPaged({ args: [], pageSize: BATCH_SIZE, startKey: result && result[result.length - 1][0] });
+		let msaResult = await sourceApi.query.msa.publicKeyToMsaId.multi(result.map(([key, _]) => key.args[0]));
+		result.forEach(([key, account], index) => {
+			const obj = {
+				publicKey: key.args[0].toHex(),
+				msaId: msaResult[index].isSome ? msaResult[index].unwrap().toNumber() : 0,
+				values: account,
+			}
+			console.log(`${JSON.stringify(obj)}`);
+		});
+	} while (result.length === BATCH_SIZE);
+}
 
 async function main() {
 	try {
