839dd2dd21e6970e7773576ac6735d5d670b618a
Wil Wade <wil.wade@projectliberty.io>
2025-04-23 13:39:30 -0400

    Remove unused functions

--- node/service/src/chain_spec/frequency_paseo_local.rs
+++ node/service/src/chain_spec/frequency_paseo_local.rs
@@ -12,23 +12,6 @@ pub type ChainSpec = sc_service::GenericChainSpec<Extensions>;
 // Generic chain spec, in case when we don't have the native runtime.
 pub type RelayChainSpec = sc_service::GenericChainSpec<RelayChainExtensions>;
 
-#[allow(clippy::unwrap_used)]
-/// Generates the Frequency Paseo chain spec from the raw json
-pub fn load_frequency_paseo_spec() -> ChainSpec {
-	ChainSpec::from_json_bytes(
-		&include_bytes!("../../../../resources/frequency-paseo.raw.json")[..],
-	)
-	.unwrap()
-}
-
-// TODO: Remove once on a Polkadot-SDK with Paseo
-#[allow(clippy::unwrap_used)]
-/// Generates the Paseo Relay chain spec from the json
-pub fn load_paseo_spec() -> RelayChainSpec {
-	RelayChainSpec::from_json_bytes(&include_bytes!("../../../../resources/paseo.json")[..])
-		.unwrap()
-}
-
 // TODO: Remove once on a Polkadot-SDK with Paseo-Local
 #[allow(clippy::unwrap_used)]
 /// Generates the Paseo-Local Relay chain spec from the json
