88c11041fe289ac2b05ddc8a6c654621e7f3da22
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2025-05-02 15:28:36 -0600

    Add Frequency Westend genesis configuration  (#2387)
    # Goal
    The goal of this PR is to correctly generate the Westend chain spec.
    
    - Added `westend.rs` to capture the chain spec generation, which is only
    needed once, but is a good reference for chain spec modifications.
    - Updated `resources/frequency-westend.json`,
    `resources/frequency-westend.raw.json`
    
    Closes #2376

--- runtime/frequency/src/genesis/mod.rs
+++ runtime/frequency/src/genesis/mod.rs
@@ -2,8 +2,12 @@
 #[cfg(any(
 	feature = "frequency-no-relay",
 	feature = "frequency-local",
-	feature = "frequency-lint-check"
+	feature = "frequency-lint-check",
+	feature = "frequency-westend"
 ))]
 pub mod helpers;
 
 pub mod presets;
+
+#[cfg(feature = "frequency-westend")]
+pub mod westend;
