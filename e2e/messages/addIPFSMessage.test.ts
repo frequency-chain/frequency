df06d686e6b1c51f3e54d9fe0ecba6ee442d98f9
Wil Wade <wil.wade@projectliberty.io>
2025-05-14 09:52:44 -0400

    Fix issues with messages.MessagesInBlock

--- e2e/messages/addIPFSMessage.test.ts
+++ e2e/messages/addIPFSMessage.test.ts
@@ -5,7 +5,7 @@ import { base32 } from 'multiformats/bases/base32';
 import { CID } from 'multiformats/cid';
 import { PARQUET_BROADCAST } from '../schemas/fixtures/parquetBroadcastSchemaType';
 import assert from 'assert';
-import { assertHasMessage, createAndFundKeypair, getOrCreateDummySchema } from '../scaffolding/helpers';
+import { assertEvent, assertHasMessage, createAndFundKeypair, getOrCreateDummySchema } from '../scaffolding/helpers';
 import { ExtrinsicHelper } from '../scaffolding/extrinsicHelpers';
 import { u16 } from '@polkadot/types';
 import { ipfsCid } from './ipfs';
@@ -108,9 +108,14 @@ describe('Add Offchain Message', function () {
 
   it('should successfully add an IPFS message', async function () {
     const f = ExtrinsicHelper.addIPFSMessage(keys, schemaId, ipfs_cid_64, ipfs_payload_len);
-    const { target: event } = await f.fundAndSend(fundingSource);
+    const { target: event, eventMap } = await f.fundAndSend(fundingSource);
 
-    assert.notEqual(event, undefined, `should have returned a MessagesInBlock event, got: ${event?.toHuman()}`);
+    // messages.MessagesInBlock in block might not be on this transaction if there are others
+    assertEvent(eventMap, 'system.ExtrinsicSuccess');
+
+    if (event) {
+      assert.notEqual(event, undefined, `should have returned a MessagesInBlock event, got: ${event?.toHuman()}`);
+    }
   });
 
   it('should successfully retrieve added message and returned CID should have Base32 encoding', async function () {
@@ -129,9 +134,14 @@ describe('Add Offchain Message', function () {
   describe('Add OnChain Message and successfully retrieve it', function () {
     it('should successfully add and retrieve an onchain message', async function () {
       const f = ExtrinsicHelper.addOnChainMessage(keys, dummySchemaId, '0xdeadbeef');
-      const { target: event } = await f.fundAndSend(fundingSource);
+      const { target: event, eventMap } = await f.fundAndSend(fundingSource);
 
-      assert.notEqual(event, undefined, 'should have returned a MessagesInBlock event');
+      // messages.MessagesInBlock in block might not be on this transaction if there are others
+      assertEvent(eventMap, 'system.ExtrinsicSuccess');
+
+      if (event) {
+        assert.notEqual(event, undefined, `should have returned a MessagesInBlock event, got: ${event?.toHuman()}`);
+      }
 
       const get = await ExtrinsicHelper.apiPromise.rpc.messages.getBySchemaId(dummySchemaId, {
         from_block: starting_block,
