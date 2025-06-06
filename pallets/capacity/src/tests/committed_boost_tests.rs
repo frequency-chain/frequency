6c6dd9b2b833e17afd763b98d683f880de239ce5
Joe Caputo <joseph.caputo@projectliberty.io>
2025-06-06 12:57:30 -0400

    fix: unit tests

--- pallets/capacity/src/tests/committed_boost_tests.rs
+++ pallets/capacity/src/tests/committed_boost_tests.rs
@@ -1,10 +1,10 @@
 use super::mock::*;
 use crate as pallet_capacity;
-use crate::{StakingConfigProvider, StakingType, StakingType::FlexibleBoost};
+use crate::{BalanceOf, StakingConfigProvider, StakingType, StakingType::FlexibleBoost};
 use common_primitives::capacity::StakingType::CommittedBoost;
 use frame_support::traits::Get;
 use pallet_capacity::Config;
-use sp_runtime::Percent;
+use sp_runtime::Perbill;
 
 #[test]
 fn percent_releasable_in_pre_commit_phase_should_be_zero() {
@@ -18,7 +18,7 @@ fn percent_releasable_in_pre_commit_phase_should_be_zero() {
 				&<Test as Config>::StakingConfigProvider::get(CommittedBoost),
 			);
 		assert_eq!(staking_type, CommittedBoost);
-		assert_eq!(percent_releasable, Percent::zero());
+		assert_eq!(percent_releasable, Perbill::zero());
 	})
 }
 
@@ -34,7 +34,7 @@ fn percent_releasable_in_initial_commit_phase_should_be_zero() {
 				&<Test as Config>::StakingConfigProvider::get(CommittedBoost),
 			);
 		assert_eq!(staking_type, CommittedBoost);
-		assert_eq!(percent_releasable, Percent::zero());
+		assert_eq!(percent_releasable, Perbill::zero());
 	})
 }
 
@@ -51,7 +51,7 @@ fn percent_releasable_after_failsafe_block_should_be_one_hundred() {
 				&<Test as Config>::StakingConfigProvider::get(CommittedBoost),
 			);
 		assert_eq!(staking_type, FlexibleBoost);
-		assert_eq!(percent_releasable, Percent::from_percent(100));
+		assert_eq!(percent_releasable, Perbill::from_percent(100));
 	})
 }
 
@@ -73,35 +73,37 @@ fn percent_releasable_at_end_of_staged_release_should_be_one_hundred() {
 				&<Test as Config>::StakingConfigProvider::get(CommittedBoost),
 			);
 		assert_eq!(staking_type, FlexibleBoost);
-		assert_eq!(percent_releasable, Percent::from_percent(100));
+		assert_eq!(percent_releasable, Perbill::from_percent(100));
 	})
 }
 
 #[test]
-fn percent_releasable_during_staged_release_should_be_correct() {
+fn amount_releasable_during_staged_release_should_be_consistent_if_always_unstaked() {
 	new_test_ext().execute_with(|| {
 		// Indicate we are in the initial-commit phase
 		set_pte_block::<Test>(Some(1));
 		let staking_config = <Test as Config>::StakingConfigProvider::get(CommittedBoost);
 		let start_block_number = 1 + staking_config.initial_commitment_blocks;
-		let end_block_number = start_block_number +
-			(staking_config.commitment_release_stage_blocks *
-				staking_config.commitment_release_stages);
 		System::set_block_number(start_block_number);
+		let mut prev_amount: BalanceOf<Test> = 0;
+		let mut total_balance: BalanceOf<Test> = 1_000_000;
 
-		println!("Start block number: {:?}", start_block_number);
-		for value in (start_block_number..end_block_number)
-			.step_by(usize::try_from(staking_config.commitment_release_stage_blocks).unwrap())
-		{
-			System::set_block_number(value);
+		for stage in 0..staking_config.commitment_release_stages {
+			let offset_blocks = stage * staking_config.commitment_release_stage_blocks;
+			let block_number = start_block_number + offset_blocks;
+			System::set_block_number(block_number);
 			let (staking_type, percent_releasable) =
 				Capacity::get_staking_type_and_releasable_percent_in_force(
 					StakingType::CommittedBoost,
 					&<Test as Config>::StakingConfigProvider::get(CommittedBoost),
 				);
-			// assert_eq!(staking_type, CommittedBoost);
-			// assert_eq!(percent_releasable, Percent::from_percent(100));
-			println!("Block: {:?} - Percent releasable: {:?}", value, percent_releasable);
+			assert_eq!(staking_type, CommittedBoost);
+			let amount = percent_releasable.mul_floor(total_balance);
+			if prev_amount > 0 && amount > 0 {
+				assert!((prev_amount as i32 - amount as i32).abs() <= 1);
+			}
+			prev_amount = amount;
+			total_balance = total_balance.saturating_sub(amount);
 		}
 	})
 }
