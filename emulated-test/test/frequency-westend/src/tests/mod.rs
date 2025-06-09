8b0a21be282c6dfc7054eb504501f8463c070fab
eNDdy <enddynayn@gmail.com>
2025-06-09 17:44:46 -0600

    feat(xcm): reject native token reserve transfers
    This change ensures that reserve transfers involving the native token are explicitly rejected when attempting to transfer native tokens back to the origin. This preserves the intended behavior where native assets can only be teleported between AssetHub and Frequency, maintaining consistency and integrity in asset movement.

--- emulated-test/test/frequency-westend/src/tests/mod.rs
+++ emulated-test/test/frequency-westend/src/tests/mod.rs
@@ -5,6 +5,7 @@ mod reserve_transfer_dot_from_relay;
 mod reserve_transfer_dot_to_assethub;
 mod reserve_transfer_dot_to_relay;
 
+mod reserve_transfer_xfrqcy_with_dot_fee_from_assethub_fails;
 mod teleport_xfrqcy_to_assethub_with_dot_fee;
 
 mod teleport_xfrqcy_with_dot_fee_from_assethub;
