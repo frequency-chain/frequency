a1c20c903d6ece5aeb3a01642e393be20170e551
eNddy <enddynayn@gmail.com>
2025-06-10 17:46:17 -0600

    feat(xcm): add checking account to track outbound teleports (#2443)
    Introduce a checking account mechanism to monitor assets teleported from
    the chain. This ensures that the total amount received on the
    destination chain does not exceed the amount minted, preventing
    over-minting or duplication errors during cross-chain transfers.

--- emulated-test/test/frequency-westend/src/lib.rs
+++ emulated-test/test/frequency-westend/src/lib.rs
@@ -32,7 +32,10 @@ mod imports {
 			AssetHubWestendParaPallet as AssetHubWestendPallet,
 		},
 		frequency_emulated_chain::{
-			frequency_runtime::ExistentialDeposit as FrequencyExistentialDeposit,
+			frequency_runtime::{
+				xcm::CheckingAccount as FrequencyCheckingAccount,
+				ExistentialDeposit as FrequencyExistentialDeposit,
+			},
 			FrequencyAssetOwner, FrequencyWestendParaPallet as FrequencyWestendPallet,
 		},
 		westend_emulated_chain::{genesis::ED as WESTEND_ED, WestendRelayPallet as WestendPallet},
@@ -65,6 +68,18 @@ mod imports {
 			<Balances as Inspect<_>>::balance(who)
 		})
 	}
+}
+
+#[macro_export]
+macro_rules! foreign_balance_on {
+	( $chain:ident, $id:expr, $who:expr ) => {
+		emulated_integration_tests_common::impls::paste::paste! {
+			<$chain>::execute_with(|| {
+				type ForeignAssets = <$chain as [<$chain Pallet>]>::ForeignAssets;
+				<ForeignAssets as FungiblesInspect<_>>::balance($id, $who)
+			})
+		}
+	};
 }
 
 #[cfg(test)]
