c3f437a4344dc3e243fb12b980d254ec77ea681f
eNddy <enddynayn@gmail.com>
2025-06-10 17:46:17 -0600

    feat(xcm): add checking account to track outbound teleports (#2443)
    Introduce a checking account mechanism to monitor assets teleported from
    the chain. This ensures that the total amount received on the
    destination chain does not exceed the amount minted, preventing
    over-minting or duplication errors during cross-chain transfers.

--- emulated-test/test/frequency-westend/src/tests/mod.rs
+++ emulated-test/test/frequency-westend/src/tests/mod.rs
@@ -10,15 +10,3 @@ mod teleport_xfrqcy_to_assethub_with_dot_fee;
 mod teleport_xfrqcy_with_dot_fee_from_assethub;
 
 mod utils;
-
-#[macro_export]
-macro_rules! foreign_balance_on {
-	( $chain:ident, $id:expr, $who:expr ) => {
-		emulated_integration_tests_common::impls::paste::paste! {
-			<$chain>::execute_with(|| {
-				type ForeignAssets = <$chain as [<$chain Pallet>]>::ForeignAssets;
-				<ForeignAssets as FungiblesInspect<_>>::balance($id, $who)
-			})
-		}
-	};
-}
--- runtime/frequency/src/xcm/asset_transactor.rs
+++ runtime/frequency/src/xcm/asset_transactor.rs
@@ -2,7 +2,7 @@ use crate::{AccountId, Balances, ForeignAssets, PolkadotXcm};
 use staging_xcm::latest::prelude::*;
 use staging_xcm_builder::{
 	FungibleAdapter, FungiblesAdapter, IsConcrete, IsParentsOnly, MatchedConvertedConcreteId,
-	NoChecking,
+	MintLocation, NoChecking,
 };
 
 use crate::xcm::location_converter::LocationToAccountId;
@@ -10,30 +10,27 @@ use crate::xcm::location_converter::LocationToAccountId;
 use frame_support::{parameter_types, traits::ConstU8};
 
 parameter_types! {
-	/// A placeholder account used to hold reserved balances temporarily during checking.
-	pub CheckingAccount: AccountId = PolkadotXcm::check_account();
 	pub HereLocation: Location = Location::here();
+	pub CheckingAccount: AccountId = PolkadotXcm::check_account();
+	pub Checking: (AccountId, MintLocation) = (CheckingAccount::get(), MintLocation::Local);
 }
 
 /// Adapter for transacting native assets (like balances)
-/// Means for transacting assets on this chain.
-/// How xcm deals with assets on this chain
 pub type LocalAssetTransactor = FungibleAdapter<
-	// Use this currency:
+	// Use native currency:
 	Balances,
-	// Use this currency when it is a fungible asset matching the given location or name:
+	// Use this currency when it is a fungible asset matching the native location
 	IsConcrete<HereLocation>,
 	// Do a simple punn to convert an AccountId32 Location into a native chain account ID:
 	LocationToAccountId,
 	// Our chain's account ID type (we can't get away without mentioning it explicitly):
 	AccountId,
-	// We don't track any teleports.
-	(),
+	// We track teleports.
+	Checking,
 >;
 
 /// Adapter for transacting foreign assets (like DOT)
 /// Type for specifying how a `Location` can be converted into an `AccountId`.
-///// Transactors ///////
 pub type ForeignAssetsAdapter = FungiblesAdapter<
 	ForeignAssets,
 	// Use this currency when it is a fungible asset matching the given location or name:
