3fdcc842b9639c399a212b49ee5969cc79ab94f3
Joe Caputo <joseph.caputo@projectliberty.io>
2025-06-13 19:41:53 -0400

    fix: correct logic for determining whether there are unclaimed rewards for an account (#2457)
    # Goal
    The goal of this PR is to correct the logic used to determine whether an account has unclaimed rewards.
    
    Closes #2295 
    
    Co-authored-by: shannonwells <shannonwells@users.noreply.github.com>

--- pallets/capacity/src/lib.rs
+++ pallets/capacity/src/lib.rs
@@ -1042,24 +1042,17 @@ impl<T: Config> Pallet<T> {
 	pub(crate) fn has_unclaimed_rewards(account: &T::AccountId) -> bool {
 		let current_era = CurrentEraInfo::<T>::get().era_index;
 		match ProviderBoostHistories::<T>::get(account) {
-			Some(provider_boost_history) => {
-				match provider_boost_history.count() {
-					0usize => false,
-					1usize => {
-						// if there is just one era entry and:
-						// it's for the previous era, it means we've already paid out rewards for that era, or they just staked in the last era.
-						// or if it's for the current era, they only just started staking.
-						provider_boost_history
-							.get_entry_for_era(&current_era.saturating_sub(1u32))
-							.is_none() && provider_boost_history
-							.get_entry_for_era(&current_era)
-							.is_none()
-					},
-					_ => true,
-				}
-			},
+			// We can ignore any entries for the current or prior era, since:
+			//   - if it's for the previous era, it means we've already paid out rewards for that era,
+			//     or they just staked in that era & hence aren't eligible for rewards yet.
+			//   - if it's for the current era, then they've only just started staking
+			// If there are any entries for eras earlier than one prior to the current era, then there
+			// are unclaimed rewards.
+			Some(provider_boost_history) =>
+				matches!(provider_boost_history.get_earliest_reward_era(),
+						 Some(era) if era < &current_era.saturating_sub(1u32)),
 			None => false,
-		} // 1r
+		}
 	}
 
 	/// Get all unclaimed rewards information for each eligible Reward Era.
