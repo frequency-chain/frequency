4e8b1dffa5d1d0c905911506f623ceb20a1ee83f
Wil Wade <wil.wade@projectliberty.io>
2025-07-09 12:30:53 -0400

    Fix recovery commitment test for testnet e2e

--- e2e/msa/recoveryCommitment.test.ts
+++ e2e/msa/recoveryCommitment.test.ts
@@ -15,7 +15,6 @@ import {
   createKeys,
   getEthereumKeyPairFromUnifiedAddress,
 } from '../scaffolding/helpers';
-import { u64 } from '@polkadot/types';
 import { Codec } from '@polkadot/types/types';
 import { createRecoveryCommitmentPayload, getUnifiedAddress, HexString, sign } from '@frequency-chain/ethereum-utils';
 import { u8aToHex } from '@polkadot/util';
@@ -25,7 +24,6 @@ let fundingSource: KeyringPair;
 describe('Recovery Commitment Testing', function () {
   // SET UP: Common variables used across tests
   let keys: KeyringPair;
-  let msaId: u64;
   let secondaryKey: KeyringPair;
   let payload: RecoveryCommitmentPayload;
   let ownerSig: Sr25519Signature;
@@ -39,12 +37,6 @@ describe('Recovery Commitment Testing', function () {
   before(async function () {
     fundingSource = await getFundingSource(import.meta.url);
 
-    // Setup an MSA with one key and a secondary funded key
-    [keys, secondaryKey] = await createAndFundKeypairs(fundingSource, ['keys', 'secondaryKey'], 2n * DOLLARS);
-    const { target } = await ExtrinsicHelper.createMsa(keys).signAndSend();
-    assert.notEqual(target?.data.msaId, undefined, 'MSA Id not in expected event');
-    msaId = target!.data.msaId;
-
     recoveryCommitmentData = {
       discriminant: 'RecoveryCommitmentPayload',
       recoveryCommitment: recoveryCommitment,
@@ -53,20 +45,27 @@ describe('Recovery Commitment Testing', function () {
     codecForPayload = ExtrinsicHelper.api.registry.createType('PalletMsaRecoveryCommitmentPayload', payload);
   });
 
-  describe('addRecoveryCommitment', function () {
+  describe('addRecoveryCommitment SR Keys', function () {
+    before(async function () {
+      // Setup an MSA with one key and a secondary funded key
+      [keys, secondaryKey] = await createAndFundKeypairs(fundingSource, ['keys', 'secondaryKey'], 2n * DOLLARS);
+      const { target } = await ExtrinsicHelper.createMsa(keys).signAndSend(undefined, {}, false);
+      assert.notEqual(target?.data.msaId, undefined, 'MSA Id not in expected event');
+    });
+
     it('should successfully add an Sr25519 signed recovery commitment', async function () {
       ownerSig = signPayloadSr25519(keys, codecForPayload);
 
       const addRecoveryCommitmentOp = ExtrinsicHelper.addRecoveryCommitment(keys, ownerSig, payload);
 
-      const { eventMap } = await addRecoveryCommitmentOp.fundAndSend(fundingSource, false);
+      const { eventMap } = await addRecoveryCommitmentOp.fundAndSend(fundingSource);
       assertEvent(eventMap, 'system.ExtrinsicSuccess');
       assertEvent(eventMap, 'msa.RecoveryCommitmentAdded');
     });
 
     it('should fail to add a recovery commitment when signature is re-used', async function () {
       const addRecoveryCommitmentOp = ExtrinsicHelper.addRecoveryCommitment(keys, ownerSig, payload);
-      await assert.rejects(addRecoveryCommitmentOp.fundAndSend(fundingSource, false), {
+      await assert.rejects(addRecoveryCommitmentOp.fundAndSend(fundingSource), {
         name: 'SignatureAlreadySubmitted',
       });
     });
@@ -75,9 +74,11 @@ describe('Recovery Commitment Testing', function () {
       badSig = signPayloadSr25519(secondaryKey, codecForPayload);
 
       const addRecoveryCommitmentOp = ExtrinsicHelper.addRecoveryCommitment(keys, badSig, payload);
-      await assert.rejects(addRecoveryCommitmentOp.fundAndSend(fundingSource, false), { name: 'InvalidSignature' });
+      await assert.rejects(addRecoveryCommitmentOp.fundAndSend(fundingSource), { name: 'InvalidSignature' });
     });
+  });
 
+  describe('addRecoveryCommitment Ethereum Keys', function () {
     it('should successfully add an Ethereum signed recovery commitment', async function () {
       // SET UP
       const ethereumKeyringPair = createKeys('Ethereum', 'ethereum');
@@ -90,8 +91,16 @@ describe('Recovery Commitment Testing', function () {
       }
 
       // Fund the Ethereum key and create an MSA for it
-      await ExtrinsicHelper.transferFunds(fundingSource, ethereumKeyringPair, 2n * DOLLARS).signAndSend();
-      const { target: msaCreationTarget } = await ExtrinsicHelper.createMsa(ethereumKeyringPair).signAndSend();
+      await ExtrinsicHelper.transferFunds(fundingSource, ethereumKeyringPair, 2n * DOLLARS).signAndSend(
+        undefined,
+        {},
+        false
+      );
+      const { target: msaCreationTarget } = await ExtrinsicHelper.createMsa(ethereumKeyringPair).signAndSend(
+        undefined,
+        {},
+        false
+      );
       assert.notEqual(msaCreationTarget?.data.msaId, undefined, 'MSA Id not in expected event');
 
       const eip712RecoveryCommitmentPayload = createRecoveryCommitmentPayload(
@@ -106,8 +115,7 @@ describe('Recovery Commitment Testing', function () {
       );
 
       // ACT
-      const { eventMap } = await addRecoveryCommitmentOp.fundAndSend(fundingSource, false);
-
+      const { eventMap } = await addRecoveryCommitmentOp.fundAndSend(fundingSource);
       // ASSERT
       assertEvent(eventMap, 'system.ExtrinsicSuccess');
       assertEvent(eventMap, 'msa.RecoveryCommitmentAdded');
