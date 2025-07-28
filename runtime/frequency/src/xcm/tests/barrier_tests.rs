b7935e670a2233af8b133d45a2dd49106d22285c
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2025-07-28 13:17:56 -0500

    parent is allowed unpaid barrier

--- runtime/frequency/src/xcm/tests/barrier_tests.rs
+++ runtime/frequency/src/xcm/tests/barrier_tests.rs
@@ -42,6 +42,25 @@ fn test_barrier_fails_unauthoized_location_unpaid() {
 }
 
 #[test]
+fn test_barrier_allows_parent_location_unpaid() {
+	let location = Location::parent();
+	let mut instructions = Xcm::<()>(vec![UnpaidExecution {
+		weight_limit: Limited(Weight::from_parts(20, 20)),
+		check_origin: Some(Location::parent()),
+	}]);
+	let mut properties = Properties { weight_credit: Weight::zero(), message_id: None };
+	let weight = Weight::from_parts(10, 0);
+
+	let result = <Barrier as XcmBarrier>::should_execute(
+		&location,
+		instructions.inner_mut(),
+		weight,
+		&mut properties,
+	);
+	assert!(result.is_ok(), "Barrier test failed: {:?}", result);
+}
+
+#[test]
 fn test_barrier_denies_unpaid_random_location() {
 	let location = Location::new(100, []);
 	let mut instructions =
