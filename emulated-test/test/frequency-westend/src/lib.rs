6a8436c45a34bd37b73e1d6013e4f3e821141377
eNddy <enddynayn@gmail.com>
2025-07-28 11:09:22 -0600

    feat(XcmConfig): disable SafeCallFilter (#2519)
    Disallow `Transact` instructions from being executed as part of incoming
    XCM instruction sets.

--- emulated-test/test/frequency-westend/src/lib.rs
+++ emulated-test/test/frequency-westend/src/lib.rs
@@ -33,7 +33,9 @@ mod imports {
 		},
 		frequency_emulated_chain::{
 			frequency_runtime::{
+				self,
 				xcm::CheckingAccount as FrequencyCheckingAccount,
+				xcm::XcmConfig as FrequencyXcmConfig,
 				ExistentialDeposit as FrequencyExistentialDeposit,
 			},
 			FrequencyAssetOwner, FrequencyWestendParaPallet as FrequencyWestendPallet, TreasuryAccount as FrequencyTreasuryAccount,
