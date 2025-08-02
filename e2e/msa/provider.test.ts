5df8458e687186f0e4edfc0084e1a47983ef8796
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2025-08-02 16:48:10 -0500

    add provider test files and placeholder for e2e

---
+++ e2e/msa/provider.test.ts
@@ -0,0 +1,41 @@
+import '@frequency-chain/api-augment';
+import assert from 'assert';
+import { CENTS, createAndFundKeypair } from '../scaffolding/helpers';
+import { KeyringPair } from '@polkadot/keyring/types';
+import { ExtrinsicHelper } from '../scaffolding/extrinsicHelpers';
+import { getFundingSource } from '../scaffolding/funding';
+
+let fundingSource: KeyringPair;
+
+describe('Create Provider', function () {
+  let keys: KeyringPair;
+  let failureKeys: KeyringPair;
+
+  before(async function () {
+    fundingSource = await getFundingSource(import.meta.url);
+    keys = await createAndFundKeypair(fundingSource, 5n * CENTS);
+    failureKeys = await createAndFundKeypair(fundingSource, 5n * CENTS, 'failure-keys');
+  });
+
+  describe('createProvider', function () {
+    it('should successfully create a provider', async function () {
+      const f = ExtrinsicHelper.createMsa(keys);
+      await f.fundAndSend(fundingSource);
+      const createProviderOp = ExtrinsicHelper.createProvider(keys, 'MyProvider');
+      const { target: providerEvent } = await createProviderOp.signAndSend();
+      assert.notEqual(providerEvent, undefined, 'setup should return a ProviderCreated event');
+      const providerId = providerEvent!.data.providerId;
+      // assert providerId is greater than 0
+      assert(providerId.toBigInt() > 0n, 'providerId should be greater than 0');
+    });
+
+    it('should fail to create a provider for long name', async function () {
+      const longName = 'a'.repeat(257); // 256 characters long
+      const f = ExtrinsicHelper.createMsa(failureKeys);
+      await f.fundAndSend(fundingSource);
+      const createProviderOp = ExtrinsicHelper.createProvider(failureKeys, longName);
+      const { target: providerEvent } = await createProviderOp.signAndSend();
+      assert.notEqual(providerEvent, undefined, 'setup should return a ProviderCreated event');
+    });
+  });
+});
