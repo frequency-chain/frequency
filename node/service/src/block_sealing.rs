7b8ef3c90da495759693161216f1fa5fe7cc5444
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2025-09-08 08:07:27 -0600

    Update crates and toolchain (#2589)
    # Goal
    The goal of this PR is to fix failures in `make lint-audit`.
    
    # Discussion
    
    - `slab` was yanked, so it was updated to version v0.4.11
    - `tracing-subscriber` reported a RUSTSEC vulnerability warning and was
    updated to v0.3.20
    - Additional crates were updated due to dependencies on these crates.
    - A new `Cargo.lock` file was generated.
    - Updating `tracing-subscriber` required Rust `edition2024` and
    therefore the rust toolchain was updated to `1.88`
    - The github actions container was updated to use `1.88` and that
    version was updated: `1.5.8`
    - Rust toolchain `1.88` required code edits to eliminate several
    warnings that have been mainlined. These are mostly around strings and
    some clippy warnings.
    - Most of these changes were grabbed from a recent polkadot SDK update,
    courtesy of @saraswatpuneet
    - `fxhash` was flagged as `unmaintained`. It is a `polkadot-sdk`
    dependency from `wasmtime`. This [Parity
    PR](https://github.com/paritytech/polkadot-sdk/pull/8714) has updated
    `wasmtime` which removes the dependency on `fxhash`.
    - The RUSTSEC unmaintained warning was added to the `deny.toml` ignore
    list until the above PR is merged into a stable release.
    - `yamux` updated to 0.13.6, possible solution to some network instability
    
    Co-authored-by: Puneet Saraswat
    <61435908+saraswatpuneet@users.noreply.github.com>

--- node/service/src/block_sealing.rs
+++ node/service/src/block_sealing.rs
@@ -25,7 +25,7 @@ use std::{sync::Arc, task::Poll};
 
 /// Function to start Frequency in dev mode without a relay chain
 /// This function is called when --chain dev --sealing= is passed.
-#[allow(clippy::expect_used)]
+#[allow(clippy::expect_used, clippy::result_large_err)]
 pub fn start_frequency_dev_sealing_node(
 	config: Configuration,
 	sealing_mode: SealingMode,
@@ -34,11 +34,11 @@ pub fn start_frequency_dev_sealing_node(
 	override_pool_config: Option<TransactionPoolOptions>,
 ) -> Result<TaskManager, sc_service::error::Error> {
 	let extra: String = if sealing_mode == SealingMode::Interval {
-		format!(" ({}s interval)", sealing_interval)
+		format!(" ({sealing_interval}s interval)")
 	} else {
 		String::from("")
 	};
-	log::info!("📎 Development mode (no relay chain) with {} sealing{}", sealing_mode, extra);
+	log::info!("📎 Development mode (no relay chain) with {sealing_mode} sealing{extra}");
 
 	let net_config = sc_network::config::FullNetworkConfiguration::<
 		_,
--- node/src/main.rs
+++ node/src/main.rs
@@ -2,6 +2,7 @@
 
 #![warn(missing_docs)]
 
+#[allow(clippy::result_large_err)]
 fn main() -> frequency_cli::Result<()> {
 	frequency_cli::run()
 }
