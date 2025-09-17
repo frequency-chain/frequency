8b0b5889b9fe6845edc74803a4a7e604f7469f47
Aramik <aramikm@gmail.com>
2025-09-16 19:12:56 -0700

    some benchmark changes

--- pallets/msa/src/lib.rs
+++ pallets/msa/src/lib.rs
@@ -1391,7 +1391,10 @@ pub mod pallet {
 		/// * [`Error::InvalidCid`] - If the provided CID is invalid.
 		/// * [`Error::InvalidBCP47LanguageCode`] - If the provided BCP 47 language code is invalid.
 		#[pallet::call_index(21)]
-		#[pallet::weight(T::WeightInfo::propose_to_add_application())]
+		#[pallet::weight(T::WeightInfo::propose_to_add_application(
+			payload.localized_names.len() as u32,
+			payload.localized_logo_250_100_png_cids.len() as u32,
+		))]
 		pub fn propose_to_add_application(
 			origin: OriginFor<T>,
 			payload: ApplicationContext<
@@ -1505,7 +1508,10 @@ pub mod pallet {
 		/// * [`Error::InvalidCid`] - If the provided CID is invalid.
 		/// * [`Error::InvalidBCP47LanguageCode`] - If the provided BCP 47 language code is invalid.
 		#[pallet::call_index(24)]
-		#[pallet::weight(T::WeightInfo::update_provider_via_governance())]
+		#[pallet::weight(T::WeightInfo::update_provider_via_governance(
+			payload.localized_names.len() as u32,
+			payload.localized_logo_250_100_png_cids.len() as u32,
+		))]
 		#[allow(clippy::useless_conversion)]
 		pub fn update_provider_via_governance(
 			origin: OriginFor<T>,
@@ -1520,7 +1526,10 @@ pub mod pallet {
 			T::CreateProviderViaGovernanceOrigin::ensure_origin(origin)?;
 			let provider_msa_id = Self::ensure_valid_msa_key(&provider_key)?;
 			Self::ensure_correct_cids(&payload)?;
-			let base_weight = T::WeightInfo::update_provider_via_governance();
+			let base_weight = T::WeightInfo::update_provider_via_governance(
+				payload.localized_names.len() as u32,
+				payload.localized_logo_250_100_png_cids.len() as u32,
+			);
 			let total_logos_removed = Self::upsert_provider_for(provider_msa_id, payload, true)?;
 			Self::deposit_event(Event::ProviderUpdated {
 				provider_id: ProviderId(provider_msa_id),
@@ -1541,7 +1550,10 @@ pub mod pallet {
 		/// * [`Error::InvalidCid`] - If the provided CID is invalid.
 		/// * [`Error::InvalidBCP47LanguageCode`] - If the provided BCP 47 language code is invalid.
 		#[pallet::call_index(25)]
-		#[pallet::weight(T::WeightInfo::propose_to_update_provider())]
+		#[pallet::weight(T::WeightInfo::propose_to_update_provider(
+			payload.localized_names.len() as u32,
+			payload.localized_logo_250_100_png_cids.len() as u32,
+		))]
 		pub fn propose_to_update_provider(
 			origin: OriginFor<T>,
 			payload: ProviderRegistryEntry<
@@ -1583,7 +1595,10 @@ pub mod pallet {
 		/// * [`Error::InvalidCid`] - If the provided CID is invalid.
 		/// * [`Error::InvalidBCP47LanguageCode`] - If the provided BCP 47 language code is invalid.
 		#[pallet::call_index(26)]
-		#[pallet::weight(T::WeightInfo::update_application_via_governance())]
+		#[pallet::weight(T::WeightInfo::update_application_via_governance(
+			payload.localized_names.len() as u32,
+			payload.localized_logo_250_100_png_cids.len() as u32,
+		))]
 		#[allow(clippy::useless_conversion)]
 		pub fn update_application_via_governance(
 			origin: OriginFor<T>,
@@ -1603,7 +1618,10 @@ pub mod pallet {
 				Error::<T>::ProviderNotRegistered
 			);
 			Self::ensure_correct_cids(&payload)?;
-			let base_weight = T::WeightInfo::update_application_via_governance();
+			let base_weight = T::WeightInfo::update_application_via_governance(
+				payload.localized_names.len() as u32,
+				payload.localized_logo_250_100_png_cids.len() as u32,
+			);
 			let total_logos_removed = Self::upsert_application_for(
 				ProviderId(provider_msa_id),
 				application_index,
@@ -1630,7 +1648,10 @@ pub mod pallet {
 		/// * [`Error::InvalidBCP47LanguageCode`] - If the provided BCP 47 language code is invalid.
 		/// * [`Error::ApplicationNotFound`] - If the application is not registered.
 		#[pallet::call_index(27)]
-		#[pallet::weight(T::WeightInfo::propose_to_update_application())]
+		#[pallet::weight(T::WeightInfo::propose_to_update_application(
+			payload.localized_names.len() as u32,
+			payload.localized_logo_250_100_png_cids.len() as u32,
+		))]
 		pub fn propose_to_update_application(
 			origin: OriginFor<T>,
 			application_index: ApplicationIndex,
