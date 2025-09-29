c0dc3999356ed03e9d15194b62456e68cc487934
saraswatpuneet <61435908+saraswatpuneet@users.noreply.github.com>
2025-09-29 21:07:35 +0000

    Update weights for release v1.17.5-rc8

--- runtime/common/src/weights/extrinsic_weights.rs
+++ runtime/common/src/weights/extrinsic_weights.rs
@@ -15,8 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 47.2.0
-//! DATE: 2025-07-15 (Y/M/D)
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 49.1.0
+//! DATE: 2025-09-29 (Y/M/D)
 //! HOSTNAME: `ip-10-173-5-194`, CPU: `Intel(R) Xeon(R) Platinum 8375C CPU @ 2.90GHz`
 //!
 //! SHORT-NAME: `extrinsic`, LONG-NAME: `ExtrinsicBase`, RUNTIME: `Frequency`
@@ -42,17 +42,17 @@ parameter_types! {
 	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 97_154, 99_331
-	///   Average:  98_029
-	///   Median:   98_086
-	///   Std-Dev:  461.06
+	///   Min, Max: 97_728, 100_156
+	///   Average:  98_929
+	///   Median:   99_046
+	///   Std-Dev:  565.4
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 99_250
-	///   95th: 98_727
-	///   75th: 98_318
+	///   99th: 99_964
+	///   95th: 99_611
+	///   75th: 99_345
 	pub const ExtrinsicBaseWeight: Weight =
-		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(98_029), 0);
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(98_929), 0);
 }
 
 #[cfg(test)]
