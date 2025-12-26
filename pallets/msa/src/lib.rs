ab3ba2dc76778d97c7b198a1c27524f6d0bed55c
Joe Caputo <joseph.caputo@projectliberty.io>
2025-12-26 11:56:44 -0500

    fix: refactor add_offchain_event to eliminate multiple conditional compiles

--- pallets/msa/src/lib.rs
+++ pallets/msa/src/lib.rs
@@ -636,8 +636,7 @@ pub mod pallet {
 			let (new_msa_id, new_public_key) = Self::create_account(public_key)?;
 
 			let event = Event::MsaCreated { msa_id: new_msa_id, key: new_public_key };
-			#[cfg(not(feature = "no-custom-host-functions"))]
-			offchain_index_event::<T>(Some(&event), new_msa_id);
+			Self::add_event_to_offchain_index(Some(&event), new_msa_id);
 			Self::deposit_event(event);
 			Ok(())
 		}
@@ -703,8 +702,7 @@ pub mod pallet {
 			)?;
 			let event =
 				Event::MsaCreated { msa_id: new_delegator_msa_id, key: new_delegator_public_key };
-			#[cfg(not(feature = "no-custom-host-functions"))]
-			offchain_index_event::<T>(Some(&event), new_delegator_msa_id);
+			Self::add_event_to_offchain_index(Some(&event), new_delegator_msa_id);
 			Self::deposit_event(event);
 			Self::deposit_event(Event::DelegationGranted {
 				delegator_id: DelegatorId(new_delegator_msa_id),
@@ -891,8 +889,7 @@ pub mod pallet {
 
 			let event =
 				Event::PublicKeyAdded { msa_id, key: add_key_payload.new_public_key.clone() };
-			#[cfg(not(feature = "no-custom-host-functions"))]
-			offchain_index_event::<T>(Some(&event), msa_id);
+			Self::add_event_to_offchain_index(Some(&event), msa_id);
 			Self::deposit_event(event);
 
 			Ok(())
@@ -926,8 +923,7 @@ pub mod pallet {
 
 					// Deposit the event
 					let event = Event::PublicKeyDeleted { key: public_key_to_delete };
-					#[cfg(not(feature = "no-custom-host-functions"))]
-					offchain_index_event::<T>(Some(&event), who_msa_id);
+					Self::add_event_to_offchain_index(Some(&event), who_msa_id);
 					Self::deposit_event(event);
 				},
 				None => {
@@ -1011,8 +1007,7 @@ pub mod pallet {
 				Some(msa_id) => {
 					Self::delete_key_for_msa(msa_id, &who)?;
 					let event = Event::PublicKeyDeleted { key: who };
-					#[cfg(not(feature = "no-custom-host-functions"))]
-					offchain_index_event::<T>(Some(&event), msa_id);
+					Self::add_event_to_offchain_index(Some(&event), msa_id);
 					Self::deposit_event(event);
 					Self::deposit_event(Event::MsaRetired { msa_id });
 				},
@@ -1077,7 +1072,6 @@ pub mod pallet {
 		}
 
 		/// A generic endpoint to replay any offchain related event to fix any potential issues
-		#[cfg(not(feature = "no-custom-host-functions"))]
 		#[pallet::call_index(13)]
 		#[pallet::weight(T::WeightInfo::reindex_offchain())]
 		pub fn reindex_offchain(
@@ -1094,10 +1088,10 @@ pub mod pallet {
 					match index_key {
 						Some(key) => {
 							let event = Event::PublicKeyAdded { msa_id, key };
-							offchain_index_event::<T>(Some(&event), msa_id);
+							Self::add_event_to_offchain_index(Some(&event), msa_id);
 						},
 						None => {
-							offchain_index_event::<T>(None, msa_id);
+							Self::add_event_to_offchain_index(None, msa_id);
 						},
 					}
 				},
@@ -1325,8 +1319,7 @@ pub mod pallet {
 				msa_id: add_key_payload.msa_id,
 				key: add_key_payload.new_public_key.clone(),
 			};
-			#[cfg(not(feature = "no-custom-host-functions"))]
-			offchain_index_event::<T>(Some(&event), add_key_payload.msa_id);
+			Self::add_event_to_offchain_index(Some(&event), add_key_payload.msa_id);
 			Self::deposit_event(event);
 
 			// Invalidate the recovery commitment (single-use requirement)
@@ -2786,6 +2779,12 @@ impl<T: Config> Pallet<T> {
 			weight_per_logo_removal.saturating_mul(removal_over_charged_by as u64);
 		let actual_weight_used = base_weight.saturating_sub(weight_to_refund);
 		Ok(PostDispatchInfo { actual_weight: Some(actual_weight_used), pays_fee: Pays::Yes })
+	}
+
+	/// Add event to offchain index
+	fn add_event_to_offchain_index(event: Option<&Event::<T>>, msa_id: MessageSourceId) {
+		#[cfg(not(feature = "no-custom-host-functions"))]
+		offchain_index_event::<T>(event, msa_id);
 	}
 }
 
