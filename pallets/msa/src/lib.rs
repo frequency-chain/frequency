36bc001e5b0a34cf539e7ec0af47446079c7f87e
Joe Caputo <joseph.caputo@projectliberty.io>
2026-01-05 15:55:24 -0500

    feat: add build targets for building a runtime without custom host function references, suitable for using with Chopsticks (#2651)
    # Goal
    The goal of this PR is to enable building a WASM runtime that can be
    used directly with Chopsticks to build blocks.
    Chopsticks runs its own node code that does not include Frequency's
    custom host functions. While it is possible to instruct Chopsticks to
    ignore unresolved host function references when loading the WASM, there
    remains the problem that any runtime calls to the unresolved host
    function(s) will result in a WASM trap/abort.
    
    This PR adds a new Cargo feature flag, `no-host-functions`, that causes
    the WASM runtime to be compiled without any compile-time OR runtime
    references to custom host functions, thereby enabling such a runtime to
    be used to produce blocks when running under Chopsticks.
    
    # Usage
    In order to build a WASM runtime suitable for using with Chopsticks, see
    the
    [README](https://github.com/frequency-chain/frequency/blob/03599e78b4becc424e0eb67e6d01a97fc3f93301/README.md?plain=1#L115)
    
    # Checklist
    - [x] Updated Readme?

--- pallets/msa/src/lib.rs
+++ pallets/msa/src/lib.rs
@@ -83,8 +83,10 @@ pub use types::{
 pub use weights::*;
 
 /// Offchain storage for MSA pallet
+#[cfg(not(feature = "no-custom-host-functions"))]
 pub mod offchain_storage;
 use crate::types::{LogoCid, PayloadTypeDiscriminator, RecoveryHash};
+#[cfg(not(feature = "no-custom-host-functions"))]
 pub use offchain_storage::*;
 
 #[cfg(feature = "runtime-benchmarks")]
@@ -608,6 +610,7 @@ pub mod pallet {
 			T::DbWeight::get().reads_writes(1u64, 1u64).saturating_add(migration_weight)
 		}
 
+		#[cfg(not(feature = "no-custom-host-functions"))]
 		fn offchain_worker(block_number: BlockNumberFor<T>) {
 			log::info!("Running offchain workers! {block_number:?}");
 			do_offchain_worker::<T>(block_number)
@@ -633,7 +636,7 @@ pub mod pallet {
 			let (new_msa_id, new_public_key) = Self::create_account(public_key)?;
 
 			let event = Event::MsaCreated { msa_id: new_msa_id, key: new_public_key };
-			offchain_index_event::<T>(Some(&event), new_msa_id);
+			Self::add_event_to_offchain_index(Some(&event), new_msa_id);
 			Self::deposit_event(event);
 			Ok(())
 		}
@@ -699,7 +702,7 @@ pub mod pallet {
 			)?;
 			let event =
 				Event::MsaCreated { msa_id: new_delegator_msa_id, key: new_delegator_public_key };
-			offchain_index_event::<T>(Some(&event), new_delegator_msa_id);
+			Self::add_event_to_offchain_index(Some(&event), new_delegator_msa_id);
 			Self::deposit_event(event);
 			Self::deposit_event(Event::DelegationGranted {
 				delegator_id: DelegatorId(new_delegator_msa_id),
@@ -886,7 +889,7 @@ pub mod pallet {
 
 			let event =
 				Event::PublicKeyAdded { msa_id, key: add_key_payload.new_public_key.clone() };
-			offchain_index_event::<T>(Some(&event), msa_id);
+			Self::add_event_to_offchain_index(Some(&event), msa_id);
 			Self::deposit_event(event);
 
 			Ok(())
@@ -920,7 +923,7 @@ pub mod pallet {
 
 					// Deposit the event
 					let event = Event::PublicKeyDeleted { key: public_key_to_delete };
-					offchain_index_event::<T>(Some(&event), who_msa_id);
+					Self::add_event_to_offchain_index(Some(&event), who_msa_id);
 					Self::deposit_event(event);
 				},
 				None => {
@@ -1004,7 +1007,7 @@ pub mod pallet {
 				Some(msa_id) => {
 					Self::delete_key_for_msa(msa_id, &who)?;
 					let event = Event::PublicKeyDeleted { key: who };
-					offchain_index_event::<T>(Some(&event), msa_id);
+					Self::add_event_to_offchain_index(Some(&event), msa_id);
 					Self::deposit_event(event);
 					Self::deposit_event(Event::MsaRetired { msa_id });
 				},
@@ -1085,10 +1088,10 @@ pub mod pallet {
 					match index_key {
 						Some(key) => {
 							let event = Event::PublicKeyAdded { msa_id, key };
-							offchain_index_event::<T>(Some(&event), msa_id);
+							Self::add_event_to_offchain_index(Some(&event), msa_id);
 						},
 						None => {
-							offchain_index_event::<T>(None, msa_id);
+							Self::add_event_to_offchain_index(None, msa_id);
 						},
 					}
 				},
@@ -1316,7 +1319,7 @@ pub mod pallet {
 				msa_id: add_key_payload.msa_id,
 				key: add_key_payload.new_public_key.clone(),
 			};
-			offchain_index_event::<T>(Some(&event), add_key_payload.msa_id);
+			Self::add_event_to_offchain_index(Some(&event), add_key_payload.msa_id);
 			Self::deposit_event(event);
 
 			// Invalidate the recovery commitment (single-use requirement)
@@ -2776,6 +2779,12 @@ impl<T: Config> Pallet<T> {
 			weight_per_logo_removal.saturating_mul(removal_over_charged_by as u64);
 		let actual_weight_used = base_weight.saturating_sub(weight_to_refund);
 		Ok(PostDispatchInfo { actual_weight: Some(actual_weight_used), pays_fee: Pays::Yes })
+	}
+
+	/// Add event to offchain index
+	fn add_event_to_offchain_index(event: Option<&Event<T>>, msa_id: MessageSourceId) {
+		#[cfg(not(feature = "no-custom-host-functions"))]
+		offchain_index_event::<T>(event, msa_id);
 	}
 }
 
